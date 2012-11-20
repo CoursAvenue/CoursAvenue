ea664da8ad8b3a85d71e215b74a44cdc167c37ff
Nima Izadi <nim.izadi@gmail.com>
2012-11-20 22:18:53 +0100

    Implemented some search functions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,33 @@
 class CoursesController < ApplicationController
 
   def index
+    params[:page] ||= 1
+    @courses = Course
+
+    @audiences = Audience.all
+    @levels    = Level.all
+
+
     unless params[:search].blank?
-      d = params[:search][:discipline]
-      @courses = Course.joins{discipline}.where{discipline.name =~ d}.all
-    else
-      @courses = Course.all
+      params[:search].each do |key, value|
+        case key
+        when 'discipline'
+          discipline_name  = value + '%'
+          @courses         = @courses.joins{discipline}.where{discipline.name =~ discipline_name}
+        when 'type'
+          type_name = (value == 'training' ? 'Course::Training' : 'Course::Lesson')
+          @courses         = @courses.where{type == type_name}
+        when 'audiences'
+          @courses = @courses.joins{audiences}.where{audiences.name.like_any value}
+        when 'levels'
+          @courses = @courses.joins{levels}.where{levels.name.like_any value}
+        end
+      end
+    end
+    @courses = @courses.paginate(page: params[:page]).all
+
+    respond_to do |format|
+      format.html { @courses }
     end
   end
 
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,11 @@
 module CoursesHelper
+  def join_audiences(course)
+    audiences = course.audiences.collect{|audience| content_tag(:li, audience.name) }
+    content_tag(:ul, audiences.join('').html_safe)
+  end
+
+  def join_levels(course)
+    levels = course.levels.collect{|level| content_tag(:li, level.name) }
+    content_tag(:ul, levels.join('').html_safe)
+  end
 end
