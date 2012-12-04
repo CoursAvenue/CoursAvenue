b830850c771313dcfacc8db0452e4b1852e3ab0a
Nima Izadi <nim.izadi@gmail.com>
2012-12-04 09:53:14 +0100

    Fixed assets for heroku

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ A custom renderer has been created in lib/
 # SCSS
 Inuit.css
 Compass for mixins
+
+# Add remote branch for Heroku
+git remote add heroku git@heroku.com:leboncours.git
--- config/application.rb
+++ config/application.rb
@@ -86,5 +86,9 @@ module LeBonCours
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Will not load the environment when compile the assets
+    # See: http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+    config.initialize_on_precompile = false
   end
 end
