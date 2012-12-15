8dd5246dde63411b244733d6969d5556708953ca
Nima Izadi <nim.izadi@gmail.com>
2012-12-15 18:01:46 +0100

    Add a line in import.sh

--- import.sh
+++ import.sh
@@ -4,6 +4,7 @@ rake "import:renting_rooms[Export/Location.csv]"
 rake "import:courses_prices_and_plannings[Export/Planning-Cours.csv]"
 
 # heroku pg:reset DATABASE
+# heroku run rake db:migrate
 # heroku run rake import:clean
 # heroku run rake "import:structures[Export/Structure.csv]"
 # heroku run rake "import:renting_rooms[Export/Location.csv]"
