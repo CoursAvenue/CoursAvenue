f76a452923ced5e3a8d262464f8c0d6f5bcc4954
Nima Izadi <nim.izadi@gmail.com>
2012-12-23 14:17:10 +0100

    Add town chooser

---
+++ app/assets/javascripts/objects/better_select.js
@@ -0,0 +1,43 @@
+(function() {
+    'use strict';
+
+    var objects = GLOBAL.namespace('GLOBAL.Objects');
+
+    /*
+     * Given an input element, will update a relative element.
+     */
+
+    objects.BetterSelect = new Class({
+
+        initialize: function(el, options) {
+            this.el             = el;
+            this.select_input   = el.getElement('select');
+            this.title          = el.getElement('.chooser-title');
+            this.title_wrapper  = el.getElement('.chooser-title-wrapper');
+            this.attachEvents();
+        },
+
+        attachEvents: function() {
+            this.el.addEvent('click', function() {
+                this.select_input.replaces(this.title_wrapper);
+                this.select_input.show();
+            }.bind(this));
+
+            this.select_input.addEvent('change', function(event) {
+                if (event.target.getSelected()[0].get('text') !== 'Paris') {
+                    alert("Nous n'avons pas encore de cours pour cette ville. Si vous voulez référencez votre cours, contactez-nous !");
+                    this.select_input.getElement('option').selected = true; // Because Paris is the first option
+                }
+                // this.title.set('text', event.target.getSelected()[0].get('text'));
+                this.title_wrapper.replaces(this.select_input);
+            }.bind(this));
+        }
+    });
+})();
+
+// Initialize all input-update objects
+window.addEvent('domready', function() {
+    $$('[data-behavior=better-select]').each(function(el) {
+        new GLOBAL.Objects.BetterSelect(el);
+    });
+});
