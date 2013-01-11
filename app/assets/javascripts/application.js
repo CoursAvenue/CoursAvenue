480a9fb71be3b6b4cf02234a76f4fb318a088f1e
Nima Izadi <nim.izadi@gmail.com>
2013-01-11 10:35:09 +0100

    Fixed Modernizr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require modernizr
-//
 //= require global
 
 // ---------------------------------- Core include
@@ -50,3 +48,4 @@ window.addEvent('domready', function() {
         transition: Fx.Transitions.Quad.easeInOut
     });
 });
+
