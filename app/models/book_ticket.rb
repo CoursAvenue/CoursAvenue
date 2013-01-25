eeb97ac2e8548dc3ec4a6daff3e8266837e190cf
Nima Izadi <nim.izadi@gmail.com>
2013-01-25 16:43:14 +0100

    Fixed courses

--- app/models/book_ticket.rb
+++ app/models/book_ticket.rb
@@ -2,4 +2,7 @@ class BookTicket < ActiveRecord::Base
   belongs_to :course_group
 
   attr_accessible :number, :price, :validity # in weeks
+
+  validates :number, :price, :validity, presence: true
+
 end
