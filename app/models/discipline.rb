651aa54871e867c376488a9d59fd30cd39663add
Nima Izadi <nim.izadi@gmail.com>
2013-01-28 10:59:20 +0100

    Fixed bug

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,6 +1,6 @@
 class Discipline < ActiveRecord::Base
   has_ancestry
-  has_attached_file :image, :styles => { :wide=> "300x300#", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { wide: "400x250#", thumb: "100x100>" }
 
   has_and_belongs_to_many :courses
 
