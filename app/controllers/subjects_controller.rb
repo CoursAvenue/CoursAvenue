201929e34d0ce270c3db6d787b39804e74cc9582
Nima Izadi <nim.izadi@gmail.com>
2013-02-11 11:03:32 +0100

    Fixed time search

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -72,29 +72,29 @@ class SubjectsController < ApplicationController
     end
 
     @search = Sunspot.search(Course) do
-      fulltext                              params[:name]                                         if params[:name].present?
+      fulltext                              params[:name]                                           if params[:name].present?
       with :city,                           params[:city_id]
-      with(:subject_ids).any_of             subject_ids                                           if params[:id]
-      with(:type).any_of                    params[:types]                                        if params[:types].present?
-      with(:audience_ids).any_of            params[:audiences]                                    if params[:audiences].present?
-      with(:level_ids).any_of               level_ids                                             unless level_ids.empty?
-      with :week_days,                      params[:week_days]                                    if params[:week_days].present?
+      with(:subject_ids).any_of             subject_ids                                             if params[:id]
+      with(:type).any_of                    params[:types]                                          if params[:types].present?
+      with(:audience_ids).any_of            params[:audiences]                                      if params[:audiences].present?
+      with(:level_ids).any_of               level_ids                                               unless level_ids.empty?
+      with :week_days,                      params[:week_days]                                      if params[:week_days].present?
 
-      with(:min_age_for_kid).less_than      params[:age][:max]                                    if params[:age].present? and params[:age][:max].present?
-      with(:max_age_for_kid).greater_than   params[:age][:min]                                    if params[:age].present? and params[:age][:min].present?
+      with(:min_age_for_kid).less_than      params[:age][:max]                                      if params[:age].present? and params[:age][:max].present?
+      with(:max_age_for_kid).greater_than   params[:age][:min]                                      if params[:age].present? and params[:age][:min].present?
 
-      with(:end_date).greater_than          params[:start_date]                                   if params[:start_date].present?
-      with(:start_date).less_than           params[:end_date]                                     if params[:end_date].present?
+      with(:end_date).greater_than          params[:start_date]                                     if params[:start_date].present?
+      with(:start_date).less_than           params[:end_date]                                       if params[:end_date].present?
 
-      with(:start_time).greater_than        params[:time_range][:min]                             if params[:time_range].present? and params[:time_range][:min].present?
-      with(:end_time).less_than             params[:time_range][:max]                             if params[:time_range].present? and params[:time_range][:max].present?
+      with(:start_time).greater_than        TimeParser.parse_time_string(params[:time_range][:min]) if params[:time_range].present? and params[:time_range][:min].present?
+      with(:end_time).less_than             TimeParser.parse_time_string(params[:time_range][:max]) if params[:time_range].present? and params[:time_range][:max].present?
 
-      with(:time_slots).any_of              params[:time_slots]                                   if params[:time_slots].present?
+      with(:time_slots).any_of              params[:time_slots]                                     if params[:time_slots].present?
 
-      with(:min_price).greater_than         params[:price_range][:min].to_i                       if params[:price_range].present? and params[:price_range][:min].present?
-      with(:max_price).less_than            params[:price_range][:max].to_i                       if params[:price_range].present? and params[:price_range][:max].present?
+      with(:min_price).greater_than         params[:price_range][:min].to_i                         if params[:price_range].present? and params[:price_range][:min].present?
+      with(:max_price).less_than            params[:price_range][:max].to_i                         if params[:price_range].present? and params[:price_range][:max].present?
 
-      with(:zip_code).any_of                params[:zip_codes]                                    if params[:zip_codes].present?
+      with(:zip_code).any_of                params[:zip_codes]                                      if params[:zip_codes].present?
 
       # TODO ------------------------------------------------------------------------------------------------------------------
       if params[:price_specificities].present?
