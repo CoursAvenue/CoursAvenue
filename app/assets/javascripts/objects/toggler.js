4f0684469d8ab398810ee3394a762540eee36c72
Nima Izadi <nim.izadi@gmail.com>
2013-02-11 10:51:24 +0100

    Fixed side search

--- app/assets/javascripts/objects/toggler.js
+++ app/assets/javascripts/objects/toggler.js
@@ -13,33 +13,41 @@
             this.el         = el;
             this.caret_icon = el.getElement('i');
             this.toggled_el = el.getElement(toggled_element_selector || el.get('data-el'));
-            this.morth      = new Fx.Morph(this.toggled_el, {transition: Fx.Transitions.Cubic.easeOut});
+            this.morph      = new Fx.Morph(this.toggled_el, {transition: Fx.Transitions.Cubic.easeOut});
             this.attachEvents();
         },
 
         attachEvents: function() {
-            var toggle          = true;
+            var is_hidden       = this.toggled_el.hasClass('hide');
             var height          = this.toggled_el.getStyle('height');
             var padding_top     = this.toggled_el.getStyle('padding-top');
             var padding_bottom  = this.toggled_el.getStyle('padding-bottom');
+            if (is_hidden) {
+                this.toggled_el.setStyles({
+                    'height'        : 0,
+                    'padding-top'   : 0,
+                    'padding-bottom': 0
+                });
+                this.toggled_el.removeClass('hide');
+            }
             this.el.addEvent('click', function() {
-                this.morth.cancel();
-                if (toggle) {
-                    this.caret_icon.removeClass('icon-caret-down').addClass('icon-caret-left');
-                    this.morth.start({
-                        'height'        : 0,
-                        'padding-top'   : 0,
-                        'padding-bottom': 0
-                    });
-                } else {
+                this.morph.cancel();
+                if (is_hidden) {
                     this.caret_icon.addClass('icon-caret-down').removeClass('icon-caret-left');
-                    this.morth.start({
+                    this.morph.start({
                         'height'        : height,
                         'padding-top'   : padding_top,
                         'padding-bottom': padding_bottom
                     });
+                } else {
+                    this.caret_icon.removeClass('icon-caret-down').addClass('icon-caret-left');
+                    this.morph.start({
+                        'height'        : 0,
+                        'padding-top'   : 0,
+                        'padding-bottom': 0
+                    });
                 }
-                toggle = !toggle;
+                is_hidden = !is_hidden;
             }.bind(this));
         }
     });
