5804fc40e70bca69a205c053852b2504b6ab59f3
Nima Izadi <nim.izadi@gmail.com>
2013-02-26 16:31:38 +0100

    Improved admin part

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,2 +1,13 @@
 class Admin::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(admin)
+    if admin.super_admin?
+      profs_structures_path
+    else
+      if admin.structure
+        profs_structure_path(admin.structure)
+      else
+        new_profs_structure_path
+      end
+    end
+  end
 end
