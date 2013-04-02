ec22e8943aef73f59f7bed2486bba0aa82868526
Nima Izadi <nim.izadi@gmail.com>
2013-04-02 18:11:05 +0200

    Add reservations in dashboard

--- app/controllers/pro/dashboard_controller.rb
+++ app/controllers/pro/dashboard_controller.rb
@@ -5,5 +5,6 @@ class Pro::DashboardController < Pro::ProController
     @reservations_logger = ReservationLogger.order("DATE(created_at)").group("DATE(created_at)").count
     @renting_rooms       = RentingRoom.all
     @click_logger        = ClickLogger.group('name').count
+    @reservations        = Reservation.all
   end
 end
