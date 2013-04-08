09daa8f0fac0ba06eebe715cec99a9286d3b87b2
Nima Izadi <nim.izadi@gmail.com>
2013-04-08 17:05:37 +0200

    Fixed lieus to lieux

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,22 @@ module ApplicationHelper
   # Overriding Kaminari's method
   # https://github.com/amatsuda/kaminari/blob/master/lib/kaminari/helpers/action_view_extension.rb
   def page_entries_info(collection, options = {})
-      entry_name = if options[:entry_name]
-        options[:entry_name]
-      elsif collection.empty? || collection.is_a?(PaginatableArray)
-        'entry'
+      if options[:pluralized_entry_name]
+        entry_name = options[:pluralized_entry_name]
       else
-        if collection.respond_to? :model  # DataMapper
-          collection.model.model_name.human.downcase
-        else  # AR
-          collection.model_name.human.downcase
+        entry_name = if options[:entry_name]
+          options[:entry_name]
+        elsif collection.empty? || collection.is_a?(PaginatableArray)
+          'entry'
+        else
+          if collection.respond_to? :model  # DataMapper
+            collection.model.model_name.human.downcase
+          else  # AR
+            collection.model_name.human.downcase
+          end
         end
+        entry_name = entry_name.pluralize unless collection.total_count == 1
       end
-      entry_name = entry_name.pluralize unless collection.total_count == 1
-
       if collection.total_pages < 2
         t('helpers.page_entries_info.one_page.display_entries', :entry_name => entry_name, :count => collection.total_count)
       else
