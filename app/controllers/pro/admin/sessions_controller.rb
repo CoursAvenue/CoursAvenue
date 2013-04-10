1acca7d3da5367eb20e98cb798dd5e8242fd2a65
Nima Izadi <nim.izadi@gmail.com>
2013-04-10 11:04:57 +0200

    Fixed bugs

--- app/controllers/pro/admin/sessions_controller.rb
+++ app/controllers/pro/admin/sessions_controller.rb
@@ -2,12 +2,12 @@ class Pro::Admin::SessionsController < Devise::SessionsController
   layout 'admin'
   def after_sign_in_path_for(admin)
     if admin.super_admin?
-      structures_path
+      pro_structures_path
     else
       if admin.structure
-        structure_path(admin.structure)
+        pro_structure_path(admin.structure)
       else
-        new_structure_path
+        new_pro_structure_path
       end
     end
   end
