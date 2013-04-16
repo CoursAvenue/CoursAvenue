9b445b85af45977ecf0ba9c2678a00a0d772ef90
Nima Izadi <nim.izadi@gmail.com>
2013-04-16 11:02:23 +0200

    Add rdfa

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -3,6 +3,14 @@ module PlacesHelper
     "#{place.city.name}"
   end
   def readable_address(place)
-    "#{place.street}, #{place.zip_code}, Paris"
+    address = ""
+    address << content_tag(:span, itemprop: 'address', itemscope: true, itemtype: 'http://schema.org/PostalAddress') do
+      inner_address = ''
+      inner_address << content_tag(:span, place.street, itemprop: 'streetAddress')
+      inner_address << ', '
+      inner_address << content_tag(:span, place.city.name, itemprop: 'addressLocality')
+      inner_address.html_safe
+    end
+    address.html_safe
   end
 end
