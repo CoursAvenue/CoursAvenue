c1034a48226ba1ee4798af565918f442d8dadc15
Nima Izadi <nim.izadi@gmail.com>
2013-04-17 17:14:22 +0200

    Improved backoffice

--- app/controllers/pro/courses_controller.rb
+++ app/controllers/pro/courses_controller.rb
@@ -17,12 +17,36 @@ class Pro::CoursesController < InheritedResources::Base
     end
   end
 
+  def update_price
+    errors = false
+    if params[:individual_course_price][:amount].present?
+      @individual_price = @course.prices.where{libelle == 'prices.individual_course'}.first || @course.prices.build
+      errors = @individual_price.update_attributes(params[:individual_course_price])
+    end
+    if params[:price] and params[:price][:amount].present?
+      @subscription = @course.prices.where{libelle != 'prices.individual_course'}.first || @course.prices.build
+      errors = errors and @subscription.update_attributes(params[:price])
+    end
+    if params[:book_ticket] and params[:book_ticket][:price].present?
+      @book_ticket = @course.book_tickets.where{number == 10}.first || @course.book_tickets.build
+      errors = errors and @book_ticket.update_attributes(params[:book_ticket])
+    end
+
+    respond_to do |format|
+      if errors
+        format.html{ redirect_to pro_structure_path(@structure) }
+      else
+        format.html{ redirect_to pro_course_prices_path(@course), error: 'Vous devez renseigner au moins un prix' }
+      end
+    end
+  end
+
   def update
     if params[:course].delete(:delete_image) == '1'
       resource.image.clear
     end
     update! do |success, failure|
-      success.html { redirect_to (params[:from] || pro_structure_path(@structure)) }
+      success.html { redirect_to pro_structure_path(@structure) }
     end
   end
 
