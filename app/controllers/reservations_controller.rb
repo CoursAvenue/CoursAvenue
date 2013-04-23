9248ffccaf98caf3626a889da42423d3d349d394
Nima Izadi <nim.izadi@gmail.com>
2013-04-23 10:26:17 +0200

    Redirection 301

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,34 +4,11 @@ class ReservationsController < ApplicationController
   before_filter :retrieve_info
 
   def new
-    @reservation = Reservation.new
-  end
-
-  def show
-    @reservation = Reservation.find params[:id]
-  end
-
-  def create
-    @reservation       = @course.reservations.build params[:reservation]
-    @reservation.price = @course.prices.where{nb_courses == 1}.first
-    respond_to do |format|
-      if @reservation.save
-        format.html { redirect_to course_path(@course), notice: 'Votre réservation à bien été pris en compte. Un mail vous a été envoyé.' }
-        UserMailer.alert_structure_for_reservation(@reservation).deliver!
-        UserMailer.alert_user_for_reservation(@reservation).deliver!
-      else
-        format.html { redirect_to course_path(@course), alert: "Veuillez remplir tous les champs pour votre réservation." }
-      end
-    end
+    redirect_to course_path(@course), status: 301
   end
 
   private
   def retrieve_info
     @course       = Course.find params[:course_id]
-    @structure    = @course.structure
-    @place        = @course.place
-    @plannings    = @course.plannings
-    @prices       = @course.prices
-    @book_tickets = @course.book_tickets
   end
 end
