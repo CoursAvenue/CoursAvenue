f05ec23a74a5f7bd704bec8555f9b0a4a03d360d
Nima Izadi <nim.izadi@gmail.com>
2013-04-24 14:14:01 +0200

    Fixed prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ CoursAvenue::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
   # Generate digests for assets URLs
   config.assets.digest = true
 
