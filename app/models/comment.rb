50b817044a30e196eba481a6a23e3b90b2ac6aae
Nima Izadi <nim.izadi@gmail.com>
2013-05-02 13:49:17 +0200

    Improved css

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,18 +15,14 @@ class Comment < ActiveRecord::Base
   # Update rating of the commentable (course, or place)
   def update_commentable_rating
     commentable = self.commentable
-    if commentable.is_a? Place
-      ratings = commentable.comments.group(:rating).count
-    else
-      ratings = commentable.comments.group(:rating).count
-    end
+    ratings     = commentable.comments.group(:rating).count
     nb_rating    = 0
     total_rating = 0
     ratings.delete_if {|k,v| k.nil?}.each do |key, value|
       nb_rating    += value
       total_rating += key * value
     end
-    new_rating = (total_rating.to_f / nb_rating.to_f)
+    new_rating = (nb_rating == 0 ? nil : (total_rating.to_f / nb_rating.to_f))
     self.commentable.update_attribute :rating, new_rating
   end
 
