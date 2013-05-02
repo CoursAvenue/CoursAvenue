1250c6db67e5de50e2c326b1cba98ca78d9396fd
Nima Izadi <nim.izadi@gmail.com>
2013-05-02 16:57:00 +0200

    Fixed bug

--- app/models/registration_fee.rb
+++ app/models/registration_fee.rb
@@ -2,8 +2,4 @@ class RegistrationFee < ActiveRecord::Base
   belongs_to :course
 
   attr_accessible :for_kid, :amount
-
-  def price
-    ('%.2f' % read_attribute(:price)).gsub('.00', '')
-  end
 end
