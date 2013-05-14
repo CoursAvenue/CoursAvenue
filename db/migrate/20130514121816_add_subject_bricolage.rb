7fd3aafe25b7873023c085973ea0953dc8dabbb2
Nima Izadi <nim.izadi@gmail.com>
2013-05-14 14:36:36 +0200

    Fixed

--- db/migrate/20130514121816_add_subject_bricolage.rb
+++ db/migrate/20130514121816_add_subject_bricolage.rb
@@ -1,14 +1,17 @@
 class AddSubjectBricolage < ActiveRecord::Migration
   def up
-    subject_1 = Subject.where(name: 'Arts manuels').first
+    subject_1        = Subject.where(name: 'Arts manuels').first
     bricolage        = Subject.create name: 'Bricolage'
     bricolage.parent = subject_1
+    bricolage.save
 
-    subject_2 = Subject.where(name: 'Ateliers enfants / duo parent-enfant').first
-    bricolage        = Subject.create name: 'Bricolage'
-    bricolage.parent = subject_2
+    subject_2          = Subject.where(name: 'Ateliers enfants / duo parent-enfant').first
+    bricolage_2        = Subject.create name: 'Bricolage'
+    bricolage_2.parent = subject_2
+    bricolage_2.save
   end
 
   def down
+    Subject.where(name: 'Bricolage').delete_all
   end
 end
