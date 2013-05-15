187539acf8e6ba285639188e1175c860174ae8aa
Nima Izadi <nim.izadi@gmail.com>
2013-05-15 11:01:04 +0200

    Fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ CoursAvenue::Application.configure do
     address:          "smtp.mandrillapp.com",
     port:             '587',
     user_name:        ENV["MANDRILL_USERNAME"],
-    password:         ENV["MANDRILL_API_KEY"]
+    password:         ENV["MANDRILL_API_KEY"],
     domain:           'coursavenue.com',
     authentication:   :plain
   }
