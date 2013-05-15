ba1af97dd6802418e93496aa9f7c3e25d596f091
Nima Izadi <nim.izadi@gmail.com>
2013-05-15 16:13:15 +0200

    Added blog and fixed typo

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,17 +4,17 @@ require 'open-uri'
 
 module HomeHelper
   def blog_feed
-    source = "http://blog.leboncours.com/feed/" # url or local file
+    source = "http://coursavenue-blog.herokuapp.com/feed" # url or local file
     content = "" # raw content of rss feed will be loaded here
     open(source) do |s| content = s.read end
     rss = RSS::Parser.parse(content, false)
 
-    html = "<ul class='blog-feed'>"
+    html = "<ul class='blog-feed flush--bottom'>"
     rss.items[0..3].each do |rss_item|
       html << "<li>"
       html << content_tag(:h4, link_to(rss_item.title, rss_item.link), class: 'blog-feed__title flush--bottom')
       html << content_tag(:span, l(rss_item.pubDate, format: :date).capitalize, class: 'blog-feed__date')
-      html << content_tag(:p, rss_item.description.html_safe, class: 'blog-feed__content')
+      html << content_tag(:p, rss_item.description.html_safe, class: 'blog-feed__content flush--bottom')
       html << "</li>"
     end
     html << "</ul>"
