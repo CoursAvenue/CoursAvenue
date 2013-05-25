f37b34c10bd2f7ed6f1ff889d63682f346f68a8f
Nima Izadi <nim.izadi@gmail.com>
2013-05-25 12:46:51 +0200

    Deleted file

--- spec/factories/participants.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :participant do
-    first_name "MyString"
-    last_name "MyString"
-    email "MyString"
-  end
-end
