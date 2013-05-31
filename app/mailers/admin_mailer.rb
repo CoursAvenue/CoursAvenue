27a35f23b95213813db9052b5c796e729dbd88d6
Nima Izadi <nim.izadi@gmail.com>
2013-05-31 11:13:31 +0200

    Improved share page

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -10,7 +10,7 @@ class AdminMailer < ActionMailer::Base
 
   def send_feedbacks(structure, email)
     @structure = structure
-    mail to: email, subject: "Un petit coup de pouce !", from: @structure.contact_email
+    mail to: email, subject: "Un petit coup de pouce !"
   end
 
   def admin_validated(admin)
