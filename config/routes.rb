b4266f7f5dcaec0955d268217ee4f2f4b71112bf
Nima Izadi <nim.izadi@gmail.com>
2013-06-05 18:19:39 +0200

    Fixed bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ CoursAvenue::Application.routes.draw do
   # ---------------------------------------------
   # ----------------------------------------- PRO
   # ---------------------------------------------
-  # constraints subdomain: 'pro' do
+  constraints subdomain: 'pro' do
     namespace :pro, path: '' do
       root :to => 'home#index'
       match 'pages/presentation'           => 'home#presentation'
@@ -79,7 +79,7 @@ CoursAvenue::Application.routes.draw do
       end
       devise_for :admins, controllers: { sessions: 'pro/admins/sessions', registrations: 'pro/admins/registrations', passwords: 'pro/admins/passwords', confirmations: 'pro/admins/confirmations'} , path: '/', path_names: { sign_in: '/connexion', sign_out: 'logout', registration: 'rejoindre-coursavenue-pro', sign_up: '/', :confirmation => 'verification'}#, :password => 'secret', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
     end
-  # end
+  end
 
   # ---------------------------------------------
   # ----------------------------------------- WWW
