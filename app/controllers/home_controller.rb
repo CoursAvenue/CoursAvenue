9677e41d7107d223c0be76fe73f0bdd997c4a56b
Nima Izadi <nim.izadi@gmail.com>
2013-06-19 21:18:12 +0200

    Fixed comment nb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @audiences        = Audience.all
     @levels           = Level.all
     @promoted_courses = Course.where{is_promoted == true}.shuffle[0...3]
-    @comments         = Comment.order('created_at DESC').limit(15)
+    @comments         = Comment.order('created_at DESC').limit(16)
 
     @homepage_images  = [['home-page/dance.jpg', 'Cours de danse'],
                          ['home-page/painter.jpg', 'Cours de peinture']]
