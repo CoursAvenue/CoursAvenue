6937cdfc534d4273ad7b9de2fc5a334aecf882cc
Nima Izadi <nim.izadi@gmail.com>
2013-06-23 22:51:46 +0200

    Improved dashboard

--- app/controllers/pro/dashboard_controller.rb
+++ app/controllers/pro/dashboard_controller.rb
@@ -4,10 +4,12 @@ class Pro::DashboardController < Pro::ProController
   def index
     raise CanCan::AccessDenied.new unless current_pro_admin.super_admin?
     # @admins = Admin.count(:order => 'DATE(created_at) DESC', :group => ["DATE(created_at)"])
+    @courses  = Course.where{(created_at > Date.parse('01/06/2013')) & (active == true)}.count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
     @admins   = Admin  .count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
     @comments = Comment.count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
     @students = Student.count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
     @users    = User   .count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
+    @medias   = Media  .count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
   end
 end
 
