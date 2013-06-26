ff7624b721c0cbef3c989295bbc635e9b85ffef2
Nima Izadi <nim.izadi@gmail.com>
2013-06-26 21:59:08 +0200

    Fixed text and add bar in migration

--- db/migrate/20130624132039_remove_all_levels_from_level.rb
+++ db/migrate/20130624132039_remove_all_levels_from_level.rb
@@ -1,10 +1,11 @@
 class RemoveAllLevelsFromLevel < ActiveRecord::Migration
   def up
     all_levels = [Level.beginner, Level.intermediate, Level.confirmed]
-
+    bar = ProgressBar.new( Course.count )
     Course.joins{levels}.where{levels.name == 'level.all'}.readonly(false).all.each do |course|
       course.levels = all_levels
       course.save
+      bar.increment! 1
     end
     Level.where(name: 'level.all').first.destroy
   end
