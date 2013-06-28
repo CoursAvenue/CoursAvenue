89d8ad22286c2234b59ae1d101730150961deb72
Nima Izadi <nim.izadi@gmail.com>
2013-06-28 16:36:12 +0200

    Removed share profil step

--- app/controllers/pro/structures_controller.rb
+++ app/controllers/pro/structures_controller.rb
@@ -159,7 +159,8 @@ class Pro::StructuresController < Pro::ProController
     respond_to do |format|
       if !@structure.new_record? or @structure.save
         session[:id] = @structure.id
-        format.html { redirect_to share_my_profile_pro_structure_path(@structure, subdomain: 'pro'), notice: 'Partagez maintenant votre profil public pour avoir des recommandations' }
+        format.html { redirect_to new_pro_admin_structure_registration_path(@structure, subdomain: 'pro'), notice: 'Félicitation, votre profil est maintenant créé !<br>Dernière étape : créez vos identifiants.' }
+        # format.html { redirect_to share_my_profile_pro_structure_path(@structure, subdomain: 'pro'), notice: 'Partagez maintenant votre profil public pour avoir des recommandations' }
       else
         format.html { render 'pro/structures/new_from_recomendation' }
       end
