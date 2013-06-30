57dd99befd10fd04f5f11f4c796eff5ca4e1a49d
Nima Izadi <nim.izadi@gmail.com>
2013-06-30 19:14:07 +0200

    Fixed bug

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,10 +2,11 @@ class Level < ActiveHash::Base
   include ActiveHash::Enum
 
   self.data = [
-    { id: 1, short_name: 'Initiation',   name: 'level.initiation',  order: 1},
-    { id: 2, short_name: 'Beginner',     name: 'level.beginner',    order: 2},
-    { id: 3, short_name: 'Intermediate', name: 'level.intermediate',order: 3},
-    { id: 4, short_name: 'Confirmed',    name: 'level.confirmed',   order: 4}
+    { id: 1, short_name: 'Initiation',   name: 'level.initiation',   order: 1},
+    { id: 2, short_name: 'Beginner',     name: 'level.beginner',     order: 2},
+    { id: 3, short_name: 'Intermediate', name: 'level.intermediate', order: 3},
+    { id: 4, short_name: 'Confirmed',    name: 'level.confirmed',    order: 4},
+    { id: 5, short_name: 'Professional', name: 'level.professional', order: 5}
   ]
   enum_accessor :short_name
 end
