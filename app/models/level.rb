a71ba40920d3386cc14f6f8d7ee61b7a65516442
Nima Izadi <nim.izadi@gmail.com>
2013-06-30 18:57:43 +0200

    Fixed level and audiences after migration

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,35 +1,11 @@
-# class Level < ActiveHash::Base
-#   include ActiveHash::Enum
+class Level < ActiveHash::Base
+  include ActiveHash::Enum
 
-#   self.data = [
-#     { id: 1, short_name: 'Initiation',   name: 'level.initiation',  order: 1},
-#     { id: 2, short_name: 'Beginner',     name: 'level.beginner',    order: 2},
-#     { id: 3, short_name: 'Intermediate', name: 'level.intermediate',order: 3},
-#     { id: 4, short_name: 'Confirmed',    name: 'level.confirmed',   order: 4}
-#   ]
-#   enum_accessor :short_name
-# end
-
-class Level < ActiveRecord::Base
-
-  has_and_belongs_to_many :plannings
-  has_many :courses,   through: :plannings
-
-  attr_accessible :name, :order
-
-  def self.initiation
-    Level.where(name: 'level.initiation').first
-  end
-
-  def self.beginner
-    Level.where(name: 'level.beginner').first
-  end
-
-  def self.intermediate
-    Level.where(name: 'level.intermediate').first
-  end
-
-  def self.confirmed
-    Level.where(name: 'level.confirmed').first
-  end
+  self.data = [
+    { id: 1, short_name: 'Initiation',   name: 'level.initiation',  order: 1},
+    { id: 2, short_name: 'Beginner',     name: 'level.beginner',    order: 2},
+    { id: 3, short_name: 'Intermediate', name: 'level.intermediate',order: 3},
+    { id: 4, short_name: 'Confirmed',    name: 'level.confirmed',   order: 4}
+  ]
+  enum_accessor :short_name
 end
