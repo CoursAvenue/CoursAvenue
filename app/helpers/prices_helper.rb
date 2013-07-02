722dba5ad81738ed4cc5427f1c64dd2a2d969e94
Nima Izadi <nim.izadi@gmail.com>
2013-07-02 12:40:37 +0200

    Some improvements in search

--- app/helpers/prices_helper.rb
+++ app/helpers/prices_helper.rb
@@ -7,12 +7,13 @@ module PricesHelper
     end
   end
 
-  def readable_amount amount
+  def readable_amount amount, round=false
     if amount.nil?
       nil
+    elsif round
+      amount.to_i
     else
       ('%.2f' % amount).gsub('.', ',').gsub(',00', '')
     end
   end
-
 end
