6737b6755b153f0b7801350e97ea1142664180bf
Nima Izadi <nim.izadi@gmail.com>
2013-07-02 15:03:53 +0200

    Fixed js rendering

--- app/controllers/pro/dashboard_controller.rb
+++ app/controllers/pro/dashboard_controller.rb
@@ -5,8 +5,14 @@ class Pro::DashboardController < Pro::ProController
     raise CanCan::AccessDenied.new unless current_pro_admin.super_admin?
     # @admins = Admin.count(:order => 'DATE(created_at) DESC', :group => ["DATE(created_at)"])
     @courses  = Course.where{(created_at > Date.parse('01/06/2013')) & (active == true)}.count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
-    @admins   = Admin  .where{created_at > Date.today - 2.months - 15.days}.count(:order => "DATE(created_at) ASC", :group => ["DATE(created_at)"])
-    @comments = Comment.where{created_at > Date.today - 2.months - 15.days}.count(:order => "DATE(created_at) ASC", :group => ["DATE(created_at)"])
+    @admins   = Admin  .where{created_at > Date.today - 1.months}.count(:order => "DATE(created_at) ASC", :group => ["DATE(created_at)"])
+    @comments = Comment.where{created_at > Date.today - 1.months}.count(:order => "DATE(created_at) ASC", :group => ["DATE(created_at)"])
+
+    hash_of_days = {}
+    ((1.months).ago.to_date..Date.today).each{ |date| hash_of_days[date.strftime("%Y-%m-%d")] = 0}
+    @comments  = hash_of_days.merge @comments
+    @admins    = hash_of_days.merge @admins
+
     @students = Student.count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
     @users    = User   .count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
     @medias   = Media  .count(:order => "DATE_TRUNC('week', created_at) ASC", :group => ["DATE_TRUNC('week', created_at)"])
--- app/controllers/pro/students_controller.rb
+++ app/controllers/pro/students_controller.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class Pro::StudentsController < Pro::ProController
   before_filter :authenticate_pro_admin!
+  respond_to :js
 
   layout 'admin'
 
@@ -13,5 +14,8 @@ class Pro::StudentsController < Pro::ProController
   def ask_for_feedbacks_stage_1
     @students = Student.find params[:id]
     @students.ask_for_feedbacks_stage_1
+    respond_to do |format|
+      format.js { render nothing: true, status: 200 }
+    end
   end
 end
