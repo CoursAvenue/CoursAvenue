47acf0f296df562c4d443beeb5d40a7b507b5f51
Nima Izadi <nim.izadi@gmail.com>
2013-07-08 17:47:13 +0200

    Fixed migration

---
+++ db/migrate/20130708154431_set_weight_to_subjects.rb
@@ -0,0 +1,12 @@
+class SetWeightToSubjects < ActiveRecord::Migration
+  def up
+    subjects = ["Théâtre & Danse" ,"Musique & Chant" ,"Cuisine & Vins" ,"Enfants & Ados" ,"Loisirs créatifs" ,"Art & Artisanat" ,"Bien-être & Santé" ,"Photo & Vidéo" ,"Sports"]
+    subjects.each_with_index do |subject_name, index|
+      s = Subject.where{(name == subject_name) & (ancestry == nil)}.first
+      s.update_column :position, (index + 1)
+    end
+  end
+
+  def down
+  end
+end
