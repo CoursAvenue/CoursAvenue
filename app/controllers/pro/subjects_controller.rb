72656f8d4ad668499517f8bdb40d393a4217de05
Nima Izadi <nim.izadi@gmail.com>
2013-07-23 14:41:30 +0200

    Fixed authorization for subjects

--- app/controllers/pro/subjects_controller.rb
+++ app/controllers/pro/subjects_controller.rb
@@ -1,5 +1,8 @@
 # encoding: utf-8
 class Pro::SubjectsController < Pro::ProController
+  before_filter :authenticate_pro_admin!
+  before_filter :authenticate_pro_super_admin
+
   def index
     @subjects = Subject.all
   end
@@ -16,6 +19,13 @@ class Pro::SubjectsController < Pro::ProController
       else
         format.html { render action: :edit }
       end
+    end
+  end
+
+  private
+  def authenticate_pro_super_admin
+    unless can? :edit, Admin
+      redirect_to root_path, alert: "Vous n'avez pas le droit !"
     end
   end
 end
