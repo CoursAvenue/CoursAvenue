1a543466ca3ed5e8d4c7dadec9014e7d2e5f1f53
Nima Izadi <nim.izadi@gmail.com>
2013-08-19 23:49:59 +0200

    Fixed hirefire config

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,8 +1,4 @@
 HireFire::Resource.configure do |config|
-  config.dyno(:resque_worker) do
-    HireFire::Macro::Resque.queue
-  end
-
   config.dyno(:dj_worker) do
     HireFire::Macro::Delayed::Job.queue
   end
