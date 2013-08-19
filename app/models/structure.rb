aa36d75329d7caeb134fc46b4ef67d2ee0ae2461
Nima Izadi <nim.izadi@gmail.com>
2013-08-19 20:21:09 +0200

    Fixed bug

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -56,7 +56,7 @@ class Structure < ActiveRecord::Base
                   :bank_name, :bank_iban, :bank_bic
 
   has_attached_file :image,
-                    styles:          { wide: '800x480#', thumb: '200x200#', normal: '450x' },
+                    styles:          { wide: '800x480#', thumb: '200x200#', normal: '450x' }#,
                     # path: 'assets/structures/:id/image/:fingerprint-:style.:extension'
                     #convert_options: { wide: '-interlace Line', thumb: '-interlace Line', normal: '-interlace Line' }
 
