42ae0a5f28b99e44ca93066b91f24b44e4b9ce39
Nima Izadi <nim.izadi@gmail.com>
2013-08-25 14:40:28 +0200

    fixed little things

--- app/controllers/pro/admins/sessions_controller.rb
+++ app/controllers/pro/admins/sessions_controller.rb
@@ -2,14 +2,12 @@ class Pro::Admins::SessionsController < Devise::SessionsController
   layout 'admin'
 
   def after_sign_in_path_for(admin)
-    if admin.super_admin?
-      pro_dashboard_path
+    if session['pro_admin_return_to']
+      session['pro_admin_return_to']
+    elsif admin.structure
+      dashboard_pro_structure_path(admin.structure)
     else
-      if admin.structure
-        session['pro_admin_return_to'] || dashboard_pro_structure_path(admin.structure)
-      else
-        inscription_pro_structures_path
-      end
+      inscription_pro_structures_path
     end
   end
 end
