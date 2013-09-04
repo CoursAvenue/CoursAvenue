8ddbffb29251b388f275eec05d4e4a1c8afc431b
Nima Izadi <nim.izadi@gmail.com>
2013-09-04 02:28:17 +0200

    Fixed default location

--- app/services/course_search.rb
+++ app/services/course_search.rb
@@ -49,14 +49,8 @@ class CourseSearch
 
   def self.retrieve_location params
     if params[:lat].blank? or params[:lng].blank?
-      # if request.location and request.location.longitude != 0 and request.location.latitude != 0
-      #   params[:lat] = request.location.latitude
-      #   params[:lng] = request.location.longitude
-      # else
-        # Setting paris lat & lng per default
-        params[:lat] = 48.8592
-        params[:lng] = 2.3417
-      # end
+      params[:lat] = 48.8592
+      params[:lng] = 2.3417
     end
   end
 end
