e53ad2801e78927fa85d55e8b7b561eab512858a
Nima Izadi <nim.izadi@gmail.com>
2013-09-10 12:23:54 +0200

    Changed prod rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,11 +53,6 @@ CoursAvenue::Application.configure do
   config.assets.precompile += %w( modernizr.js libs/richmarker.js email.css )
 
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'coursavenue.com' }
-  config.action_mailer.asset_host     = 'http://www.coursavenue.com'
-  config.action_controller.asset_host = "http://www.coursavenue.com"
 
   # Enable threaded mode
   # config.threadsafe!
@@ -84,6 +79,9 @@ CoursAvenue::Application.configure do
   }
 
   # ------------ Mailer configuration
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'coursavenue.com' }
   config.action_mailer.asset_host = 'http://www.coursavenue.com'
 
   config.action_mailer.smtp_settings = {
