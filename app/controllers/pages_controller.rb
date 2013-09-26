89d0b71468d3259908e906005fa92d38116f265e
Nima Izadi <nim.izadi@gmail.com>
2013-09-26 10:22:08 +0200

    Add redir and changed route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class PagesController < ApplicationController
-  def rent_places
-    @renting_room = RentingRoom.new
+  def faq_partners
+    redirect_to pro_pages_questions_url(subdomain: 'pro'), status: 301
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ CoursAvenue::Application.routes.draw do
   constraints subdomain: 'pro' do
     namespace :pro, path: '' do
       root :to => 'home#index'
-      match 'pages/pourquoi-etre-recommande' => 'home#why_be_recommended', as: 'pages_why_be_recommended'
-      match 'pages/presentation'             => 'home#presentation'
-      match 'pages/des-questions'            => 'home#questions',          as: 'pages_questions'
-      match 'pages/offre-et-tarifs'          => 'home#price',              as: 'pages_price'
-      match 'pages/nos-convictions'          => 'home#convictions',        as: 'pages_convictions'
-      match 'pages/presse'                   => 'home#press',              as: 'pages_press'
-      match '/dashboard'                     => 'dashboard#index',         as: 'dashboard'
+      match 'pages/pourquoi-etre-recommande'      => 'home#why_be_recommended', as: 'pages_why_be_recommended'
+      match 'pages/presentation'                  => 'home#presentation'
+      match 'pages/questions-les-plus-frequentes' => 'home#questions',          as: 'pages_questions'
+      match 'pages/offre-et-tarifs'               => 'home#price',              as: 'pages_price'
+      match 'pages/nos-convictions'               => 'home#convictions',        as: 'pages_convictions'
+      match 'pages/presse'                        => 'home#press',              as: 'pages_press'
+      match '/dashboard'                          => 'dashboard#index',         as: 'dashboard'
       # 301 Redirection
       match 'etablissements/demande-de-recommandations', to: 'redirect#structures_new'
       match 'inscription'                              , to: 'structures#new'
