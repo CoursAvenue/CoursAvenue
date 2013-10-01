7ecb4a294376c1dd5764abce64f4b7331ab50479
Nima Izadi <nim.izadi@gmail.com>
2013-10-01 14:15:35 +0200

    Fixed bug

--- db/migrate/20131001094644_change_type_of_facebook_url_on_structures.rb
+++ db/migrate/20131001094644_change_type_of_facebook_url_on_structures.rb
@@ -1,9 +1,9 @@
 class ChangeTypeOfFacebookUrlOnStructures < ActiveRecord::Migration
   def up
-    update_column :structures, :facebook_url, :text
+    change_column :structures, :facebook_url, :text
   end
 
   def down
-    update_column :structures, :facebook_url, :string
+    change_column :structures, :facebook_url, :string
   end
 end
