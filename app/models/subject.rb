37388adce74deea92d56e9f8474bd0ae84e7b403
Nima Izadi <nim.izadi@gmail.com>
2013-10-02 18:39:20 +0200

    Fixed children bug

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -16,7 +16,7 @@ class Subject < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: {scope: 'ancestry'}
 
-  scope :children,               where{ancestry != nil}
+  scope :children,               where{ancestry_depth == 2}
   scope :roots_with_position,    where{(ancestry == nil) & (position != nil)}
   scope :roots_without_position, where{(ancestry == nil) & (position == nil)}
 
