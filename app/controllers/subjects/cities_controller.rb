d1492c119dee8e12d8ba52d3da1c8af02929ca69
Nima Izadi <nim.izadi@gmail.com>
2013-10-15 21:52:58 +0200

    Fixed cache

--- app/controllers/subjects/cities_controller.rb
+++ app/controllers/subjects/cities_controller.rb
@@ -15,13 +15,6 @@ class Subjects::CitiesController < ApplicationController
     @structures = @structure_search.results
     @locations  = @structures.map{|s| s.locations_around(@city.latitude, @city.longitude, 4) }
 
-    # @lesson_plannings, @workshop_plannings, @training_plannings = [], [], []
-    # @structures[0..50].each do |structure|
-    #   @lesson_plannings   += structure.courses.lessons.map(&:plannings).flatten
-    #   @workshop_plannings += structure.courses.workshops.map(&:plannings).flatten
-    #   @training_plannings += structure.courses.trainings.map(&:plannings).flatten
-    # end
-    # @plannings  = @lesson_plannings + @workshop_plannings + @training_plannings
     @plannings = @structures.map{|structure| structure.courses.map(&:plannings).flatten}.flatten
 
     @medias     = @structures.collect(&:medias).flatten
