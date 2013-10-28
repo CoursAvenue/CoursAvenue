94597b8c45f01ab89a175ecd26b522ab4d7762fd
Nima Izadi <nim.izadi@gmail.com>
2013-10-28 12:29:11 +0100

    Fixed .slug on nil current_user in conversation controller

--- app/controllers/users/conversations_controller.rb
+++ app/controllers/users/conversations_controller.rb
@@ -12,7 +12,7 @@ class Users::ConversationsController < ApplicationController
     respond_to do |format|
       if !current_user and !@user.reset_password_token_valid?(params[:token])
         format.html { redirect_to root_path, alert: 'Vous ne pouvez pas visualiser cette page' }
-      elsif current_user.slug != params[:user_id]
+      elsif current_user and current_user.slug != params[:user_id]
         format.html { redirect_to user_conversation_path(current_user, @conversation)}
       else
         format.html
