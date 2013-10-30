573110ac8b53916c0bea7da567a6270c4c7b3e7e
variousauthors <various.authors@gmail.com>
2013-10-30 12:46:43 +0100

    Structure Item -- Showable
    Made the individual review snippets clickable, and added a useful URL
    to the serializer.

--- app/assets/javascripts/backbone/views/comment_view.js
+++ app/assets/javascripts/backbone/views/comment_view.js
@@ -4,7 +4,16 @@ FilteredSearch.module('Views', function(Views, App, Backbone, Marionette, $, _) 
     /* views here temporarily to get this all all started */
     Views.CommentView = Backbone.Marionette.ItemView.extend({
         template: "backbone/templates/comment_view",
-        tagName: "li"
+        tagName: "li",
+
+        events: {
+          'click': 'handleClick'
+        },
+
+        handleClick: function () {
+          console.log("Oh, we handled that.");
+
+        }
     });
 
 });
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -3,12 +3,17 @@ class CommentSerializer < ActiveModel::Serializer
   include ActionView::Helpers::UrlHelper
   include TruncateHtmlHelper
 
-  attributes :id, :content, :title, :author_name, :course_name, :created_at
+  attributes :id, :content, :title, :author_name, :course_name, :created_at, :comment_url, :rating
 
   def content
     # TODO there is a problem with UrlHelper in Rails 4 that breaks this
-    # omission = link_to('Lire la suite →', structure_url(object.commentable_id, anchor: "recommantation-#{object.id}"))
-    truncate_html(object.content, length: 100, omission: "...").html_safe
+    # omission = link_to('Lire la suite →', structure_url(object.commentable_id, anchor: "recommandation-#{object.id}"))
+    truncate_html(object.content, length: 60, omission: "...").html_safe
+  end
+
+  # while link_to and such aren't working, we will just use this
+  def comment_url
+    "#{object.structure.slug}#recommandation-#{object.id}"
   end
 
 end
