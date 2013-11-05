9011b3aac86dba08876510c816cc7d2435a825c8
variousauthors <various.authors@gmail.com>
2013-11-05 15:36:06 +0100

    Search page - header filters
    Implemented resetCategoricalFilterTool event responder, and put the data
    into the header filters.

--- app/assets/javascripts/backbone/views/categorical_filter_view.js
+++ app/assets/javascripts/backbone/views/categorical_filter_view.js
@@ -3,8 +3,15 @@ FilteredSearch.module('Views', function(Views, App, Backbone, Marionette, $, _) 
     Views.CategoricalFilterView = Backbone.Marionette.ItemView.extend({
         template: 'backbone/templates/categorical_filter_view',
 
-        resetCategoricalFilterTool: function () {
+        /* data to describe the pagination tool */
+        resetCategoricalFilterTool: function (data) {
+            this.current_summary_data = data;
 
-        }
+            this.render();
+        },
+
+        serializeData: function (data) {
+            return this.current_summary_data;
+        },
     });
 });
