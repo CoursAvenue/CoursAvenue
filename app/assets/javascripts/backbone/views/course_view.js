1b16b253ee74bae4d05729514c3313cc44265e73
Nima Izadi <nim.izadi@gmail.com>
2013-11-06 10:21:34 +0100

    Improved design on table courses

--- app/assets/javascripts/backbone/views/course_view.js
+++ app/assets/javascripts/backbone/views/course_view.js
@@ -3,9 +3,9 @@ FilteredSearch.module('Views', function(Views, App, Backbone, Marionette, $, _) 
 
     /* views here temporarily to get this all all started */
     Views.CourseView = Backbone.Marionette.ItemView.extend({
-        template: "backbone/templates/course_view",
-        tagName: "table",
-        className: "structure-item__plannings-table white-box",
+        template:  "backbone/templates/course_view",
+        tagName:   "table",
+        className: "white-box table--striped table--data push-half--bottom",
 
         events: {
           'click': 'handleClick'
--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,11 +1,19 @@
 class CourseSerializer < ActiveModel::Serializer
   include CoursesHelper
 
-  attributes :id, :name, :type, :start_date, :end_date, :price_range
+  attributes :id, :name, :type, :start_date, :end_date, :price_range, :min_price_amount, :min_price_libelle
   has_many :plannings
 
   def price_range
     plain_price_range(object)
+  end
+
+  def min_price_amount
+    object.best_price.amount.to_i if object.best_price
+  end
+
+  def min_price_libelle
+    object.best_price.localized_libelle.downcase if object.best_price
   end
 
   def type
@@ -18,5 +26,9 @@ class CourseSerializer < ActiveModel::Serializer
 
   def end_date
     I18n.l(object.end_date, format: :short) if object.end_date
+  end
+
+  def data_url
+    structure_course_path(object.structure, object)
   end
 end
