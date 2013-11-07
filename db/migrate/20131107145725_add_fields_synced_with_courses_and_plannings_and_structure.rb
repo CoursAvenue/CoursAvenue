ca4eaaf31c180bfa4677485812968bc3923c9971
Nima Izadi <nim.izadi@gmail.com>
2013-11-07 19:06:51 +0100

    Fixed migrations

--- db/migrate/20131107145725_add_fields_synced_with_courses_and_plannings_and_structure.rb
+++ db/migrate/20131107145725_add_fields_synced_with_courses_and_plannings_and_structure.rb
@@ -3,10 +3,16 @@ class AddFieldsSyncedWithCoursesAndPlanningsAndStructure < ActiveRecord::Migrati
     add_column :structures, :audience_ids,             :string
     add_column :structures, :gives_group_courses,      :boolean
     add_column :structures, :gives_individual_courses, :boolean
-    bar = ProgressBar.new Structure.count
+    bar = ProgressBar.new Structure.count + Course.count
     Structure.find_each do |s|
       bar.increment!
       s.send(:update_synced_attributes)
+    end
+
+    Course.find_each(batch_size: 100) do |course|
+      bar.increment!
+      course.send(:set_teaches_at_home)
+      course.save(validate: false)
     end
   end
 end
