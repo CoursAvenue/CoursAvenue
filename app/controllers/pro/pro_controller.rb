880ffe865255f32549f05cd31d145c032c719682
Nima Izadi <nim.izadi@gmail.com>
2013-11-10 16:55:47 +0100

    Fixed bug

--- app/controllers/pro/pro_controller.rb
+++ app/controllers/pro/pro_controller.rb
@@ -2,7 +2,7 @@ class Pro::ProController < ApplicationController
   layout 'admin'
 
   def authenticate_pro_super_admin!
-    unless current_user.super_admin?
+    unless current_pro_admin.super_admin?
       redirect_to root_path, alert: "Vous n'avez pas le droit !"
     end
   end
