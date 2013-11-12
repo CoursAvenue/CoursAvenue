f7a7977c64841c015909a610f783e4851db610c8
Nima Izadi <nim.izadi@gmail.com>
2013-11-12 12:57:48 +0100

    Add sticker demand online

--- app/models/sticker_demand.rb
+++ app/models/sticker_demand.rb
@@ -1,6 +1,14 @@
 class StickerDemand < ActiveRecord::Base
   belongs_to :structure
 
-  attr_accessible :round_number, :square_number, :sent
+  attr_accessible :round_number, :square_number, :address, :sent
+  validate :at_least_one_sticker, on: :create
 
+  private
+
+  def at_least_one_sticker
+    if self.round_number == 0 and self.square_number == 0
+      self.errors[:base] << I18n.t('sticker_demands.errors.at_least_one_sticker')
+    end
+  end
 end
