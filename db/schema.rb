2f03384eef2c7f83460a885954e61b9911f49e8a
Nima Izadi <nim.izadi@gmail.com>
2013-11-14 22:49:01 +0100

    Remove coaching sportif

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112101443) do
+ActiveRecord::Schema.define(version: 20131114214332) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "resource_id",   null: false
