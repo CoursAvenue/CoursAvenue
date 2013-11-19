e7827344b394cf7efa19f18aaeb2f7dcc732dcdf
variousauthors <various.authors@gmail.com>
2013-11-19 15:42:27 +0100

    Marker Bounce
    Added plannings_view. Course_view is now a composite view.

--- app/assets/javascripts/backbone/views/course_view.js
+++ app/assets/javascripts/backbone/views/course_view.js
@@ -2,12 +2,23 @@
 FilteredSearch.module('Views', function(Views, App, Backbone, Marionette, $, _) {
 
     /* views here temporarily to get this all all started */
-    Views.CourseView = Backbone.Marionette.ItemView.extend({
+    Views.CourseView = Backbone.Marionette.CompositeView.extend({
         template:  "backbone/templates/course_view",
         className: "push-half--top soft-half--top bordered--top",
+        itemView: Backbone.Marionette.ItemView.extend({
+            template: "backbone/templates/plannings_view",
+            tagName: 'tr',
+            attributes: {
+                'data-type': 'line-item'
+            }
+        }),
+        itemViewContainer: 'tbody',
 
         initialize: function(options){
             this.index = options.index;
+            this.collection = new Backbone.Collection(_.map(options.model.get("plannings"), function (data) {
+                return new Backbone.Model(data);
+            }));
         },
 
         onRender: function() {
