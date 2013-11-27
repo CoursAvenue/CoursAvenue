e8885bd20eb633e3bb993e0177c52ca7a0fb42a9
variousauthors <various.authors@gmail.com>
2013-11-27 11:35:26 +0100

    Moved 'lib' namespace to the top level

--- app/assets/javascripts/backbone/cours_avenue.js
+++ app/assets/javascripts/backbone/cours_avenue.js
@@ -1,5 +1,6 @@
 //= require ./core_ext
-
+//
+//= require ./lib/manifest
 //= require ./filtered_search/manifest
 //= require ./user_management/manifest
 
---
+++ app/assets/javascripts/backbone/lib/manifest.js
@@ -0,0 +1,3 @@
+
+//= require ./models/manifest
+//= require ./views/manifest
