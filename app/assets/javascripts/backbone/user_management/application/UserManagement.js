76921d163d2a0a6a0c0b44de0c434cb850a7ae9d
Nima Izadi <nim.izadi@gmail.com>
2013-12-03 15:19:35 +0100

    Refactored user management app

--- app/assets/javascripts/backbone/user_management/application/UserManagement.js
+++ app/assets/javascripts/backbone/user_management/application/UserManagement.js
@@ -1,40 +1,4 @@
-UserManagement = (function (){
-    var self = new Backbone.Marionette.Application({
-        slug: 'user-management',
-
-        /* for use in query strings */
-        root:   function() { return self.slug + '-root'; },
-        loader: function() { return self.slug + '-loader'; },
-
-        /* methods for returning the relevant jQuery collections */
-        $root: function() {
-            return $('[data-type=' + self.root() + ']');
-        },
-
-        /* Return the element in which the application will be appended */
-        $loader: function() {
-            return $('[data-type=' + self.loader() + ']');
-        },
-
-        /* A filteredSearch should only start if it detects
-         * an element whose data-type is the same as its
-         * root property.
-         * @throw the root was found to be non-unique on the page */
-        detectRoot: function() {
-            var result = self.$root().length;
-
-            if (result > 1) {
-                throw {
-                    message: 'UserManagement->detectRoot: ' + self.root() + ' element should be unique'
-                }
-            }
-
-            return result > 0;
-        },
-    });
-
-    return self;
-}());
+UserManagement = new Backbone.Marionette.Application({ slug: 'user-management' });
 
 UserManagement.addRegions({
     mainRegion: '#' + UserManagement.slug
