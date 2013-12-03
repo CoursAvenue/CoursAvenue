55003252ec16b88d92bdbf662440301720514979
Nima Izadi <nim.izadi@gmail.com>
2013-12-03 18:20:03 +0100

    Edit in place

--- app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
+++ app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
@@ -4,6 +4,16 @@ UserManagement.module('Views.UserProfilesCollection.UserProfile', function(Modul
 
     Module.UserProfileView = Backbone.Marionette.ItemView.extend({
         template: Module.templateDirname() + 'user_profile_view',
-        tagName: 'tr'
+        tagName: 'tr',
+
+        onRender: function () {
+            this.$('[data-behavior=editable]').editable();
+            this.$('[data-behavior=editable]').on('save', function(){
+                var that = this;
+                setTimeout(function() {
+                    $(that).closest('td').next().find('[data-behavior=editable]').editable('show');
+                }, 200);
+            });
+        }
     });
 });
--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -2,5 +2,13 @@ class UserProfileSerializer < ActiveModel::Serializer
 
   has_many :tags
 
-  attributes :email, :first_name, :last_name, :birthdate, :notes, :phone, :mobile_phone, :address
+  attributes :id, :email, :first_name, :last_name, :birthdate, :notes, :phone, :mobile_phone, :address, :path, :tag_name
+
+  def path
+    pro_structure_user_profile_path(object.structure, object)
+  end
+
+  def tag_name
+    object.tags.map(&:name).join(', ')
+  end
 end
