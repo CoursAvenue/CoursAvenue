7ce1384a8cb767d94087917666705abd7ab31971
Nima Izadi <nim.izadi@gmail.com>
2013-12-04 16:34:33 +0100

    Adding editing stuff wip

--- app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
+++ app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
@@ -6,6 +6,25 @@ UserManagement.module('Views.UserProfilesCollection.UserProfile', function(Modul
         template: Module.templateDirname() + 'user_profile_view',
         tagName: 'tr',
 
+        events: {
+            'data-behavior="more-detail"': 'edit'
+        },
+
+        edit: function() {
+            debugger
+            var lala = $.fancybox({href: ''}, {
+                    openSpeed   : 300,
+                    maxWidth    : 800,
+                    maxHeight   : 500,
+                    fitToView   : false,
+                    width       : width,
+                    height      : height,
+                    autoSize    : false,
+                    autoResize  : true
+            });
+
+        },
+
         onRender: function () {
             this.$('[data-behavior=editable]').editable();
             this.$('[data-behavior=editable]').on('save', function(){
