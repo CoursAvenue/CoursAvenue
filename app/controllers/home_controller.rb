81fc3acab8e3ce038b546b8f158ff4f15af691ff
Nima Izadi <nim.izadi@gmail.com>
2013-12-08 12:00:52 +0100

    Add videos on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,22 +6,6 @@ class HomeController < ApplicationController
     @audiences        = Audience.all
     @levels           = Level.all
     @comments         = Comment.accepted.order('created_at DESC').limit(5)
-    @homepage_images  = [['home-page/dance.jpg', 'Cours de danse']]
-                         # ,['home-page/painter.jpg', 'Cours de peinture']]
-    @structures = StructureSearch.search({lat: 48.8540,
-                                          lng: 2.3417,
-                                          radius: 5,
-                                          sort: 'rating_desc',
-                                          has_logo: true,
-                                          per_page: 130,
-                                          bbox: true
-                                        }).results
-
-    @json_locations_addresses = Gmaps4rails.build_markers(@structures) do |structure, marker|
-      marker.lat structure.latitude
-      marker.lng structure.longitude
-    end
-
   end
 
   def contact
