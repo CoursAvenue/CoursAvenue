cb2e96594a6708f1c3eddfbcbac4fa6b4c89c0f2
Nima Izadi <nim.izadi@gmail.com>
2013-12-10 11:38:19 +0100

    Add share buttons to facebook and twitter

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,18 @@
 # encoding: utf-8
 module CommentsHelper
 
+  def structure_comment_url comment
+    structure_url(comment.structure, anchor: "recommandation-#{comment.id}", subdomain: 'www')
+  end
+
+  def share_comment_url comment, provider=:facebook
+    case provider
+    when :facebook
+      "http://www.facebook.com/sharer.php?s=100&p[title]=#{comment.title}&p[url]=#{URI.encode(structure_comment_url(comment))}&p[summary]=#{truncate(comment.content, length: 200)}"
+    when :twitter
+      "https://twitter.com/intent/tweet?text=#{comment.title}&via=CoursAvenue&url=#{URI.encode(structure_comment_url(comment))}"
+    end
+  end
   def comment_rating_title rating
     rating = rating.to_i
     case rating
