1e574cc2d6d26d6fba84677a5ee3659ac170df2a
variousauthors <various.authors@gmail.com>
2013-12-17 17:12:12 +0100

    CRM -- Click to edit
    When clicked, fields become inputs.

--- app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
+++ app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
@@ -6,33 +6,52 @@ UserManagement.module('Views.UserProfilesCollection.UserProfile', function(Modul
         template: Module.templateDirname() + 'user_profile_view',
         tagName: 'tr',
 
+        ui: {
+            '$editable': "[data-behavior=editable]:not(:has('input'))",
+            '$editing' : "[data-behavior=editable]:has('input')"
+        },
+
         events: {
-            'data-behavior="more-detail"': 'edit'
+            'click @ui.$editable': 'startEditing',
         },
 
-        edit: function() {
-            debugger
-            var lala = $.fancybox({href: ''}, {
-                    openSpeed   : 300,
-                    maxWidth    : 800,
-                    maxHeight   : 500,
-                    fitToView   : false,
-                    width       : width,
-                    height      : height,
-                    autoSize    : false,
-                    autoResize  : true
-            });
+        finishEditing: function (e) {
+            if (e.target.tagName === "INPUT") {
+                return;
+            }
 
+            var $field = $(e.currentTarget);
+            var $input = $field.find("input");
+
+            text = $input.prop('value');
+            $data = $("<div>").text(text);
+
+            $field.html($data);
+            $field.off('click', this.finishEditing);
+            e.stopPropagation();
         },
 
-        onRender: function () {
-            this.$('[data-behavior=editable]').editable();
-            this.$('[data-behavior=editable]').on('save', function(){
-                var that = this;
-                setTimeout(function() {
-                    $(that).closest('td').next().find('[data-behavior=editable]').editable('show');
-                }, 200);
-            });
+        startEditing: function (e) {
+            // stop editing other fields
+            $(this.ui.$editing.selector).click();
+
+            var $field = $(e.currentTarget);
+
+            text = $field.text();
+            $input = $("<input>").prop("value", text);
+
+            $field.html($input);
+            $field.on('click', this.finishEditing);
         }
+
+//      onRender: function () {
+//          this.$('[data-behavior=editable]').editable();
+//          this.$('[data-behavior=editable]').on('save', function(){
+//              var that = this;
+//              setTimeout(function() {
+//                  $(that).closest('td').next().find('[data-behavior=editable]').editable('show');
+//              }, 200);
+//          });
+//      }
     });
 });
