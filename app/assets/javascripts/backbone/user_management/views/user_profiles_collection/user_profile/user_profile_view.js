18dcebf885505641212961245568f3964be49597
variousauthors <various.authors@gmail.com>
2013-12-19 18:42:07 +0100

    Switched to row-at-a-time editing
    This makes more sense: you click on a field and get the whole row.
    When you click off, your edits get commit.

--- app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
+++ app/assets/javascripts/backbone/user_management/views/user_profiles_collection/user_profile/user_profile_view.js
@@ -1,3 +1,6 @@
+/* TODO consider making every field on the itemview editable on click, rather than one
+*  field at a time. This shows "we are editing this model", and lets us put the "commit"
+*  button in a consistent place. */
 
 /* just a basic marionette view */
 UserManagement.module('Views.UserProfilesCollection.UserProfile', function(Module, App, Backbone, Marionette, $, _) {
@@ -20,9 +23,28 @@ UserManagement.module('Views.UserProfilesCollection.UserProfile', function(Modul
         },
 
         events: {
-            'click @ui.$editable'                  : 'startEditing',
-            'blur  [data-behavior=editable] input' : 'finishEditing',
-            'keydown'                              : 'handleKeyDown'
+            'click @ui.$editable' : 'startEditing',
+            'focusout'            : 'handleBlur',
+            'keydown'             : 'handleKeyDown'
+        },
+
+        /* the row has lost focus */
+        handleBlur: function (e) {
+            var self = this;
+            /* a hack to determine whether it was the row, or a field
+            *  that triggered the blur event
+            *  see: http://stackoverflow.com/questions/121499/when-onblur-occurs-how-can-i-find-out-which-element-focus-went-to */
+            setTimeout(_.bind(function() {
+                var $target = $(document.activeElement);
+
+                /* if focus is moving within the row, NOP */
+                if (this.$el.find($target).length > 0) {
+                    return;
+                }
+
+                this.finishEditing();
+
+            }, this), 1);
         },
 
         startEditing: function (e) {
@@ -31,40 +53,51 @@ UserManagement.module('Views.UserProfilesCollection.UserProfile', function(Modul
                 return;
             }
 
-            var $field    = $(e.currentTarget);
-            var text = $field.text();
-            console.log("html: %o", text);
-            var $input = $("<input>").prop("value", text);
+            var $fields    = this.ui.$editable;
 
-            $field.html($input);
-            $input.focus();
+            _.each($fields, function (field) {
+                var $field = $(field);
+                var text = $field.text();
+                var $input = $("<input>").prop("value", text);
+
+                $field.html($input);
+            });
+
+            var $current     = $(e.currentTarget).find("input");
+            $current.focus();
         },
 
         /* given a $field, replace that $field's contents with text */
         finishEditing: function (e) {
-            var $input    = $(e.currentTarget);
-            var $field    = $(this.ui.$editing.selector);
-            var attribute = $field.data("name");
-            var text = $input.val();
-            $field.html(text);
+            var $fields   = this.$(this.ui.$editing.selector);
+            var update    = {
+                user_profile: { }
+            };
 
-            console.log("saving: %o", text);
-            if (text !== this.model.get(attribute)) {
-                var data = { user_profile: { }};
-                data.user_profile[attribute] = text;
-                this.model.save(data, {
-                    success: function (model, response) {
-                        console.log(model);
-                        console.log(response);
-                    },
-                    error: function (model, response) {
-                        GLOBAL.flash(response.responseJSON.errors.join("\n"), "error");
-                        console.log(model);
-                        console.log(response);
-                    },
-                    wait: true
-                });
-            }
+            _.each($fields, function (field) {
+                var $field    = $(field);
+                var $input    = $field.find("input");
+                var attribute = $field.data("name");
+                var text      = $input.val();
+
+                $field.html(text);
+                update.user_profile[attribute] = text;
+            });
+
+            this.model.save(update, {
+                success: function (model, response) {
+                    console.log(model);
+                    console.log(response);
+                },
+                error: function (model, response) {
+                    GLOBAL.flash(response.responseJSON.errors.join("\n"), "error");
+                    console.log(model);
+                    console.log(response);
+                    /* display a flash containing the error message */
+                    /* persist the user's changes */
+                },
+                wait: true
+            });
         },
 
         handleKeyDown: function (e) {
@@ -73,7 +106,7 @@ UserManagement.module('Views.UserProfilesCollection.UserProfile', function(Modul
                 return;
             }
 
-            var $field    = $(this.ui.$editing.selector);
+            var $field    = this.$(this.ui.$editing.selector);
             var attribute = $field.data("name");
             var $input    = $field.find("input");
             var text      = $input.prop('value') || null;
