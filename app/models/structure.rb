9f07cb5f60f39b137affedfd17a401c3bcf9cf18
Nima Izadi <nim.izadi@gmail.com>
2014-01-10 17:15:09 +0100

    Fixed bugs

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -84,7 +84,7 @@ class Structure < ActiveRecord::Base
   has_many :cities                    , through: :places
   has_many :prices                    , through: :courses
   has_many :reservations,         as: :reservable
-  has_many :comment_notifications
+  has_many :comment_notifications     , dependent: :destroy
   has_many :sticker_demands           , dependent: :destroy
 
   has_and_belongs_to_many :subjects
