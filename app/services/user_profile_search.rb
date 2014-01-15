0bed72bf38d95f2593dac6fbd6dc2c0e5db5963d
Nima Izadi <nim.izadi@gmail.com>
2014-01-15 18:33:00 +0100

    Re added params for sorting

--- app/services/user_profile_search.rb
+++ app/services/user_profile_search.rb
@@ -6,9 +6,10 @@ class UserProfileSearch
   def self.search params
     @search = Sunspot.search(UserProfile) do
       fulltext params[:name]                         if params[:name].present?
-      order_by :first_name, :desc
 
       with :structure_id, params[:structure_id].to_i if params[:structure_id]
+
+      order_by(params[:sort].to_sym, params[:order].to_sym) if params[:sort] and params[:order]
 
       paginate page: (params[:page] || 1), per_page: (params[:per_page] || 30)
     end
