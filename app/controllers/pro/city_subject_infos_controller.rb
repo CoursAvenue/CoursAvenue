a21e57b9316e2a1e19a66fe417f217cb598354aa
Nima Izadi <nim.izadi@gmail.com>
2014-01-16 16:52:35 +0100

    Fixed city subject info bug

--- app/controllers/pro/city_subject_infos_controller.rb
+++ app/controllers/pro/city_subject_infos_controller.rb
@@ -15,11 +15,11 @@ class Pro::CitySubjectInfosController < Pro::ProController
 
 
   def create
-    @city      = City.find params[:city_subject_info][:city_id]
-    @subject   = Subject.find params[:city_subject_info][:subject_id]
-    city_id    = @city.id
-    subject_id = @subject.id
-    if (@city_subject_info = CitySubjectInfo.where{(city_id == city_id) & (subject_id == subject_id)}.first).nil?
+    @city       = City.find params[:city_subject_info][:city_id]
+    @subject    = Subject.find params[:city_subject_info][:subject_id]
+    _city_id    = @city.id
+    _subject_id = @subject.id
+    if (@city_subject_info = CitySubjectInfo.where{(city_id == _city_id) & (subject_id == _subject_id)}.first).nil?
       @city_subject_info = CitySubjectInfo.new(city_id: @city.id, subject_id: @subject.id)
     end
     @city_subject_info.update_attributes params[:city_subject_info]
