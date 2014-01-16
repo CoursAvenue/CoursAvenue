e5e36a8b3ca4df059e0481c1568e581417c6aaaf
Nima Izadi <nim.izadi@gmail.com>
2014-01-16 11:56:55 +0100

    Add fields for vertical pages

--- app/models/city_subject_info.rb
+++ app/models/city_subject_info.rb
@@ -2,6 +2,7 @@ class CitySubjectInfo < ActiveRecord::Base
   belongs_to :city
   belongs_to :subject
 
-  attr_accessible :title, :description, :city_id, :subject_id
+  attr_accessible :title, :description, :city_id, :subject_id,
+                  :where_to_practice, :where_to_suit_up, :average_price, :tips
 
 end
