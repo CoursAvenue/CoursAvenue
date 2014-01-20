63a3770a9cd0767c56bb090dabc3bdffe6e1463d
variousauthors <various.authors@gmail.com>
2014-01-20 11:04:03 +0100

    Added specs for create user_profiles

--- spec/controllers/pro/structures/user_profiles_controller_spec.rb
+++ spec/controllers/pro/structures/user_profiles_controller_spec.rb
@@ -40,18 +40,42 @@ describe Pro::Structures::UserProfilesController do
 
   end
 
+  describe :create do
+
+    it "does not create tags if no tags are given" do
+      post :create, format: :json, id: user_profile.id, structure_id: structure.id, user_profile: { email: "bob@email.com" }
+
+      expect(JSON.parse(response.body)).to include("tags" => [])
+    end
+
+    it "responds with a CSV of the tags in tag_name" do
+      post :create, format: :json, id: user_profile.id, structure_id: structure.id, user_profile: { email: "bob@email.com", tags: ["this", "that"] }
+
+      tag_name = JSON.parse(response.body)["tags"].map { |h| h["name"] }.join(", ")
+      expect(JSON.parse(response.body)).to include("tag_name" => tag_name)
+    end
+
+    it "creates a profile with tags if given" do
+      post :create, format: :json, id: user_profile.id, structure_id: structure.id, user_profile: { email: "bob@email.com", tags: ["this", "that"] }
+
+      tags = JSON.parse(response.body)["tags"].map { |h| h["name"] }
+      expect(tags).to match_array(["this", "that"])
+    end
+
+  end
+
   describe :update do
     it "does not change the tags if no tags are given" do
       put :update, format: :json, id: user_profile.id, structure_id: structure.id, user_profile: { }
 
-      expect(JSON.parse(response.body)).to include("tags" => []);
+      expect(JSON.parse(response.body)).to include("tags" => [])
     end
 
     it "updates tags if applicable" do
       put :update, format: :json, id: user_profile.id, structure_id: structure.id, user_profile: { tags: ["this", "that"] }
 
-      tag_names = JSON.parse(response.body)["tags"].map { |h| h["name"] }
-      expect(tag_names).to match_array(["this", "that"]);
+      tags = JSON.parse(response.body)["tags"].map { |h| h["name"] }
+      expect(tags).to match_array(["this", "that"])
     end
   end
 
