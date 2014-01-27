5c627be122db8ce5665365abcd7ed4cd895aa534
Nima Izadi <nim.izadi@gmail.com>
2014-01-27 15:48:52 +0100

    Added add to mailchimp

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -14,6 +14,7 @@ class Structure < ActiveRecord::Base
   after_validation :geocode
 
   after_save :delay_subscribe_to_nutshell
+  after_save :delay_subscribe_to_mailchimp
 
   STRUCTURE_STATUS        = %w(SA SAS SASU EURL SARL)
   STRUCTURE_TYPES         = ['structures.company',
@@ -503,10 +504,6 @@ class Structure < ActiveRecord::Base
     end
   end
 
-  def delay_subscribe_to_nutshell
-    self.delay.subscribe_to_nutshell if Rails.env.production?
-  end
-
   def parisian?
     paris_lat    = 48.8592
     paris_lng    = 2.3417
@@ -549,8 +546,12 @@ class Structure < ActiveRecord::Base
     self.active = true
   end
 
-  def subscribe_to_nutshell
-    NutshellUpdater.update(self)
+  def delay_subscribe_to_mailchimp
+    MailchimpUpdater.delay.update(self) if self.main_contact and Rails.env.production?
+  end
+
+  def delay_subscribe_to_nutshell
+    NutshellUpdater.delay.update(self) if self.main_contact and Rails.env.production?
   end
 
   def fix_website_url
--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
-Gibbon::API.api_key = '1e3947447b4fefc19334286d6d32a488-us7'
+Gibbon::API.api_key = '84c005b1d30790d3bcc76183bb5f464f-us3'
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
