4a7e66ae58581d9aa6ccfa7820e10aa1a25e4f91
Nima Izadi <nim.izadi@gmail.com>
2014-01-31 20:25:40 +0100

    Test migration

--- db/migrate/20140129093528_create_default_tags_for_user_profiles.rb
+++ db/migrate/20140129093528_create_default_tags_for_user_profiles.rb
@@ -6,16 +6,16 @@ class CreateDefaultTagsForUserProfiles < ActiveRecord::Migration
       bar.increment!
       next unless structure.main_contact
       structure.user_profiles.find_each do |user_profile|
-        user_profile_email      = user_profile.email
-        user_profile_name       = user_profile.first_name
+        user_profile_email        = user_profile.email
+        user_profile_name         = user_profile.first_name
         if user_profile_name.present?
           user_profile.first_name = user_profile_name.split(' ')[0..-2].join(' ')
           user_profile.last_name  = user_profile_name.split(' ').last if user_profile_name.split(' ').length > 1
           user_profile.save
         end
-        tags = []
-        tags << UserProfile::DEFAULT_TAGS[:comments] if structure.comments.where{email == user_profile_email}
-        structure.tag(user_profile, with: tags.join(','), on: :tags) if tags.present?
+        if structure.comments.where{email == user_profile_email}
+          structure.tag(user_profile, with: UserProfile::DEFAULT_TAGS[:comments], on: :tags, skip_save: true)
+        end
       end
     end
   end
