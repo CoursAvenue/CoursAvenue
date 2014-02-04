4086f5738c779e8722aefe12c5d134e32ac1fb40
variousauthors <various.authors@gmail.com>
2014-02-04 19:01:10 +0100

    Wrote specs for structures_controller#index

--- spec/controllers/structures_controller_spec.rb
+++ spec/controllers/structures_controller_spec.rb
@@ -28,6 +28,7 @@ describe StructuresController do
 
   describe :index, search: true do
     let(:structure) { FactoryGirl.build(:structure_with_place) }
+    let(:subject) { Subject.first }
 
     before do
       @structure = FactoryGirl.create(:structure_with_place)
@@ -50,6 +51,29 @@ describe StructuresController do
       result.keys.should include('meta')
       assigns(:structure_search).total.should eq(result['meta']['total'])
     end
+
+    it "sets params[:other] when subject_id is 'other'" do
+      get :index, format: :json, lat: 48.8592, lng: 2.3417, subject_id: 'other'
+
+      expect(controller.params[:other]).to be_true
+      expect(controller.params).not_to have_key(:subject_id)
+    end
+
+    it "correctly finds the subject if subject_id is provided" do
+      get :index, format: :json, lat: 48.8592, lng: 2.3417, subject_id: subject.slug
+
+      expect(assigns(:subject)).to eq(subject)
+      expect(controller.params).to have_key(:subject_id)
+    end
+
+    it "guesses the subject if params[:name] matches any subject" do
+      get :index, format: :json, lat: 48.8592, lng: 2.3417, name: subject.name
+
+      expect(assigns(:subject)).to eq(subject)
+    end
+
+    # @pending the subbing for solr doesn't recognize 'group'
+    it "searches per planning when given the appropriate filters"
   end
 
   describe :show do
