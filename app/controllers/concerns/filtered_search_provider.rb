a2375f90e7196dc486b615790a8d4076efbc4a94
variousauthors <various.authors@gmail.com>
2014-02-05 11:21:25 +0100

    Added search concern to structures_controller

---
+++ app/controllers/concerns/filtered_search_provider.rb
@@ -0,0 +1,60 @@
+module FilteredSearchProvider extend ActiveSupport::Concern
+
+  PLANNING_FILTERED_KEYS = ['audience_ids', 'level_ids', 'min_age_for_kids', 'max_price', 'min_price',
+                            'price_type', 'max_age_for_kids', 'trial_course_amount', 'course_types',
+                            'week_days', 'discount_types', 'start_date', 'end_date', 'start_hour', 'end_hour']
+
+  # if the subject_id is set, we return a subject
+  # otherwise just nil
+  def filter_by_subject?
+    params.delete(:subject_id) if params[:subject_id].blank?
+    if params[:subject_id] == 'other'
+      params[:other] = true
+      params.delete(:subject_id)
+    end
+
+    if params[:subject_id].present?
+      Subject.friendly.find params[:subject_id]
+    else
+      # Little hack to determine if the name is equal a subject
+      _name = params[:name]
+      if _name.present? and Subject.where{name =~ _name}.any?
+        Subject.where{name =~ _name}.first
+      end
+    end
+  end
+
+  def params_has_planning_filters?
+    (params.keys & PLANNING_FILTERED_KEYS).any?
+  end
+
+  def search_plannings
+    search          = PlanningSearch.search(params, group: :structure_id_str)
+    structures      = search.group(:structure_id_str).groups.collect do |planning_group|
+      planning_group.results.first.structure
+    end
+    total           = search.group(:structure_id_str).total
+
+    [ structures, total ]
+  end
+
+  def search_structures
+    search           = StructureSearch.search(params)
+    structures       = search.results
+    total            = search.total
+
+    [ structures, total ]
+  end
+
+  def retrieve_location
+    StructureSearch.retrieve_location(params)
+  end
+
+  def jasonify(structures)
+    structures.map do |structure|
+      StructureSerializer.new(structure, { root: false })
+    end
+  end
+
+
+end
--- app/controllers/structures_controller.rb
+++ app/controllers/structures_controller.rb
@@ -1,11 +1,8 @@
 # encoding: utf-8
 class StructuresController < ApplicationController
+  include FilteredSearchProvider
 
   respond_to :json
-
-  PLANNING_FILTERED_KEYS = ['audience_ids', 'level_ids', 'min_age_for_kids', 'max_price', 'min_price',
-                            'price_type', 'max_age_for_kids', 'trial_course_amount', 'course_types',
-                            'week_days', 'discount_types', 'start_date', 'end_date', 'start_hour', 'end_hour']
 
   layout :choose_layout
 
@@ -27,61 +24,11 @@ class StructuresController < ApplicationController
     index           = 0
   end
 
-  def filter_by_subject?
-    params.delete(:subject_id) if params[:subject_id].blank?
-    if params[:subject_id] == 'other'
-      params[:other] = true
-      params.delete(:subject_id)
-    elsif params[:subject_id].present?
-      @subject = Subject.friendly.find params[:subject_id]
-    else
-      # Little hack to determine if the name is equal a subject
-      _name = params[:name]
-      if _name.present? and Subject.where{name =~ _name}.any?
-        @subject = Subject.where{name =~ _name}.first
-      end
-    end
-  end
-
-  def params_has_planning_filters?
-    (params.keys & PLANNING_FILTERED_KEYS).any?
-  end
-
-  def search_plannings
-    search          = PlanningSearch.search(params, group: :structure_id_str)
-    structures      = search.group(:structure_id_str).groups.collect do |planning_group|
-      planning_group.results.first.structure
-    end
-    total           = search.group(:structure_id_str).total
-
-    [ structures, total ]
-  end
-
-  def search_structures
-      # Else, can search per structure
-      search           = StructureSearch.search(params)
-      structures       = search.results
-      total            = search.total
-
-    [ structures, total ]
-  end
-
-  def retrieve_location
-    StructureSearch.retrieve_location(params)
-  end
-
-  def retrieve_models
-    @structures.map do |structure|
-      StructureSerializer.new(structure, { root: false })
-    end
-  end
-
   def index
-    filter_by_subject?
+    @subject = filter_by_subject?
 
     params[:page] = 1 unless request.xhr?
 
-    # If any of those key are in the params, then search per planning
     if params_has_planning_filters?
       @structures, @total = search_plannings
     else
@@ -89,7 +36,7 @@ class StructuresController < ApplicationController
     end
 
     @latlng = retrieve_location
-    @models = retrieve_models 
+    @models = jasonify @structures
 
     if params[:name].present?
       # Log search terms
