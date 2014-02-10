0edeca54aab37059e39c5d782ec634e3e1a4d13a
Nima Izadi <nim.izadi@gmail.com>
2014-02-10 09:54:17 +0100

    Add waiting list to participations and add test for it

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,15 +1,29 @@
 class Participation < ActiveRecord::Base
   acts_as_paranoid
 
+  ######################################################################
+  # Relations                                                          #
+  ######################################################################
   belongs_to :user
   belongs_to :planning
 
   has_one :structure, through: :planning
-  has_one :course, through: :planning
+  has_one :course   , through: :planning
+
+  ######################################################################
+  # Callbacks                                                          #
+  ######################################################################
+  before_save :set_waiting_list
+  after_destroy :update_planning_participations_waiting_list
 
   attr_accessible :user, :planning
 
-  default_scope -> { order('created_at ASC') }
+  ######################################################################
+  # Scopes                                                             #
+  ######################################################################
+  default_scope               -> { order('created_at ASC') }
+  scope :waiting_list       , -> { where { waiting_list == true } }
+  scope :not_in_waiting_list, -> { where { waiting_list == false } }
 
   def alert_for_destroy
     ParticipationMailer.delay.alert_for_destroy(self)
@@ -19,4 +33,28 @@ class Participation < ActiveRecord::Base
     ParticipationMailer.delay.alert_for_changes(self)
   end
 
+  ######################################################################
+  # Callbacks                                                          #
+  ######################################################################
+  # Set waiting list to true regarding the number of participants and the
+  # /!\ Don't make it private
+  #
+  # @return nil
+  def set_waiting_list
+    self.waiting_list = ( self.planning.nb_place_available <= 0 )
+    nil
+  end
+
+  private
+
+  # When participation is destroyed, update waiting list of all participations of same planning
+  #
+  # @return nil
+  def update_planning_participations_waiting_list
+    planning.participations.each do |participation|
+      participation.set_waiting_list
+      participation.save if participation.waiting_list_changed?
+    end
+    nil
+  end
 end
