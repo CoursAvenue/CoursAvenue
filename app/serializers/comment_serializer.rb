a6c10e29e43af49ea2199be9e9bc4655ef381b28
variousauthors <various.authors@gmail.com>
2014-02-10 12:04:36 +0100

    Return the full comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -4,10 +4,14 @@ class CommentSerializer < ActiveModel::Serializer
   include ActionView::Helpers::UrlHelper
   include TruncateHtmlHelper
 
-  attributes :id, :content, :title, :author_name, :course_name, :created_at, :comment_url, :rating, :comments_url, :distance_of_time
+  attributes :id, :content, :title, :author_name, :course_name, :created_at, :comment_url, :rating, :comments_url, :distance_of_time, :full_content
 
   def content
     truncate_html(object.content, length: 100, omission: "...").html_safe
+  end
+
+  def full_content
+    object.content
   end
 
   # while link_to and such aren't working, we will just use this
