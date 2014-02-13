35f76b68558ea71d55b5483d8b8886124eda1f2b
Nima Izadi <nim.izadi@gmail.com>
2014-02-13 17:58:59 +0100

    Added back hour range

--- app/assets/javascripts/backbone/open_doors_search/views/structures_collection/filters/date_filter_view.js
+++ app/assets/javascripts/backbone/open_doors_search/views/structures_collection/filters/date_filter_view.js
@@ -4,7 +4,8 @@ OpenDoorsSearch.module('Views.StructuresCollection.Filters', function(Module, Ap
     Module.DateFilterView = Backbone.Marionette.ItemView.extend({
         template: Module.templateDirname() + 'date_filter_view',
         initialize: function() {
-            this.announce = _.debounce(this.announce, 800);
+            this.announce     = _.debounce(this.announce, 800);
+            this.announceTime = _.debounce(this.announceTime, 700);
         },
 
         setup: function (data) {
@@ -12,7 +13,22 @@ OpenDoorsSearch.module('Views.StructuresCollection.Filters', function(Module, Ap
             _.each(data.week_days, function(week_day) {
                 self.activateInput(week_day);
             });
-            this.announceBreadcrumb();
+            this.populateHourRange(this.ui.$hour_range.find('select').first(), 0, 24);
+            this.populateHourRange(this.ui.$hour_range.find('select').last(), 0, 24);
+            this.ui.$hour_range.hide();
+
+            // If hours correspond to a value of the select, then select it,
+            if (this.ui.$time_select.find('option[value=' + data.start_hour + '-' + data.end_hour + ']').length > 0) {
+                this.ui.$time_select.val(data.start_hour + '-' + data.end_hour);
+            // else show the time picker
+            } else if (data.start_hour.length !== 0 || data.end_hour.length !== 0) {
+                this.ui.$hour_range.show();
+                this.$('#start-hour').val(data.start_hour);
+                this.$('#end-hour').val(data.end_hour);
+                this.ui.$time_select.val('choose-slot');
+            }
+
+            this.announceBreadcrumbs();
         },
 
         // Clears all the given filters
@@ -22,15 +38,55 @@ OpenDoorsSearch.module('Views.StructuresCollection.Filters', function(Module, Ap
                 $input.prop("checked", false);
                 $input.parent('.btn').removeClass('active');
             });
+            this.ui.$time.find('select').val('all-day');
             this.announce();
         },
 
         ui: {
-            '$buttons': '[data-toggle=buttons]'
+            '$buttons'    : '[data-toggle=buttons]',
+            '$time'       : '[data-type=time]',
+            '$time_select': '[data-type=time] select',
+            '$hour_range':  '[data-type=hour-range]'
         },
 
         events: {
-            'change input': 'announce'
+            'change input'     : 'announce',
+            'change [data-type =time] select':         'announceTime',
+            'change [data-type =time] > select':       'showHourRange',
+            'change [data-type=time] > select':        'showHourRange',
+            'change [data-type=hour-range] select':    'narrowHourRange',
+            'change [data-type=hour-range] > select':  'announceHourRange'
+        },
+
+        showHourRange: function () {
+            if (this.ui.$time.find('select').val() !== "choose-slot") {
+                this.ui.$hour_range.slideUp();
+                return;
+            }
+
+            this.ui.$hour_range.slideDown();
+
+            if (this.$el.find('#start-hour').val() && this.$el.find('#end-hour').val()) {
+                this.announceHourRange();
+            } else {
+                this.$el.find('#start-hour').val(8);
+                this.$el.find('#end-hour').val(20);
+            }
+        },
+
+        narrowHourRange: function (e) {
+            var $select = this.ui.$hour_range.find('select:not(#' + e.currentTarget.id + ')'),
+                val = e.currentTarget.value, min, max;
+
+            if ($select.attr('id') !== 'start-hour') {
+                min = parseInt(val, 10);
+                max = 24;
+            } else {
+                min = 0;
+                max = parseInt(val, 10) + 1; // the range is closed on top
+            }
+
+            this.populateHourRange($select, min, max);
         },
 
         announce: function (e, data) {
@@ -41,14 +97,66 @@ OpenDoorsSearch.module('Views.StructuresCollection.Filters', function(Module, Ap
                 start_date: null,
                 end_date  : null
             });
+            this.announceTime();
+            this.announceHourRange();
 
-            this.announceBreadcrumb(week_days);
+            this.announceBreadcrumbs(week_days);
         },
 
-        announceBreadcrumb: function(week_days) {
+        announceHourRange: function (e) {
+            if (this.ui.$time.find('select').val() !== "choose-slot") {
+                return;
+            }
+
+            this.trigger("filter:date", {
+                start_hour: this.$el.find('#start-hour').val(),
+                end_hour:   this.$el.find('#end-hour').val(),
+            });
+            this.announceBreadcrumbs();
+        },
+
+        announceTime: function (e, data) {
+            var range, data;
+            switch(this.ui.$time.find('select').val()) {
+                case 'choose-slot':
+                    return;
+                break;
+                case 'all-day':
+                    data = {start_hour: null, end_hour: null};
+                break;
+                default:
+                    // Value is formatted as: 9-12
+                    range = this.ui.$time.find('select').val().split('-');
+                    data  = {
+                        start_hour: range[0],
+                        end_hour: range[1]
+                    };
+                break;
+            }
+            this.trigger("filter:date", data);
+            this.announceBreadcrumbs();
+        },
+
+        populateHourRange: function ($select, min, max) {
+            var val = parseInt($select.val(), 10) || 0;
+            $select.empty();
+
+            _.times((max - min), function (index) {
+                $select.append('<option value="' + (index + min) + '">' + (index + min) + 'h' + '</option>')
+            });
+
+            // if the select value is still in [min, max), apply it
+            if (min <= val && val < max) {
+                $select.val(val);
+            } else {
+                ($select.attr('id') === 'start-hour')? $select.val(min) : $select.val(max);
+            }
+        },
+
+        announceBreadcrumbs: function(week_days) {
             var title;
             week_days = week_days || _.map(this.$('[name="week_days[]"]:checked'), function(input){ return input.value });
-            if (week_days.length === 0) {
+            if (week_days.length === 0 && this.ui.$time_select.val() === 'all-day') {
                 this.trigger("filter:breadcrumb:remove", {target: 'date'});
             } else {
                 title = _.map(this.$('[name="week_days[]"]:checked'), function(input){ return $(input).parent().text().trim() });
@@ -57,7 +165,19 @@ OpenDoorsSearch.module('Views.StructuresCollection.Filters', function(Module, Ap
         },
 
         breadcrumbTitle: function () {
-            return "date";
+            var title = '';
+            if (this.ui.$buttons.find('input:checked')) {
+                title = _.map(this.ui.$buttons.find('input:checked'), function(input) { return $(input).parent().text() }).join(', ');
+            }
+            if (this.ui.$time_select.val() === 'all-day') {
+                title += ' toute la journée';
+            } else if (this.ui.$time_select.val() === 'choose-slot') {
+                title += ' de ' + this.$el.find('#start-hour').val() + 'h'
+                if (this.$el.find('#end-hour').val().length > 0) { title += ' à ' + this.$el.find('#end-hour').val() + 'h' }
+            } else if (this.ui.$time_select.val() !== 'all-day') {
+                title += ' / ' + this.ui.$time_select.find('option[value=' + this.ui.$time_select.val() + ']').text()
+            }
+            return title;
         },
 
         activateInput: function(week_day) {
