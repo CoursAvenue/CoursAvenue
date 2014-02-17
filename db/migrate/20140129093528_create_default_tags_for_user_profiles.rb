333eeb5721ec00cbae04cc4eaa04de39f2a84905
Nima Izadi <nim.izadi@gmail.com>
2014-02-17 18:10:05 +0100

    Add more find in batches

--- db/migrate/20140129093528_create_default_tags_for_user_profiles.rb
+++ db/migrate/20140129093528_create_default_tags_for_user_profiles.rb
@@ -3,23 +3,26 @@ class CreateDefaultTagsForUserProfiles < ActiveRecord::Migration
   def up
     bar = ProgressBar.new Structure.count
     Structure.find_in_batches batch_size: 50 do |structure_group|
-      GC.start
       structure_group.each do |structure|
         bar.increment!
         next unless structure.main_contact
-        structure.user_profiles.find_each do |user_profile|
-          user_profile_email        = user_profile.email
-          user_profile_name         = user_profile.first_name
-          if user_profile_name.present?
-            user_profile.first_name = user_profile_name.split(' ')[0..-2].join(' ')
-            user_profile.last_name  = user_profile_name.split(' ').last if user_profile_name.split(' ').length > 1
-            user_profile.save
+        structure.find_in_batches batch_size: 250 do |user_profile_group|
+          user_profile_group.each do |user_profile|
+            user_profile_email        = user_profile.email
+            user_profile_name         = user_profile.first_name
+            if user_profile_name.present?
+              user_profile.first_name = user_profile_name.split(' ')[0..author_name.split(' ').length - 2].join(' ') if user_profile.first_name.blank? # All except the last
+              user_profile.last_name  = user_profile_name.split(' ').last                                            if user_profile.last_name.blank? and self.author_name.split(' ').length > 1
+              user_profile.save
+            end
+            if structure.comments.where{email == user_profile_email}.any?
+              structure.tag(user_profile, with: UserProfile::DEFAULT_TAGS[:comments], on: :tags)
+            end
           end
-          if structure.comments.where{email == user_profile_email}
-            structure.tag(user_profile, with: UserProfile::DEFAULT_TAGS[:comments], on: :tags)
-          end
+          GC.start
         end
       end
+      GC.start
     end
   end
 
