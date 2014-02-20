e72517c5dd2d136c37fb54d60ef25c76fa118744
variousauthors <various.authors@gmail.com>
2014-02-20 16:59:44 +0100

    Show the comments
    Unfinished comments are shown as comments in the show.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -13,6 +13,7 @@ class VisitorsController < ApplicationController
 
   def show
     @visitor = Visitor.where(fingerprint: params[:fingerprint]).first
+    @comments = @visitor.comments
 
     respond_to do |format|
       format.html
@@ -23,6 +24,8 @@ class VisitorsController < ApplicationController
     @visitor = Visitor.where(fingerprint: params[:fingerprint]).first_or_create
     params[:visitor].delete(:fingerprint)
 
+    # if the comment was not submitted, add it as an abandoned
+    # comment, otherwise add it as a regular comment
     params[:comments].each do |data|
       comment = @visitor.comments.build
       comment.fields = data
