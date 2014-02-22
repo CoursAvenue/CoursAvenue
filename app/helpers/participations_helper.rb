7dfc921d1fc7a345bc5da4bef290aa7953cbf606
Nima Izadi <nim.izadi@gmail.com>
2014-02-22 19:29:12 +0100

    Updated sharing text

--- app/helpers/participations_helper.rb
+++ app/helpers/participations_helper.rb
@@ -2,19 +2,21 @@
 module ParticipationsHelper
 
   def share_participation_url(participation, provider = :facebook)
-    course = participation.course
+    course  = participation.course
+    summary = "Je viens de m'inscrire aux Journées Portes Ouvertes de CoursAvenue des 5-6 avril en Ile-de-France. Comme moi, participez gratuitement à l'atelier «#{@participations.last.course.name}»."
     case provider
     when :facebook
-      URI.encode("http://www.facebook.com/sharer.php?s=100&p[title]=#{course.name} par #{course.structure.name}&p[url]=#{jpo_structure_url(course.structure, subdomain: 'www')}&p[summary]=#{truncate(course.description, length: 200)}")
+      URI.encode("http://www.facebook.com/sharer.php?s=100&p[title]=#{course.name} par #{course.structure.name}&p[url]=#{jpo_structure_url(course.structure, subdomain: 'www')}&p[summary]=#{summary}")
     when :twitter
       URI.encode("https://twitter.com/intent/tweet?text=Je participe à un cours gratuit donné par #{course.structure.name}&via=CoursAvenue&hashtags=JPO2014&url=#{jpo_structure_url(course.structure, subdomain: 'www')}")
     end
   end
 
   def add_to_calendar(participation)
-    structure = participation.structure
-    planning  = participation.planning
-    course    = participation.course
+    structure  = participation.structure
+    planning   = participation.planning
+    course     = participation.course
+    place_info = "Infos sur le lieu : #{planning.place.info} #{planning.place.private_info}".gsub(/\r\n/, ' ')
     # Date format
     #  <span class="_end">11-05-2012 11:38:46</span>
     link = <<-eos
@@ -24,7 +26,7 @@ module ParticipationsHelper
         <span class="_end">#{l(planning.end_date)} #{l(planning.end_time, format: :default_only_time)}</span>
         <span class="_zonecode">40</span>
         <span class="_summary">JPO CoursAvenue - #{course.name}</span>
-        <span class="_description">Journées Portes Ouvertes CoursAvenue</span>
+        <span class="_description">#{place_info}</span>
         <span class="_location">#{planning.place.location.address}</span>
         <span class="_organizer">CoursAvenue en collaboration avec #{structure.name}</span>
         <span class="_organizer_email">#{structure.main_contact.email}</span>
