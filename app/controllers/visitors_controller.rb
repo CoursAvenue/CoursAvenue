2d10df3308c05760fe9d797885f5494fce4de90c
variousauthors <various.authors@gmail.com>
2014-02-24 11:17:19 +0100

    Addresses comments for pull request
    - Removed backtics from Gemfile
    - extracted a private method build_visitors
    - sexy class is sexy

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -24,13 +24,7 @@ class VisitorsController < ApplicationController
     @visitor = Visitor.where(fingerprint: params[:fingerprint].to_s).first_or_create
     params[:visitor].delete(:fingerprint)
 
-    params[:comments].each do |data|
-      comment = @visitor.comments.build
-
-      comment.fields     = data
-      comment.ip_address = request.remote_ip
-
-    end unless params[:comments].nil?
+    build_visitors unless params[:comments].nil?
 
     @visitor.update_attributes(params[:visitor])
     @visitor.save!
@@ -39,6 +33,21 @@ class VisitorsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  # for each set of attributes for comments, we build
+  # on comment and include the field data and ip address
+  # TODO later we will do this in update_attributes by
+  #   using accepts_nested_attributes_for
+  def build_visitors
+    params[:comments].each do |data|
+      comment = @visitor.comments.build
+
+      comment.fields     = data
+      comment.ip_address = request.remote_ip
+    end 
   end
 
 end
