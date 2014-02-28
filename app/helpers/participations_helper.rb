e7f0b99415e6e9ada949b7c8c042fb598bf9c796
Nima Izadi <nim.izadi@gmail.com>
2014-02-28 23:30:13 +0100

    Update to work in staging

--- app/helpers/participations_helper.rb
+++ app/helpers/participations_helper.rb
@@ -3,7 +3,7 @@ module ParticipationsHelper
 
   def share_participation_to_facebook_friend_url(participation, friend_id=nil)
     course       = participation.course
-    link         = URI.encode(jpo_structure_url(course.structure, subdomain: 'www'))
+    link         = URI.encode(jpo_structure_url(course.structure, subdomain: (Rails.env.staging? ? 'staging' : 'www')))
     redirect_uri = URI.encode(user_participations_url(current_user))
     URI.encode("http://www.facebook.com/dialog/send?app_id=#{CoursAvenue::Application::FACEBOOK_APP_ID}&link=#{link}&redirect_uri=#{redirect_uri}&to=#{friend_id}")
   end
