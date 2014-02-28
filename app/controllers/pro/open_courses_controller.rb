2e76936dd4e66eec8cf3ca1ac5a8859ab612e64f
Nima Izadi <nim.izadi@gmail.com>
2014-02-28 19:20:56 +0100

    Fixed course opens for jpo

--- app/controllers/pro/open_courses_controller.rb
+++ app/controllers/pro/open_courses_controller.rb
@@ -4,10 +4,7 @@ class Pro::OpenCoursesController < Pro::ProController
   layout 'admin'
 
   def index
-    @courses_new = Course::Open.where{created_at > Date.parse('27/02/2014')}.all.sort_by{ |course| course.structure.name.downcase.strip }
-    @courses_old = Course::Open.where{created_at <= Date.parse('27/02/2014')}.all
-    @courses_old = @courses_old + @courses_new.select{|c| c.active?}
-    @courses_old = @courses_old.sort_by{ |course| course.structure.name.downcase.strip }
-    @courses_new = @courses_new.reject{|c| c.active? }
+    @courses_inactive = Course::Open.where{active == false}.all.sort_by{ |course| course.structure.name.downcase.strip }
+    @courses_active = Course::Open.where{active == true}.all.sort_by{ |course| course.structure.name.downcase.strip }
   end
 end
