69b39eed3072dda7ee777d2e7848adb16aec1992
Nima Izadi <nim.izadi@gmail.com>
2014-02-28 18:38:16 +0100

    Fixed sorting

--- app/controllers/pro/open_courses_controller.rb
+++ app/controllers/pro/open_courses_controller.rb
@@ -5,8 +5,9 @@ class Pro::OpenCoursesController < Pro::ProController
 
   def index
     @courses_new = Course::Open.where{created_at > Date.parse('27/02/2014')}.all.sort_by{ |course| course.structure.name.downcase.strip }
-    @courses_old = Course::Open.where{created_at <= Date.parse('27/02/2014')}.all.sort_by{ |course| course.structure.name.downcase.strip }
+    @courses_old = Course::Open.where{created_at <= Date.parse('27/02/2014')}.all
     @courses_old = @courses_old + @courses_new.select{|c| c.active?}
+    @courses_old = @courses_old.sort_by{ |course| course.structure.name.downcase.strip }
     @courses_new = @courses_new.reject{|c| c.active? }
   end
 end
