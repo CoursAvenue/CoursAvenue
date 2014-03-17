24020faf18c659ae1755fe7e876631828b237986
Nima Izadi <nim.izadi@gmail.com>
2014-03-17 18:57:11 +0100

    Added blank days for graph

--- app/controllers/pro/participations_controller.rb
+++ app/controllers/pro/participations_controller.rb
@@ -6,7 +6,11 @@ class Pro::ParticipationsController < Pro::ProController
   def index
     @participations       = Participation.all
     @participations_graph = {}
-    Participation.where{created_at > Date.parse('04/01/2014')}.group_by{|p| p.created_at.to_date}.each do |date, participations|
+    dates = (Date.parse('2014/03/03')..Date.today).step
+    dates.each do |date|
+      @participations_graph[date] = 0
+    end
+    Participation.where{created_at > Date.parse('2014/03/03')}.group_by{|p| p.created_at.to_date}.each do |date, participations|
       @participations_graph[date] = participations.length
     end
   end
