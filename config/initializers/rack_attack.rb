16c22846ac8fbfaad6dae29b3f70172c140c147c
Nima Izadi <nim.izadi@gmail.com>
2014-03-23 20:13:23 +0100

    Add gem to prevent from bot attacks

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,16 @@
+# Block requests from 1.2.3.4
+Rack::Attack.blacklist('block 46.105.44.24') do |request|
+  # Request are blocked if the return value is truthy
+  request.ip == '46.105.44.24'
+end
+
+# Block requests containing '/etc/password' in the params.
+# After 3 blocked requests in 10 minutes, block all requests from that IP for 5 minutes.
+Rack::Attack.blacklist('fail2ban pentesters') do |req|
+  # `filter` returns truthy value if request fails, or if it's from a previously banned IP
+  # so the request is blocked
+  Rack::Attack::Fail2Ban.filter(req.ip, :maxretry => 3, :findtime => 10.minutes, :bantime => 5.minutes) do
+    # The count for the IP is incremented if the return value is truthy.
+    CGI.unescape(req.query_string) =~ %r{/etc/passwd}
+  end
+end
