2b64ebd5ad84e600aecc935dcc02ef9d54858d01
variousauthors <various.authors@gmail.com>
2014-04-02 15:36:39 +0200

    Prevented a special case for saving model
    when the url is like /mes-eleves/?stuff instead of just /mes-eleves?stuff
    we were having problems. So now I've written a message to normalize
    the URL

--- app/assets/javascripts/backbone/user_management/models/user_profile.js
+++ app/assets/javascripts/backbone/user_management/models/user_profile.js
@@ -15,7 +15,30 @@ UserManagement.module('Models', function(Models, App, Backbone, Marionette, $, _
                 resource = resource + '/' + id;
             }
 
+            // we were having a problem at some point where the URL would sometimes
+            // be mes-eleves/? and sometimes just mes-eleves?
+            //
+            // this was causing the url method to return mes-eleves/mes-eleves/:id,
+            // so we are just sanitizing away the duplicate
+            resource = this.sanitizeUrl(resource);
+
             return this.collection.url.basename + resource;
+        },
+
+        /* @pre assuming the resource is a string of the form "/resource/:id" */
+        sanitizeUrl: function sanitizeUrl (resource) {
+            var basename_parts = this.collection.url.basename.split("/"),
+                last_part      = basename_parts.slice(-1),
+                resource_parts = resource.split("/"),
+                first_part     = resource_parts.slice(1, 2); // the array is like ["", "resource", "id"]
+
+            // if the basename_parts already contain the resource,
+            if (_.isEqual(first_part, last_part)) {
+                resource_parts = ["", resource_parts.slice(-1)]; // then we just want the :id part
+            }
+
+            return resource_parts.join("/");
         }
+
     });
 });
