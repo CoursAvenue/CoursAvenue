1e672d36339f2375b98da7909acf85e4c0917f98
Nima Izadi <nim.izadi@gmail.com>
2014-04-04 18:49:27 +0200

    Fixed phone helper

--- app/helpers/user_profiles_helper.rb
+++ app/helpers/user_profiles_helper.rb
@@ -3,7 +3,8 @@ module UserProfilesHelper
 
   def readable_phone_number phone_number
     if phone_number
-      "#{phone_number[0..1]} #{phone_number[2..3]} #{phone_number[4..5]} #{phone_number[6..7]} #{phone_number[8..9]}"
+      _phone_number = phone_number.gsub(' ', '')
+      "#{_phone_number[0..1]} #{_phone_number[2..3]} #{_phone_number[4..5]} #{_phone_number[6..7]} #{_phone_number[8..9]}"
     end
   end
 
