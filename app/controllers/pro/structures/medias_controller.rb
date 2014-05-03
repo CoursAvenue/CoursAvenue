577c8c827d8aa575892f9a1211132a161975abca
Nima Izadi <nim.izadi@gmail.com>
2014-05-03 14:56:58 +0200

    Fixed medias action page

--- app/controllers/pro/structures/medias_controller.rb
+++ app/controllers/pro/structures/medias_controller.rb
@@ -1,18 +1,19 @@
 # encoding: utf-8
 class Pro::Structures::MediasController < Pro::ProController
   before_action :authenticate_pro_admin!
+
+  # Because edit lets you choose if the media will be on the vertical page
   before_action :authenticate_pro_super_admin!, only: [:edit]
+  before_action :load_structure
   layout 'admin'
 
   def index
-    @structure = Structure.friendly.find params[:structure_id]
     @media     = @structure.medias.build
     @image     = Media::Image.new mediable: @structure
     @medias    = @structure.medias.videos_first.cover_first.reject(&:new_record?)
   end
 
   def destroy
-    @structure = Structure.friendly.find params[:structure_id]
     @media     = Media.find params[:id]
     @media.destroy
     respond_to do |format|
@@ -21,13 +22,11 @@ class Pro::Structures::MediasController < Pro::ProController
   end
 
   def edit
-    @structure = Structure.friendly.find params[:structure_id]
     @media     = Media.find params[:id]
     render layout: false
   end
 
   def update
-    @structure = Structure.friendly.find params[:structure_id]
     @media     = Media.find params[:id]
     @media.update_attributes(params[:media])
     respond_to do |format|
@@ -45,5 +44,11 @@ class Pro::Structures::MediasController < Pro::ProController
     respond_to do |format|
       format.html { redirect_to pro_structure_medias_path(@structure) }
     end
+  end
+
+  private
+
+  def load_structure
+    @structure = Structure.friendly.find params[:structure_id]
   end
 end
