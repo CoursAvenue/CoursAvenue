405213781d0677fc8d36a1597eb5488b30e1fa29
Nima Izadi <nim.izadi@gmail.com>
2014-05-11 20:06:28 +0200

    Test 2

--- app/assets/javascripts/backbone/cours_avenue.js
+++ app/assets/javascripts/backbone/cours_avenue.js
@@ -1,12 +1,10 @@
 //= require ./core_ext
 //= require ./coursavenue/manifest
 //
-//= require ./daedalus/daedalus.js
-//= require_tree ./daedalus
 //= require ./data_mining/manifest
 //
-//= require ./filtered_search/manifest
+
 //= require ./open_doors_search/manifest
 //= require ./home_index_structures/manifest
 //= require ./user_management/manifest
-
+//= require ./structure_profile/manifest
