2ba9f09a87fe0e052c1b4ab6a5f6fb4236becc33
Nima Izadi <nim.izadi@gmail.com>
2014-05-12 09:34:24 +0200

    Trying to remove potential db call

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,109 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require global
+
+// ---------------------------------- Core include
+//= require jquery_ujs
+//= require libs/jquery-ui-1.10.3.custom
+//= require js-routes
+
+// require jquery.turbolinks
+
+// ---------------------------------- Lib includes
+//= require libs/handlebars
+//= require libs/handlebars-helpers
+//= require libs/chosen.jquery
+//= require libs/jquery.tablesorter
+//= require libs/jquery.cookie
+//= require libs/typeahead
+//= require libs/jquery.scrollTo
+//= require libs/jquery.Jcrop
+//= require libs/jquery.fileupload/vendor/jquery.ui.widget
+//= require libs/jquery.fileupload/jquery.iframe-transport
+//= require libs/jquery.fileupload/jquery.fileupload
+//= require libs/jquery.fileupload/jquery.fileupload-process
+//= require libs/jquery.fileupload/jquery.fileupload-validate
+//= require libs/jquery.fileupload/jquery.fileupload-image
+//= require libs/jquery.nouislider
+//= require libs/jquery.stickem
+//= require libs/jquery.masonry
+//= require libs/jquery.lazyload
+//= require libs/moment
+//= require libs/responsiveslides
+//= require libs/add_this_event
+//= require libs/jquery.placeholder
+
+//= require libs/fancybox/jquery.fancybox
+//= require underscore
+
+//= require fingerprint
+
+// ---------------------------------- Backbone
+//= require backbone
+//= require backbone.marionette
+//= require libs/backbone.googlemaps
+//= require backbone-relational
+//= require libs/backbone.paginator.js
+//= require libs/backbone.poller.js
+//= require backbone/cours_avenue
+//= require railsy_backbone.sync
+//= require railsy_backbone.datalink
+
+//= require_tree ./libs/fancybox/helpers/
+
+//---- Bootstrap plugins
+//= require libs/bootstrap/bootstrap.transition
+//= require libs/bootstrap/bootstrap.collapse
+//= require libs/bootstrap/bootstrap.tooltip
+//= require libs/bootstrap/bootstrap.popover
+//= require libs/bootstrap/bootstrap.tab
+//= require libs/bootstrap/bootstrap.datepicker
+//= require libs/bootstrap/bootstrap.button
+//= require libs/bootstrap/datepicker-locales/bootstrap.datepicker.fr
+
+
+//---- Highcharts
+//= require libs/highcharts/highcharts
+//= require libs/highcharts/modules/exporting
+
+// ---------------------------------- jQuery plugins
+// See boilerplate and pattern:
+// - http://jqueryboilerplate.com/
+// - https://github.com/jquery-boilerplate/jquery-patterns/
+// - https://github.com/jquery-boilerplate/jquery-boilerplate
+//= require_tree ./plugins/
+// require plugins/address_picker
+// require plugins/checkbox_list
+// require plugins/city_autocomplete
+// require plugins/closer
+// require plugins/date_range
+// require plugins/drop_down
+// require plugins/dropped_options
+// require plugins/flash
+// require plugins/highlight
+// require plugins/input_updaters
+// require plugins/read_more
+// require plugins/text_counter
+// require plugins/textarea_resizer
+// require plugins/toggler
+// require plugins/time_range
+// require plugins/image_input
+// require plugins/show_more_on_demand
+// require plugins/jquery-sticky.js
+// require plugins/parent_descendant_subjects
+// require plugins/wizard_helper
+
+//= require gmaps/google
+
+//= require ./plugins_initalization
+//= require handlebars
