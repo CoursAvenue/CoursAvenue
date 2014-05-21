4084ac6516a881a947d9791fcd34bd4c40088ab4
Nima Izadi <nim.izadi@gmail.com>
2014-05-21 16:09:45 +0200

    Fixed bug and typos

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -9,6 +9,6 @@ module ConversationsHelper
 
   def conversation_label_name(conversation)
     label = Mailboxer::Label.find(conversation.mailboxer_label_id)
-    I18n.t(label.name)
+    I18n.t(label.name).pluralize(2)
   end
 end
