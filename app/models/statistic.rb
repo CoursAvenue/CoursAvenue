f7ddac8a9a50d4d40724a965f980c801c4320a0e
Nima Izadi <nim.izadi@gmail.com>
2014-06-10 10:25:37 +0200

    Added comments to stats and assure that there is always a fingerprint

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -18,19 +18,50 @@ class Statistic < ActiveRecord::Base
   scope :views,       -> { where( action_type: 'view') }
   scope :actions,     -> { where( action_type: 'action') }
 
-  def self.print(structure_id, user, fingerprint, infos=nil)
+
+  # Creates a statistic when a structure appears in the results of a search
+  # @param structure_id Integer Structure id that appeared in the search
+  # @param user User who searched for it
+  # @param fingerprint=generate_fingerprint String, Fingerprint (hash) generated client side to identify a unique user
+  # @param infos=nil String, more info on the stat
+  #
+  # @return Statistic
+  def self.print(structure_id, user, fingerprint=generate_fingerprint, infos=nil)
     Statistic.create(action_type: 'impression', structure_id: structure_id, user_fingerprint: fingerprint)
   end
 
-  def self.view(structure_id, user, fingerprint, infos=nil)
+
+  # Creates a statistic when a structure has been viewed (#show action)
+  # @param structure_id Integer Structure id that appeared in the search
+  # @param user User who searched for it
+  # @param fingerprint=generate_fingerprint String, Fingerprint (hash) generated client side to identify a unique user
+  # @param infos=nil String, more info on the stat
+  #
+  # @return Statistic
+  def self.view(structure_id, user, fingerprint=generate_fingerprint, infos=nil)
     Statistic.create(action_type: 'view', structure_id: structure_id, user_fingerprint: fingerprint)
   end
 
-  def self.action(structure_id, user, fingerprint, infos=nil)
+  # Creates a statistic when there has been an action on a structure (eg. contact etc.)
+  # @param structure_id Integer Structure id that appeared in the search
+  # @param user User who searched for it
+  # @param fingerprint=generate_fingerprint String, Fingerprint (hash) generated client side to identify a unique user
+  # @param infos=nil String, more info on the stat
+  #
+  # @return Statistic
+  def self.action(structure_id, user, fingerprint=generate_fingerprint, infos=nil)
     Statistic.create(action_type: 'action', structure_id: structure_id, user_fingerprint: fingerprint, infos: infos )
   end
 
-  def self.create_action(action_name, structure_id, user, fingerprint, infos=nil)
+  # Creates a statistic regarding the action name
+  # @param action_name String Type of an action
+  # @param structure_id Integer Structure id that appeared in the search
+  # @param user User who searched for it
+  # @param fingerprint=generate_fingerprint String, Fingerprint (hash) generated client side to identify a unique user
+  # @param infos=nil String, more info on the stat
+  #
+  # @return Statistic
+  def self.create_action(action_name, structure_id, user, fingerprint=generate_fingerprint, infos=nil)
     case action_name
     when 'print'
       Statistic.print(structure_id, user, fingerprint, infos)
@@ -39,5 +70,18 @@ class Statistic < ActiveRecord::Base
     when 'view'
       Statistic.view(structure_id, user, fingerprint, infos)
     end
+  end
+
+  private
+
+  #
+  # Generate a random number if the fingerprint is nil
+  # Fingerprint is important because it will be unique when looking at the stats
+  # and if nil, the stat won't show up.
+  #
+  # @return String Fingerprint with one character longer than the fingerprint
+  # generated client side to be able to identify it
+  def self.generate_fingerprint
+    (rand * 10000000000).to_i.to_s
   end
 end
