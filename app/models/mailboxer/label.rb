3a8d6239d3e97bd8a69bc8385805feeaa9b689fa
Nima Izadi <nim.izadi@gmail.com>
2014-06-14 10:48:11 +0200

    Changed mailboxes label for information

--- app/models/mailboxer/label.rb
+++ app/models/mailboxer/label.rb
@@ -2,7 +2,7 @@ class Mailboxer::Label < ActiveHash::Base
   include ActiveHash::Enum
 
   self.data = [
-    { id: 1, short_name: 'Information' , name: 'mailboxer.label.information' , color: 'orange'    },
+    { id: 1, short_name: 'Information' , name: 'mailboxer.label.information' , color: 'blue'    },
     { id: 2, short_name: 'Comment'     , name: 'mailboxer.label.comment'     , color: 'green'    },
     { id: 3, short_name: 'Conversation', name: 'mailboxer.label.conversation', color: 'gray'    }
   ]
