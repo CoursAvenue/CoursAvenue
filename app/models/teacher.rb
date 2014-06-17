5491a193504317a6ed6df5866862b590934dde4b
Nima Izadi <nim.izadi@gmail.com>
2014-06-17 18:20:31 +0200

    Fixed avatar

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -13,7 +13,7 @@ class Teacher < ActiveRecord::Base
 
   has_attached_file :image,
                      styles: { normal: '150x' },
-                     default_url: ActionController::Base.helpers.asset_path('icons/avatar.png')
+                     default_url: ActionController::Base.helpers.asset_path('avatars/original/missing.png')
 
   validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
