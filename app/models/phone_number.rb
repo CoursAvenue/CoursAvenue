659e00f3c2c8a9887994c325770058096535bfe6
Nima Izadi <nim.izadi@gmail.com>
2014-06-17 16:17:12 +0200

    Improved design and fixed some stuffs

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,8 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
 
-  PHONE_TYPES  = ['phone_numbers.type.mobile',
-                  'phone_numbers.type.home']
-
   attr_accessible :number, :phone_type
 
   belongs_to :callable, polymorphic: true
