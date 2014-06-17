b6a37defeb19ed517e0172a0feaed4dd3f21277c
Nima Izadi <nim.izadi@gmail.com>
2014-06-17 16:39:24 +0200

    Fixed plannings not showing

--- app/controllers/structures/courses_controller.rb
+++ app/controllers/structures/courses_controller.rb
@@ -20,7 +20,6 @@ class Structures::CoursesController < ApplicationController
 
     @plannings.group_by(&:course_id).each do |course_id, plannings|
       course    = Course.find(course_id)
-      next unless course.active
       @courses << CourseSerializer.new(course, {
         root: false,
         structure: @structure,
