ee4539586b22a7f06e0c001bfcde6b461ea60976
Nima Izadi <nim.izadi@gmail.com>
2014-06-17 17:57:11 +0200

    Trying to fix teachers avatar

--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,10 +1,6 @@
 class TeacherSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image, :structure_id
 
-  def description
-    object.description.force_encoding(Encoding::ISO_8859_1) if object.description
-  end
-
   def image
     object.image.url(:normal)
   end
