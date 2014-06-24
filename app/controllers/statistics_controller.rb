7ca3732903dde49cf1d89279167e2bed40a5d120
Nima Izadi <nim.izadi@gmail.com>
2014-06-24 18:44:44 +0200

    Prevents from bug when creating statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,8 +1,10 @@
 class StatisticsController < ApplicationController
 
   def create
-    params[:structure_ids].each do |structure_id|
-      Statistic.create_action(params[:action_type], structure_id, current_user, params[:fingerprint], request.ip, params[:infos]) unless current_pro_admin
+    if params[:structure_ids]
+      params[:structure_ids].each do |structure_id|
+        Statistic.create_action(params[:action_type], structure_id, current_user, params[:fingerprint], request.ip, params[:infos]) unless current_pro_admin
+      end
     end
     respond_to do |format|
       format.js { render nothing: true }
