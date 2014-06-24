3455721e8bb01c2d9548f7431b7cb90aff894a3b
Nima Izadi <nim.izadi@gmail.com>
2014-06-24 12:23:29 +0200

    Removed nico from interceptor

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,10 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    if Rails.env.staging?
-      message.to = "nim.izadi@gmail.com, lagardenicolas@gmail.com"
-    else
-      message.to = "nim.izadi@gmail.com"
-    end
+    message.to = "nim.izadi@gmail.com"
   end
 end
