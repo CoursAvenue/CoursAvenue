9ccf9f8ff32d3d902752980bfa5207583f3d7286
Nima Izadi <nim.izadi@gmail.com>
2014-06-25 12:13:11 +0200

    Add validation on end_date for lesson courses

--- app/models/course/lesson.rb
+++ app/models/course/lesson.rb
@@ -2,6 +2,11 @@ class Course::Lesson < Course
 
   validates :start_date, :end_date, presence: true
 
+  ######################################################################
+  # Validations                                                        #
+  ######################################################################
+  validate :end_date_in_future
+
   def is_lesson?
     true
   end
@@ -20,5 +25,15 @@ class Course::Lesson < Course
 
   def latest_end_date
     self.end_date
+  end
+
+  private
+  # Add errors if end date is not in the future
+  #
+  # @return [type] [description]
+  def end_date_in_future
+    if end_date and end_date < Date.today
+      errors.add(:end_date, 'Le cours ne peut pas être dans le passé.')
+    end
   end
 end
