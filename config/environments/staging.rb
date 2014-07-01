da6b6c726211a3062fba1b5fd6bd0919b7193352
Nima Izadi <nim.izadi@gmail.com>
2014-07-01 11:51:42 +0200

    Test

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -9,7 +9,7 @@ CoursAvenue::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets  = true
