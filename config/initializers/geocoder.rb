75d053e2d71551248ea6cc419a258836f7e38c9d
Nima Izadi <nim.izadi@gmail.com>
2014-07-16 20:52:34 +0200

    Put back maps api key for geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,28 +1,28 @@
-# # https://github.com/alexreisner/geocoder
+# https://github.com/alexreisner/geocoder
 
-# if Rails.env.production?
-#   Geocoder.configure(
+if Rails.env.production?
+  Geocoder.configure(
 
-#     # geocoding service (see below for supported options):
-#     # :lookup => :yandex,
+    # geocoding service (see below for supported options):
+    # :lookup => :yandex,
 
-#     # IP address geocoding service (see below for supported options):
-#     # :ip_lookup => :maxmind,
+    # IP address geocoding service (see below for supported options):
+    # :ip_lookup => :maxmind,
 
-#     :api_key => ENV['GOOGLE_MAPS_API_KEY'],
-#     :use_https => true,
+    :api_key => ENV['GOOGLE_MAPS_API_KEY'],
+    :use_https => true,
 
-#     # to use an API key:
-#     # :api_key => '',
+    # to use an API key:
+    # :api_key => '',
 
-#     # geocoding service request timeout, in seconds (default 3):
-#     :timeout => 5,
+    # geocoding service request timeout, in seconds (default 3):
+    :timeout => 5,
 
-#     # set default units to kilometers:
-#     :units => :km,
+    # set default units to kilometers:
+    :units => :km,
 
-#     # caching (see below for details):
-#     # :cache => Redis.new,
-#     # :cache_prefix => "..."
-#   )
-# end
+    # caching (see below for details):
+    # :cache => Redis.new,
+    # :cache_prefix => "..."
+  )
+end
