b0020d2d670563a0da6e0cf978e2b9a322398f4c
Nima Izadi <nim.izadi@gmail.com>
2014-08-06 11:53:12 +0200

    Log searches

--- app/controllers/pro/search_term_logs_controller.rb
+++ app/controllers/pro/search_term_logs_controller.rb
@@ -3,7 +3,11 @@ class Pro::SearchTermLogsController < Pro::ProController
   before_action :authenticate_pro_super_admin!
 
   def index
-    @search_term_logs = SearchTermLog.count(order: 'name', group: ['name'])
-    @search_term_logs = @search_term_logs.reject { |search_term_log| search_term_log.length < 4 }
+    # @search_term_logs = SearchTermLog.order('name ASC').group('name').count
+    # @search_term_logs = @search_term_logs.reject { |search_term_log, count| count < 4 }
+    # @search_term_logs = @search_term_logs.sort_by(&:last).reverse
+
+    @search_term_logs = SearchTermLog.where( SearchTermLog.arel_table[:name].matches("filtre%") ).order('name ASC').group('name').count
+    @search_term_logs = @search_term_logs.sort_by(&:last).reverse
   end
 end
