2d630c9269036215d5b1aee6657bc291f627ec69
Nima Izadi <nim.izadi@gmail.com>
2014-08-11 17:44:50 +0200

    Fixed bug and updated wording

--- app/assets/javascripts/backbone/structure_profile/models/structure.js
+++ app/assets/javascripts/backbone/structure_profile/models/structure.js
@@ -1,7 +1,7 @@
 /* just a basic backbone model */
 StructureProfile.module('Models', function(Module, App, Backbone, Marionette, $, _) {
 
-    Module.Structure = CoursAvenue.Models.Structure.extend({
+    Module.Structure = Backbone.Model.extend({
 
         initialize: function initialize(bootstrap, bootstrap_meta) {
             var relations = ['places', 'courses', 'trainings', 'comments', 'teachers'],
