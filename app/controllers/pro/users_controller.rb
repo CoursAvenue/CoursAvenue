bc37dd80be62ed459b3880eb9d5f8ac19958d2fc
Nima Izadi <nim.izadi@gmail.com>
2014-08-14 18:24:45 +0200

    Fixed bug

--- app/controllers/pro/users_controller.rb
+++ app/controllers/pro/users_controller.rb
@@ -10,9 +10,9 @@ class Pro::UsersController < Pro::ProController
     if params[:with_comments]
       @users = UserSearch.search(active: true, has_comments: true, page: params[:page]).results
     elsif params[:inactive]
-      @users = UserSearch.where(active: true, has_confirmed: false ).params[:page].results
+      @users = UserSearch.search(active: true, has_confirmed: false ).params[:page].results
     else
-      @users = UserSearch.where(active: true).params[:page].results
+      @users = UserSearch.search(active: true).params[:page].results
     end
     @users_per_hour = { 0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0, 13 => 0, 14 => 0, 15 => 0, 16 => 0, 17 => 0, 18 => 0, 19 => 0, 20 => 0, 21 => 0, 22 => 0, 23 => 0 }
     User.active.find_each do |user|
