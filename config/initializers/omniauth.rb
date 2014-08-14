1640cb37ab7950a4c034507474f14036cc72b4fa
Nima Izadi <nim.izadi@gmail.com>
2014-08-14 18:23:16 +0200

    gs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 # Already in devise.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], provider_ignores_state: true, display: 'popup',
-            scope: 'email,user_location,age,user_birthday,gender', display: 'popup'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], provider_ignores_state: true, scope: 'email,user_location,age,user_birthday,gender'
 end
