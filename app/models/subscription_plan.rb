5c2eac085c65d755558003990975192b6c9e5080
Nima Izadi <nim.izadi@gmail.com>
2014-08-16 16:12:10 +0200

    Added comment

--- app/models/subscription_plan.rb
+++ app/models/subscription_plan.rb
@@ -143,7 +143,7 @@ class SubscriptionPlan < ActiveRecord::Base
     params['method']          = 'payment'
 
     res = Net::HTTP.post_form URI(ENV['BE2BILL_REST_URL']), params
-    puts res.body
+    puts res.body # For debugging purpose
     if res.is_a?(Net::HTTPSuccess)
       return true
     else
