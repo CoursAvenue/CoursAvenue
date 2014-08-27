63b7818c1c9559fe2ed8ac27e3f2424d0eb9236b
Nima Izadi <nim.izadi@gmail.com>
2014-08-27 16:03:10 +0200

    Fixed comment avatar images

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -3,7 +3,7 @@ class CommentSerializer < ActiveModel::Serializer
   include ActionView::Helpers::DateHelper
 
   attributes :id, :content, :title, :author_name, :course_name, :created_at, :rating,
-             :distance_of_time, :comment_url
+             :distance_of_time, :comment_url, :avatar_url
 
   has_one :reply, serializer: CommentReplySerializer
 
@@ -13,5 +13,9 @@ class CommentSerializer < ActiveModel::Serializer
 
   def comment_url
     structure_url((options[:structure] || object.structure), anchor: "recommandation-#{object.id}", subdomain: CoursAvenue::Application::WWW_SUBDOMAIN, host: 'coursavenue.com')
+  end
+
+  def avatar_url
+    object.user.avatar_url
   end
 end
