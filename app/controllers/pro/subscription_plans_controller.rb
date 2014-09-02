fb03b835c04718201e6ebf496db8081f91bd88d8
Nima Izadi <nim.izadi@gmail.com>
2014-09-02 11:22:18 +0200

    Fixed stats in subplancontroller

--- app/controllers/pro/subscription_plans_controller.rb
+++ app/controllers/pro/subscription_plans_controller.rb
@@ -7,9 +7,9 @@ class Pro::SubscriptionPlansController < Pro::ProController
 
     # @orders_per_month  = Order.order("DATE_TRUNC('month', created_at) ASC").group("DATE_TRUNC('month', created_at)").count
     @orders_per_month = {}
-    (Date.new(2014, 6)..Date.today + 1.month).select {|d| d.day == 1}.map {|d| d - 1}.drop(1).each do |begining_of_month|
-      @orders_per_month[begining_of_month] = SubscriptionPlan.where(SubscriptionPlan.arel_table[:expires_at].gteq(begining_of_month).and(
-                                                                    SubscriptionPlan.arel_table[:created_at].lteq(begining_of_month))).count
+    (Date.new(2014, 6)..Date.today + 1.month).select {|d| d.day == 1}.map {|d| d - 1}.drop(1).each do |end_of_month|
+      @orders_per_month[end_of_month] = SubscriptionPlan.where(SubscriptionPlan.arel_table[:expires_at].gteq(end_of_month - 1.month).and(
+                                                               SubscriptionPlan.arel_table[:created_at].lteq(end_of_month))).count
     end
 
     # dates = (2.month.ago.to_date..Date.today).step
