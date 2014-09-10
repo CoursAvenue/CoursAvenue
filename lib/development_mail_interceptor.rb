69a0a946a18eea33600f4d9361d28aae3adb8b8a
Nima Izadi <nim.izadi@gmail.com>
2014-09-10 12:18:26 +0200

    Removed commented code in interceptor

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -2,7 +2,5 @@ class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
     message.to = ENV["INTERCEPTOR_MAIL"]
-    # message.to = "nim.izadi@gmail.com, nima.izadi@hotmail.fr, izadi.nima@orange.fr"
-    # message.to = "nim.izadi@gmail.com"
   end
 end
