1356f03a2d861c4835ed69aa93d2c6a6e499e389
Nima Izadi <nim.izadi@gmail.com>
2014-09-12 16:25:33 +0200

    Fixed js bug in developpment env

--- app/assets/javascripts/backbone/structure_profile/views/messages/message_form_view.js
+++ app/assets/javascripts/backbone/structure_profile/views/messages/message_form_view.js
@@ -87,8 +87,10 @@ StructureProfile.module('Views.Messages', function(Module, App, Backbone, Marion
             this.$('.input_field_error').remove();
             this.model.sync({
                 success: function success (response) {
-                    window._fbq.push(['track', '6016785958627', { 'value':'0.00','currency':'EUR' }]);
-                    ga('send', 'event', 'Action', 'message');
+                    if (window._fbq) { // If on production
+                        window._fbq.push(['track', '6016785958627', { 'value':'0.00','currency':'EUR' }]);
+                        ga('send', 'event', 'Action', 'message');
+                    }
                     this.$('form').trigger('ajax:complete.rails');
                     $.magnificPopup.open({
                           items: {
