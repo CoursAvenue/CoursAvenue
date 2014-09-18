c2f509ba1f070da03a895f15cb103eaaa84c1ee4
Nima Izadi <nim.izadi@gmail.com>
2014-09-18 10:24:48 +0200

    Fixed stat weird bug and wording

--- app/controllers/pro/structures/statistics_controller.rb
+++ app/controllers/pro/structures/statistics_controller.rb
@@ -9,29 +9,29 @@ class Pro::Structures::StatisticsController < Pro::ProController
     @structure  = Structure.find params[:structure_id]
     @statistics = @structure.metrics
 
-    empty_hash_of_days = {}
-    (15.days.ago.to_date..Date.today).each { |date| empty_hash_of_days[date] = 0 }
+    @empty_hash_of_days = {}
+    (15.days.ago.to_date..Date.today).each { |date| @empty_hash_of_days[date] = 0 }
 
-    @impressions, @views, @phone_actions, @website_actions, @message_actions, @follow_actions = (1..6).map { empty_hash_of_days.dup }
+    @impressions, @views, @phone_actions, @website_actions, @message_actions, @follow_actions = (1..6).map { @empty_hash_of_days.dup }
     @impressions_total_count, @views_total_count, @actions_total_count = 0, 0, 0
     # Selecting all stats from 15 days ago
     # Ordering them by creation date
     # Grouping them by creation date
     # selecting counting by DISTINCT(user_fingerprint, ip_address) because each stats is counted only onced per user & per day
-    @statistics.impressions      .where(:created_at.gt => (Date.today - 15.days) )
+    @statistics.impressions      .where(:created_at.gt => (15.days.ago) )
                                  .asc(:created_at)
                                  .group_by { |metric| metric.created_at.to_date }
                                  .map{ |date, metrics| { created_at: date, user_count: metrics.uniq(&:identify).length } }
                                  .each{ |stat| @impressions[stat[:created_at]] = stat[:user_count]; @impressions_total_count += stat[:user_count] }
 
 
-    @statistics.views            .where(:created_at.gt => (Date.today - 15.days) )
+    @statistics.views            .where(:created_at.gt => (15.days.ago) )
                                  .asc(:created_at)
                                  .group_by { |metric| metric.created_at.to_date }
                                  .map{ |date, metrics| { created_at: date, user_count: metrics.uniq(&:identify).length } }
                                  .each{ |stat| @views[stat[:created_at]] = stat[:user_count]; @views_total_count += stat[:user_count] }
 
-    actions = @statistics.actions.where(:created_at.gt => (Date.today - 15.days) )
+    actions = @statistics.actions.where(:created_at.gt => (15.days.ago) )
                                  .asc(:created_at)
                                  .group_by(&:infos)
 
