8581afb7584a8c53b354c43ac09d3cf508a617c7
Aliou Diallo <code@aliou.me>
2014-09-23 17:23:55 +0200

    Make sure you can access and set the media

--- app/models/emailing_section_bridge.rb
+++ app/models/emailing_section_bridge.rb
@@ -1,4 +1,14 @@
 class EmailingSectionBridge < ActiveRecord::Base
+
+  attr_accessible :media_id
+
   belongs_to :structure
   belongs_to :emailing_section
+
+  # Get the Media associated with this bridge.
+  #
+  # @return the Media or nil
+  def media
+    Media.where(id: self.media_id).first
+  end
 end
