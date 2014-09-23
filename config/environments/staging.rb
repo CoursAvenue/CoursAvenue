aaaf654665e30a9bc7909bf451e560efee1ae0b2
Nima Izadi <nim.izadi@gmail.com>
2014-09-23 19:48:07 +0200

    Changed paperclip path

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -80,7 +80,7 @@ CoursAvenue::Application.configure do
     },
     url: ':s3_alias_url',
     s3_host_alias: ENV['PAPERCLIP_S3_HOST_ALIAS'],
-    path: ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
+    path: ":class/:attachment/:id_partition/:style/:filename"
   }
 
   # ------------ Mailer configuration
