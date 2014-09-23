0bd34fcbbcf8e691e494c9345213fef1265e048c
Nima Izadi <nim.izadi@gmail.com>
2014-09-23 23:28:56 +0200

    Added https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ CoursAvenue::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-  config.action_controller.asset_host = "http://cdn%d.coursavenue.com"
+  config.action_controller.asset_host = "https://cdn%d.coursavenue.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( email.css )
