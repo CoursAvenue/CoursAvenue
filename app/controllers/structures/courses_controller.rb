13ca2d418406923e7929dda20d01900d2d1f8cf3
Nima Izadi <nim.izadi@gmail.com>
2014-09-25 16:46:46 +0200

    /!\ Removed not_filtered link on structure profile page

--- app/controllers/structures/courses_controller.rb
+++ app/controllers/structures/courses_controller.rb
@@ -11,13 +11,6 @@ class Structures::CoursesController < ApplicationController
     @plannings            = @planning_search.results
     @courses              = []
 
-    @total_planning_search = PlanningSearch.search({ structure_id: @structure.id,
-                                                     course_types: params[:course_types] || [],
-                                                     is_published: true,
-                                                     visible: true, per_page: 1000 }).results.count
-                                                     # We don't use `.total` here to prevent from missing document
-                                                     # See: https://www.coursavenue.com/etablissements/2802/cours.json?course_types%5B%5D=training for example
-
     @plannings = @plannings.sort do |planning_a, planning_b|
       [planning_a.week_day, planning_a.start_date, planning_a.start_time].compact <=> [planning_b.week_day, planning_b.start_date, planning_b.start_time].compact
     end
@@ -35,7 +28,7 @@ class Structures::CoursesController < ApplicationController
     respond_to do |format|
       # We use courses root to be able to add meta.
       # It's used in the structureProfile backbone app.
-      format.json { render json: { courses: @courses, meta: { total_not_filtered: @total_planning_search  } } }
+      format.json { render json: { courses: @courses } }
       format.html { redirect_to structure_path(@structure)}
     end
   end
