1ff1915fe9a0130eab6c370ea44bdf28410149a5
Aliou Diallo <code@aliou.me>
2014-09-29 11:51:40 +0200

    Store the structure logo by default if it doesn't have any media

--- app/models/emailing_section.rb
+++ app/models/emailing_section.rb
@@ -30,15 +30,29 @@ class EmailingSection < ActiveRecord::Base
 
   # Set the media by default if it isn't already set.
   #
+  # If the Structure doesn't have any media, we store its id and add a flag showing
+  # that we use the logo instead.
+  #
   # @return nothing
   def set_media
     self.structures.each do |structure|
       bridge = media_with_structure(structure)
+
+      if structure.medias.any?
+        is_logo = false
+        media = structure.medias.first.id
+      else
+        is_logo = true
+        media = structure.id
+      end
+
       if bridge.nil?
         self.emailing_section_bridges.create(structure_id: structure.id,
-                                             media_id: structure.medias.first.id)
+                                                 media_id: media,
+                                                  is_logo: is_logo)
       elsif bridge.media_id.nil?
-        bridge.media_id = structure.medias.first.id
+        bridge.media_id = media
+        bridge.is_logo = is_logo
         bridge.save
       end
     end
