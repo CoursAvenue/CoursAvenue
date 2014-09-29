35f020e12568b45b99bf7b9352a7f5f4cf5324c3
Aliou Diallo <code@aliou.me>
2014-09-29 18:04:23 +0200

    Remove boilerplate code.

--- app/assets/javascripts/backbone/emailings/models/emailing_section_bridges_collection.js
+++ app/assets/javascripts/backbone/emailings/models/emailing_section_bridges_collection.js
@@ -2,11 +2,6 @@ Emailing.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.EmailingSectionBridgesCollection = Backbone.Collection.extend({
         model: Module.EmailingSectionBridge,
 
-        url: function () {
-            var id = window.coursavenue.bootstrap.id;
-            return Routes.sections_pro_emailing_path(id);
-        },
-
         parse: function (data) {
             return data;
         }
