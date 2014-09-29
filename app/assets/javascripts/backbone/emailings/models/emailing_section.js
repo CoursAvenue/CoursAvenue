bb3f3b1043e27e849afd2cc5891f32394025271d
Aliou Diallo <code@aliou.me>
2014-09-29 18:35:17 +0200

    Access the attribute using the `get` method

--- app/assets/javascripts/backbone/emailings/models/emailing_section.js
+++ app/assets/javascripts/backbone/emailings/models/emailing_section.js
@@ -1,7 +1,7 @@
 Emailing.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.EmailingSection = Backbone.Model.extend({
         initialize: function initialize () {
-            var bridge_collection = new Module.EmailingSectionBridgesCollection(this.attributes.emailing_section_bridges);
+            var bridge_collection = new Module.EmailingSectionBridgesCollection(this.get('emailing_section_bridges'));
         }
     });
 });
