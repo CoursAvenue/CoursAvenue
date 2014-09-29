90a7588c68233229b1d326e2d3c40641e7128439
Aliou Diallo <code@aliou.me>
2014-09-29 12:36:38 +0200

    Pass the emailing sections to the backbone application

--- app/assets/javascripts/backbone/emailings/application/emailing.js
+++ app/assets/javascripts/backbone/emailings/application/emailing.js
@@ -1,7 +1,5 @@
 Emailing = new Backbone.Marionette.Application({ slug: 'emailings' });
 
-// here was have to use Emailing rather than slug, because "this" will be window
-// I think...
 Emailing.addRegions({
     mainRegion: '#emailings-region'
 });
@@ -14,48 +12,19 @@ Emailing.addInitializer(function(options) {
     var emailing_sections_collection_view = new Emailing.Views.EmailingSectionsCollection.EmailingSectionsCollectionView({
         collection: EmailingSection,
         events: {
-            'EmailingSection:go':     'someMethod'
         }
     });
 
 
     var layout           = new Emailing.Views.EmailingSectionsLayout();
 
-    /* code to demonstrate initializing some submodules to be added to the layout here */
-    // var SubModules = Emailing.Views.WidgetsCollection.SubModules;
-
-    // var submodule               = new SubModules.SubModule({});
-    // var submodule_with_events   = new SubModules.SubModuleWithEvents({});
-    // var submodule_with_selector = new SubModules.SubModuleWithSelector({});
-
-    // Emailing.mainRegion.show(layout);
-
-    /* we can add a widget along with a callback to be used
-     * for setup */
-    //layout.showWidget(submodule_with_events, {
-    //    events: {
-    //        'some:event':               'aMethod orTwo',
-    //    }
-    //});
-
-    //layout.showWidget(submodule);
-
-    //layout.showWidget(submodule_with_selector, {
-    //    events: {
-    //        'some:event':               'aMethod orTwo',
-    //    },
-    //    selector: '[data-type=something-weird]'
-    //});
 
     Emailing.mainRegion.show(layout);
-    layout.master.show(emailing_sections_collection_view); // shouldn't be "results" should be app specific
-    debugger
+    layout.master.show(emailing_sections_collection_view);
 });
 
 $(document).ready(function() {
-    /* we only want Emailing on the correct page */
     if (Emailing.detectRoot()) {
         Emailing.start({});
     }
-
 });
