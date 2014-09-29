e073e1ef3105bb46458183bd35dcf2766657d58c
Aliou Diallo <code@aliou.me>
2014-09-29 16:52:44 +0200

    Fetch the EmailingSectionBridges from the attributes

--- app/assets/javascripts/backbone/emailings/models/emailing_section.js
+++ app/assets/javascripts/backbone/emailings/models/emailing_section.js
@@ -1,7 +1,7 @@
 Emailing.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.EmailingSection = Backbone.Model.extend({
         initialize: function initialize () {
-            var bridge_collection = new Module.EmailingSectionBridgesCollection(this.emailing_section_bridges);
+            var bridge_collection = new Module.EmailingSectionBridgesCollection(this.attributes.emailing_section_bridges);
         }
     });
 });
