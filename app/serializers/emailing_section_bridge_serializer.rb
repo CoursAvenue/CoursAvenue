cd7029a9c46c3b75c503ce8483db39c7dd790874
Aliou Diallo <code@aliou.me>
2014-09-30 17:41:43 +0200

    Also return the video thumbnails with the images

--- app/serializers/emailing_section_bridge_serializer.rb
+++ app/serializers/emailing_section_bridge_serializer.rb
@@ -1,9 +1,16 @@
 class EmailingSectionBridgeSerializer < ActiveModel::Serializer
   attributes :media_id, :media_url, :images
 
+  # Simplify the images to return.
+  #
+  # @return An Array of Hashes containing some images attributes.
   def images
-    object.structure.medias.images.map do |media|
-      { id: media.id, url: media.image.url }
+    object.structure.medias.map do |media|
+      if media.type == 'Media::Video'
+        { id: media.id, url: media.thumbnail_url }
+      else
+        { id: media.id, url: media.image.url }
+      end
     end
   end
 end
