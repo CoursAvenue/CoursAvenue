c6713f60e9477b5a76e942cc38e5d3afe27e8f0d
Aliou Diallo <code@aliou.me>
2014-09-30 18:05:55 +0200

    Animate photo chooser.

--- app/assets/javascripts/backbone/emailings/models/bridge.js
+++ app/assets/javascripts/backbone/emailings/models/bridge.js
@@ -1,7 +1,8 @@
 Emailing.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.Bridge = Backbone.Model.extend({
         initialize: function initialize () {
-        }
+            this.on('change', this.save);
+        },
     });
 });
 
--- app/assets/javascripts/backbone/emailings/views/sections/bridges/bridge_view.js
+++ app/assets/javascripts/backbone/emailings/views/sections/bridges/bridge_view.js
@@ -8,7 +8,43 @@ Emailing.module('Views.Sections.Bridges', function(Module, App, Backbone, Marion
             'width': '33%'
         },
 
+        events: {
+            'click [data-slide-next]': 'slideNext',
+            'click [data-slide-prev]': 'slidePrev'
+        },
+
         initialize: function initialize () {
+          this.model.on('change', this.render);
+        },
+
+        slideNext: function slideNext (event) {
+            var images = this.model.get('images');
+            var current   = _.find(images, function(image) {
+                return (image.id == this.model.get('media_id'));
+            }.bind(this));
+
+            var next = images[images.indexOf(current) + 1];
+
+            this.setCurrentImage(next);
+        },
+
+        slidePrev: function slideNext () {
+            var images = this.model.get('images');
+            var current   = _.find(images, function(image) {
+                return (image.id == this.model.get('media_id'));
+            }.bind(this));
+
+            var prev = images[images.indexOf(current) - 1];
+
+            this.setCurrentImage(prev);
+        },
+
+        setCurrentImage: function setCurrentImage (image) {
+          if (image) {
+              console.log(image);
+              this.model.set( { media_url: image.url, media_id: image.id } );
+          }
+          // AJAX request changing the image.
         },
     });
 });
