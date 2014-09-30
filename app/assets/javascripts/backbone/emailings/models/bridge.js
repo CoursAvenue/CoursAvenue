51f22f0ce00bb95ecd57bc3996ee31545967ed26
Aliou Diallo <code@aliou.me>
2014-09-30 18:49:58 +0200

    Define the URL to use to update the Bridge models

--- app/assets/javascripts/backbone/emailings/models/bridge.js
+++ app/assets/javascripts/backbone/emailings/models/bridge.js
@@ -3,6 +3,11 @@ Emailing.module('Models', function(Module, App, Backbone, Marionette, $, _) {
         initialize: function initialize () {
             this.on('change', this.save);
         },
+
+        url: function url () {
+            var id = window.coursavenue.bootstrap.id;
+            return Routes.bridge_update_pro_emailing_path(id)
+        },
     });
 });
 
