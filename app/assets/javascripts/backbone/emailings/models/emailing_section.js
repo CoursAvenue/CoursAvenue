6cf6cbeaf96c2179e45a41edb5edbc42e3306cbb
Aliou Diallo <code@aliou.me>
2014-09-30 10:04:44 +0200

    Try to show EmailingSectionBridges in the EmailingSections

--- app/assets/javascripts/backbone/emailings/models/emailing_section.js
+++ app/assets/javascripts/backbone/emailings/models/emailing_section.js
@@ -2,6 +2,7 @@ Emailing.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.EmailingSection = Backbone.Model.extend({
         initialize: function initialize () {
             var bridge_collection = new Module.EmailingSectionBridgesCollection(this.get('emailing_section_bridges'));
+            this.set('bridges', bridge_collection);
         }
     });
 });
--- app/assets/javascripts/backbone/emailings/views/emailing_sections_collection/emailing_section_view.js
+++ app/assets/javascripts/backbone/emailings/views/emailing_sections_collection/emailing_section_view.js
@@ -1,12 +1,19 @@
 Emailing.module('Views.EmailingSectionsCollection', function(Module, App, Backbone, Marionette, $, _) {
 
-    Module.EmailingSectionView = Backbone.Marionette.ItemView.extend({
+    Module.EmailingSectionView = Backbone.Marionette.CompositeView.extend({
         template: Module.templateDirname() + 'emailing_section_view',
         tagName: 'div',
         className: 'emailing-section',
         itemViewContainer: '[data-type=container]',
         attributes: {
             'data-behavior': 'emailing-section'
-        }
+        },
+        initialize: function initialize () {
+            new Module.EmailingSectionBridgesCollectionView(this.model.get('bridges'));
+            debugger
+        },
+
+        // render: 
+
     });
 });
