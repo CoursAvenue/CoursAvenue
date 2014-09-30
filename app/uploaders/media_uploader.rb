ddb58e6f4b188276c45df5364115711b2e4a8208
Nima Izadi <nim.izadi@gmail.com>
2014-09-30 17:50:51 +0200

    Added image missing in up loaders

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -7,12 +7,9 @@ class MediaUploader < CarrierWave::Uploader::Base
   # process resize_to_fit: [1600]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    ActionController::Base.helpers.asset_path("images/" + [version_name, "missing.png"].compact.join('/'))
+  end
 
   # Create different versions of your uploaded files:
   version :original do
--- app/uploaders/structure_logo_uploader.rb
+++ app/uploaders/structure_logo_uploader.rb
@@ -3,12 +3,9 @@ class StructureLogoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    ActionController::Base.helpers.asset_path("logos/" + [version_name, "missing.png"].compact.join('/'))
+  end
 
   cloudinary_transformation :transformation => [{  width: 600, height: 600, crop: :pad }]
   process convert: "jpg"
