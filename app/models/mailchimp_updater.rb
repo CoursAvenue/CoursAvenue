a97d4122da3aba0f7efc2240c334cf740c063520
Nima Izadi <nim.izadi@gmail.com>
2014-10-04 09:53:44 +0200

    Updated Mailchimp updater for structures

--- app/models/mailchimp_updater.rb
+++ app/models/mailchimp_updater.rb
@@ -22,19 +22,13 @@ class MailchimpUpdater
   end
 
   def self.update_structure(structure)
-    Gibbon::API.lists.subscribe({id: '34fb5a48e8',
+    Gibbon::API.lists.subscribe({id: '79f30bcce9',
                                  email: { email: user.email},
                                  merge_vars: {
-                                   :FNAME      => user.first_name.try(:capitalize),
-                                   :LNAME      => user.last_name,
-                                   :GENDER     => user.gender,
-                                   :CITY       => user.city.try(:name),
-                                   :ZIPCODE    => user.zip_code,
-                                   :REGION     => user.city.try(:region_name),
-                                   :EMAILSTAT  => user.delivery_email_status,
-                                   :ID         => user.id,
-                                   :GROUP      => user.id.modulo(6),
-                                   :SLEEPING   => (user.active? ? 'Oui' : 'Non')
+                                   :FAME      => structure.name,
+                                   :PARISIAN  => (structure.parisian? ? 'Oui' : 'Non'),
+                                   :JPO       => (structure.courses.open_courses.any? ? 'Oui' : 'Non'),
+                                   :SLEEPING  => (structure.is_sleeping? ? 'Oui' : 'Non')
                                  },
                                    double_optin: false,
                                    update_existing: true
