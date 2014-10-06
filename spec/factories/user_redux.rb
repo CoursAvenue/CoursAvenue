df31179e15695f613a021c80d4e77f00f66f3ab4
Aliou Diallo <code@aliou.me>
2014-10-06 16:03:04 +0200

    Update tests

--- spec/factories/user_redux.rb
+++ spec/factories/user_redux.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 
-FactoryGirl.define do :user_redux do
-  email        { Faker::Internet.email }
-  confirmed_at nil
+FactoryGirl.define do
+  factory :user_redux do
+    email        { Faker::Internet.email }
+    confirmed_at nil
+  end
 end
