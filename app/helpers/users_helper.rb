09da199205f1dc61bcd52f5fcc87d34a123d6721
Aliou Diallo <code@aliou.me>
2014-10-07 14:18:32 +0200

    Create helper method to create share URL.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,19 @@
 # encoding: utf-8
 module UsersHelper
 
+  # Creates a URL to a Facebook Send Dialog.
+  # More information: <https://developers.facebook.com/docs/sharing/reference/send-dialog>
+  #
+  # @param user      — The User sending the message.
+  # @param friend_id — The friend Facebook ID.
+  #
+  # @return The Send Dialog URL String.
+  def share_sponsorship_request_to_facebook_friend_url(user, friend_id)
+    link = user.sponsorship_slug
+    redirect_uri = user.sponsorship_slug
+    URI.encode("http://www.facebook.com/dialog/send?app_id=#{CoursAvenue::Application::FACEBOOK_APP_ID}&link=#{link}&redirect_uri=#{redirect_uri}&to=#{friend_id}")
+  end
+
   def facebook_friends user
 
   end
