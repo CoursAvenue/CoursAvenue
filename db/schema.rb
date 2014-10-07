e8e4b0ca03fb74382902c60788d99ac70e19ba5b
Aliou Diallo <code@aliou.me>
2014-10-07 11:12:10 +0200

    Create the promo code just before the validation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006134756) do
+ActiveRecord::Schema.define(version: 20141007083941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -771,6 +771,7 @@ ActiveRecord::Schema.define(version: 20141006134756) do
     t.boolean  "registered"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "promo_code"
   end
 
   create_table "statistics", force: true do |t|
