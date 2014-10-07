2d9ce11520b7de6bd696c5d7499e780296968c4d
Aliou Diallo <code@aliou.me>
2014-10-07 16:21:29 +0200

    Also store sponsorship if any, to reuse later

--- app/controllers/sponsorships_controller.rb
+++ app/controllers/sponsorships_controller.rb
@@ -3,9 +3,11 @@ class SponsorshipsController < ApplicationController
   # GET obtenir-mon-passe-decouverte/:slug/
   def show
     @sponsor = User.where(sponsorship_slug: params[:id]).first
+    @sponsorship = nil
+
     if @sponsor.nil?
-      sponsorship = Sponsorship.where(promo_code: params[:id]).first
-      @sponsor = sponsorship.user if sponsorship.present?
+      @sponsorship = Sponsorship.where(promo_code: params[:id]).first
+      @sponsor = @sponsorship.user if @sponsorship.present?
     end
 
   end
