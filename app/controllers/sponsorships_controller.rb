1b36d82ad5e5450f95e2e27062c1c7c4d75e21d5
Aliou Diallo <code@aliou.me>
2014-10-07 15:41:55 +0200

    Create sponsorship controller

---
+++ app/controllers/sponsorships_controller.rb
@@ -0,0 +1,12 @@
+class SponsorshipsController < ApplicationController
+
+  # GET obtenir-mon-passe-decouverte/:slug/
+  def show
+    @sponsor = User.where(sponsorship_slug: params[:id]).first
+    if @sponsor.nil?
+      sponsorship = Sponsorship.where(promo_code: params[:id]).first
+      @sponsor = sponsorship.user if sponsorship.present?
+    end
+
+  end
+end
