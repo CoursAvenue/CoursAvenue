1122988d50bc64390e90735bf1dee6bda27a04cb
Aliou Diallo <code@aliou.me>
2014-10-07 10:06:15 +0200

    Skip validation for sponsored user in spec

--- spec/models/sponsorship_spec.rb
+++ spec/models/sponsorship_spec.rb
@@ -2,5 +2,9 @@ require 'spec_helper'
 
 describe Sponsorship, :type => :model do
   let (:user)           { FactoryGirl.create(:user) }
-  let (:sponsored_user) { FactoryGirl.create(:user_redux) }
+  let (:sponsored_user) {
+    user = FactoryGirl.create(:user_redux)
+    user.save(validate: false)
+    user
+  }
 end
