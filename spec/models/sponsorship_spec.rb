85b38977ceb7148d45a2bbe1f782e498775259a9
Aliou Diallo <code@aliou.me>
2014-10-07 10:37:16 +0200

    Add promo code test in Sponsorship spec

--- spec/models/sponsorship_spec.rb
+++ spec/models/sponsorship_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
 describe Sponsorship, :type => :model do
-  let (:user)           { FactoryGirl.create(:user) }
-  let (:sponsored_user) {
-    user = FactoryGirl.create(:user_redux)
-    user.save(validate: false)
-    user
-  }
+  context 'callbacks' do
+    describe '#set promo_code' do
+      let (:user)           { FactoryGirl.create(:user) }
+      let (:sponsored_user) {
+        user = FactoryGirl.build(:user_redux)
+        user.save(validate: false)
+        user
+      }
+
+      it 'sets the promo code after the sponsorship is created' do
+        sponsorship = user.sponsorships.create(sponsored_user: sponsored_user)
+
+        expect(sponsorship.promo_code).to_not be_nil
+      end
+    end
+  end
 end
