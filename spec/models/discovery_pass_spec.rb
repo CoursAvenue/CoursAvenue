263c01aea7820a41bdb594a5cd81e20c3249ba8d
Aliou Diallo <code@aliou.me>
2014-10-08 11:39:17 +0200

    Add DiscoveryPass spec

---
+++ spec/models/discovery_pass_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe DiscoveryPass, :type => :model do
+  context :sponsorship do
+    describe '#apply_sponsorship_promo' do
+      let (:user)           { FactoryGirl.create(:user) }
+      let (:sponsored_user) { FactoryGirl.create(:user) }
+      let (:sponsorship)    { user.sponsorships.create(sponsored_user: sponsored_user) }
+
+      it 'applies the sponsorship promo' do
+        sponsored_user.discovery_passes.create
+
+        expect(sponsorship.promo_code_used).to be true
+      end
+
+      it "updates the sponsor's credit" do
+        expect { sponsored_user.discovery_passes.create }.to change { user.sponsorship_credit }.by(Sponsorship::SPONSOR_CREDIT)
+      end
+
+      it "updates the sponsored user's credit" do
+        expect { sponsored_user.discovery_passes.create }.to change { sponsored_user.sponsorship_credit }.by(Sponsorship::SPONSORED_CREDIT)
+      end
+    end
+  end
+end
