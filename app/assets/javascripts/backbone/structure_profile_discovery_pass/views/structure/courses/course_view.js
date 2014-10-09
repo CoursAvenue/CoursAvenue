a70ffc54f56ad1d7ed0f9a6437d1ef0df009f704
Nima Izadi <nim.izadi@gmail.com>
2014-10-09 13:59:12 +0200

    Improved views

--- app/assets/javascripts/backbone/structure_profile_discovery_pass/views/structure/courses/course_view.js
+++ app/assets/javascripts/backbone/structure_profile_discovery_pass/views/structure/courses/course_view.js
@@ -5,8 +5,16 @@ StructureProfileDiscoveryPass.module('Views.Structure.Courses', function(Module,
         template: Module.templateDirname() + 'course_view',
         itemView: Module.Plannings.PlanningView,
 
+        events: {
+            "click [data-behavior=register]": 'registerToCourse'
+        },
+
         onItemviewRegister: function onItemviewRegister (view, data) {
             this.trigger("register", data);
+        },
+
+        registerToCourse: function registerToCourse (view, data) {
+            this.trigger("register", { course_id: this.model.get('id') });
         },
 
     });
