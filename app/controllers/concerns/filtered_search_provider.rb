a6a53dca61c6f3e5a4765d5535712fe5c875356e
Nima Izadi <nim.izadi@gmail.com>
2014-10-11 02:45:11 +0200

    Improved discovery pass page

--- app/controllers/concerns/filtered_search_provider.rb
+++ app/controllers/concerns/filtered_search_provider.rb
@@ -59,10 +59,10 @@ module FilteredSearchProvider
     structures   = []
     places       = []
     place_search.group(:place_id_str).groups.each do |place_group|
-      places << place_group.results.first.try(:place_id)
+      places << place_group.value
     end
-    search.group(:structure_id_str).groups.each do |planning_group|
-      structures << planning_group.results.first.try(:structure)
+    search.group(:structure_id_str).groups.each do |structure_group|
+      structures << Structure.find(structure_group.value)
     end
     places = places.uniq
     total  = search.group(:structure_id_str).total
--- app/decorators/structure_decorator.rb
+++ app/decorators/structure_decorator.rb
@@ -9,8 +9,9 @@ class StructureDecorator < Draper::Decorator
     else
       # Use find_by_id to prevent from exception when place is not find.
       # In this case: http://www.coursavenue.dev/etablissements/voix-et-voie/pass-decouverte place wasn't found...
-      places = (place_ids ? place_ids.map{|place_id| Place.find_by_id(place_id)} : object.places).compact
+      places = (place_ids ? place_ids.map{|place_id| Place.find_by_id(place_id)} : object.places)
       places.each do |place|
+        next if place.nil?
         output << "<div class='push-half--bottom'><strong>#{place.name}</strong><br>#{place.street}, #{place.city.name}</div>"
       end
     end
