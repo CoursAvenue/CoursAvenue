6d3e2d0000df46d44b7fd341d8efb942302a615d
Nima Izadi <nim.izadi@gmail.com>
2014-10-24 12:04:26 +0200

    Updated migration for rollback

--- db/migrate/20141022090449_rename_discovery_pass_policy_to_trial_courses.rb
+++ db/migrate/20141022090449_rename_discovery_pass_policy_to_trial_courses.rb
@@ -1,7 +1,13 @@
 class RenameDiscoveryPassPolicyToTrialCourses < ActiveRecord::Migration
-  def change
+  def up
     rename_column :structures, :discovery_pass_policy, :trial_courses_policy
-    remove_column :plannings, :available_in_discovery_pass
     rename_column :courses, :available_in_discovery_pass, :is_open_for_trial
+    remove_column :plannings, :available_in_discovery_pass
+  end
+
+  def down
+    rename_column :structures, :trial_courses_policy, :discovery_pass_policy
+    rename_column :courses, :is_open_for_trial, :available_in_discovery_pass
+    add_column :plannings, :available_in_discovery_pass, :boolean
   end
 end
