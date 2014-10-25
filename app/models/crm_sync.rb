de1c964e43a7ab9ba13c3b64b6d70df2e8264701
Nima Izadi <nim.izadi@gmail.com>
2014-10-26 01:11:17 +0200

    Updated info

--- app/models/crm_sync.rb
+++ app/models/crm_sync.rb
@@ -25,7 +25,7 @@ class CrmSync
     person.set_field_value("# avis"                         , structure.comments_count)
     person.set_field_value('# de cours actifs'              , structure.courses.select(&:is_published?).length)
     person.set_field_value('# de photos/vidéos'             , structure.medias.count)
-    person.set_field_value('Dernière connexion à son profil', I18n.l(admin.last_sign_in_at, format: :date)) if admin and admin.last_sign_in_at.present?
+    person.set_field_value('Dernière connexion à son profil', I18n.l(admin.current_sign_in_at, format: :date)) if admin and admin.current_sign_in_at.present?
     person.set_field_value('ID'                             , "https://coursavenue1.highrisehq.com/people/#{person.id}")
     person.set_field_value('JPO'                            , structure.courses.open_courses.count)
     person.set_field_value('Pass Decouverte Policy'         , structure.discovery_pass_policy)
