f28d9f9e36e150b1aedcd9785a373d10efb8252f
Aliou Diallo <code@aliou.me>
2014-10-29 11:10:45 +0100

    Also destroy relations when destroying a section

--- app/models/faq/section.rb
+++ app/models/faq/section.rb
@@ -3,7 +3,7 @@ class Faq::Section < ActiveRecord::Base
 
   attr_accessible :title, :questions, :questions_attributes
 
-  has_many :questions, class_name: 'Faq::Question', foreign_key: 'faq_section_id'
+  has_many :questions, class_name: 'Faq::Question', foreign_key: 'faq_section_id', dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: :reject_question, allow_destroy: true
 
   validates :title, presence: true
