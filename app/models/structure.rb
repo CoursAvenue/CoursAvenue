63562d01bdf77c37ced2ef76badb612b91f50357
Nima Izadi <nim.izadi@gmail.com>
2014-11-01 22:04:03 +0100

    Add is sleeping for reindex

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -173,7 +173,15 @@ class Structure < ActiveRecord::Base
   # Algolia                                                            #
   ######################################################################
   algoliasearch do
-    attribute :name, :slug, :search_score
+    attribute :name, :slug
+    add_attribute :search_score do
+      self.search_score.try(:to_i)
+    end
+
+    add_attribute :is_sleeping do
+      self.is_sleeping?
+    end
+
     add_attribute :type do
       'structure'
     end
@@ -184,7 +192,7 @@ class Structure < ActiveRecord::Base
     add_attribute :logo_url do
       self.logo.url(:small_thumb)
     end
-    customRanking ['desc(search_score)']
+    customRanking ['desc(search_score)', 'desc(is_sleeping)']
   end
 
   ######################################################################
