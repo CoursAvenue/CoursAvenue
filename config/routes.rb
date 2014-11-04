942b5756830e8199251fb626b3ed2fcf2c9013b8
Aliou Diallo <code@aliou.me>
2014-11-04 15:25:56 +0100

    Add temporary email processor route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ CoursAvenue::Application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
   mount_griddler
+  get "/email_processor", to: proc { [200, {}, ["OK"]] }, as: "mandrill_head_test_request"
   get '/robots.txt' => 'home#robots'
   # ---------------------------------------------
   # ----------------------------------------- PRO
