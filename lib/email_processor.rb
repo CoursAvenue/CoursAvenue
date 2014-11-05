7d82f17d4dc22df4565f6d15b3a90b3b27655041
Aliou Diallo <code@aliou.me>
2014-11-05 11:20:18 +0100

    Make griddler process participation requests

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -3,14 +3,47 @@ class EmailProcessor
     @email = email
   end
 
-  # Process the received email.
-  # We start by finding who sent the email.
+  # Process the reply received to a notification email.
+  #
+  # In this method, we only parse the reply type and then delegate the actions
+  # to other methods: ex: participation_request -> process_participation_request
+  # @return nothing
   def process
-    user      = User.where(email: @email.from[:email]).first
-    structure = Structure.first # Find the structure by parsing the headers.
+    Bugsnag.notify(RuntimeError.new("EmailProcessor#process"), {
+      email_data: {
+        json: @email.to_json,
+        to: @email.to.first[:token],
+        body: @email.body
+      }
+    })
 
-    UserMailer.delay.welcome(user) unless user.nil?
+    token = ReplyToken.find @email.to.first[:token]
+
+    case token.reply_type
+    when 'participation_request'
+      process_participation_request(token)
+    when 'conversation'
+      process_conversation(token)
+    end
   end
 
   private
+
+  def process_participation_request(reply_token)
+    pr = ParticipationRequest.find reply_token.participation_request_id.to_i
+
+    if reply_token.sender_type.downcase == 'admin'
+      sender = Admin.find reply_token.sender_id.to_i
+    else
+      sender = User.find reply_token.sender_id.to_i
+    end
+
+    message = @email.body
+    if reply_token.sender_type == 'admin'
+      pr.accept!(message, 'Structure')
+    end
+  end
+
+  def process_conversation(reply_token)
+  end
 end
