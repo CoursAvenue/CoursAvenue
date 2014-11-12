68934380f4f377a02de6bf43a2061986b2248ac4
Aliou Diallo <code@aliou.me>
2014-11-12 11:54:09 +0100

    Send the right HTTP code to google.

--- app/controllers/reply_token_contoller.rb
+++ app/controllers/reply_token_contoller.rb
@@ -1,5 +1,26 @@
 class ReplyTokenController < ApplicationController
+
+  # This is the route Google will use when a user clicks on the email action.
+  #
+  # On success, it should return a response code of 200.
+  # Otherwise, it should return:
+  # * 400 for a bad request           => Google will fail the action.
+  # * 401 for an unauthorized request => Google will fail the action.
+  # * 404 for a not found request     => Google will fail the action.
+  # * 408 for a request timeout       => Google will retry again later.
+  #
+  # We start by checking the validaty of the token, and then execute the
+  # actions.
   def show
-    @reply_token = ReplyToken.find params[:id]
+    # @reply_token = ReplyToken.find params[:id]
+    @reply_token = ReplyToken.where(token: params[:id]).first
+    head :not_found if @reply_token.nil?
+
+    if @token.still_valid?
+      @token.use!
+      head :ok
+    else
+      head :unauthorized
+    end
   end
 end
