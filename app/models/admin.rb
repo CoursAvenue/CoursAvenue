493b162d29cd7576bd82e340b184a3b3284a6f23
Aliou Diallo <code@aliou.me>
2014-11-26 13:02:24 +0100

    Access the sms_opt_in field in Admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -24,7 +24,8 @@ class ::Admin < ActiveRecord::Base
                   :structure_id,
                   :email_opt_in,
                   :student_action_email_opt_in, :newsletter_email_opt_in,
-                  :monday_email_opt_in, :jpo_email_opt_in, :stats_email, :delivery_email_status
+                  :monday_email_opt_in, :jpo_email_opt_in, :stats_email, :delivery_email_status,
+                  :sms_opt_in
 
   store_accessor :email_opt_in_status, :student_action_email_opt_in, :newsletter_email_opt_in,
                                        :monday_email_opt_in, :jpo_email_opt_in, :stats_email
