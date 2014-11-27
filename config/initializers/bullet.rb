0f292cc55418ec092d4ef226d7885e6f8b094975
Nima Izadi <nim.izadi@gmail.com>
2014-11-27 16:57:43 +0100

    Removed bullet footer

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -5,7 +5,7 @@ if Rails.env.development?
       Bullet.bullet_logger = true
       Bullet.console       = true
       Bullet.rails_logger  = true
-      Bullet.add_footer    = true
+      # Bullet.add_footer    = true
     end
   end
 end
