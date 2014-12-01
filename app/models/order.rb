2c32059130c3676440395f98a13ffc8eeb586dfa
Aliou Diallo <code@aliou.me>
2014-12-01 15:35:21 +0100

    Upload the invoice to S3 after create.

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,31 @@ class Order < ActiveRecord::Base
   ######################################################################
   # Callbacks                                                          #
   ######################################################################
+  after_create :upload_invoice
+
+  ######################################################################
+  # Validations                                                        #
+  ######################################################################
   validates :order_id, uniqueness: { scope: :type }
+
+  private
+
+  # Export an order and upload it to S3.
+  #
+  # @return The URL of the exported order.
+  def upload_invoice
+    @order       = self
+    @structure   = self.structure
+
+    file    = CoursAvenue::Application::S3_BUCKET.objects["#{ invoice_path }"]
+    invoice = ApplicationController.new.render_to_string('pro/structures/orders/export.pdf.haml',
+                                                         pdf:    "#{ invoice_path }",
+                                                         layout: 'pdf.html.haml',
+                                                         locals: { :@order => @order, :@structure => @structure } )
+    pdf = WickedPdf.new.pdf_from_string(invoice)
+    file.write(pdf)
+    file.url_for(:read).to_s
+  end
+  handle_asynchronously :upload_invoice
 
 end
