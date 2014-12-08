110669c544eecbd6cbfc36c575bb0f65c7e89712
Aliou Diallo <code@aliou.me>
2014-12-08 15:54:47 +0100

    Include rails_helper instead of spec_helper

--- spec/controllers/pro/structures/comment_notifications_controller.rb
+++ spec/controllers/pro/structures/comment_notifications_controller.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "rails_helper"
 
 describe Pro::Structures::CommentNotificationsController do
   let(:admin) { FactoryGirl.create(:admin) }
--- spec/models/sponsorship_spec.rb
+++ spec/models/sponsorship_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Sponsorship, :type => :model do
   context 'callbacks' do
