d99b6ca70a1314cb5cd68a442f12e62373e3f83b
Aliou Diallo <code@aliou.me>
2014-12-08 16:22:59 +0100

    Write test for Admin#facebook_pages

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -11,4 +11,29 @@ describe Admin do
       expect(admin.super_admin).to be false
     end
   end
+
+  context :facebook do
+    describe 'Connection with Facebook' do
+      describe '#facebook_pages' do
+
+        context 'not connected from Facebook' do
+          let(:admin) { FactoryGirl.build(:admin) }
+          it 'should return nothing' do
+            expect(admin.facebook_pages).to be_empty
+          end
+        end
+
+        context 'connected from Facebook' do
+          context 'without any pages' do
+            let(:admin) { FactoryGirl.build(:admin_from_facebook) }
+
+            it 'should return the pages administrated by the admin' do
+              expect(admin.facebook_pages).not_to be_empty
+            end
+          end
+        end
+
+      end
+    end
+  end
 end
