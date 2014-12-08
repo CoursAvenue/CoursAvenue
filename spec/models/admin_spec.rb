025f1dbd3f8364f67e696d6d1a7461a31f004ef8
Aliou Diallo <code@aliou.me>
2014-12-08 17:30:19 +0100

    Stub FbGraph::User in spec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -30,6 +30,9 @@ describe Admin do
             let(:admin) { FactoryGirl.build(:admin_from_facebook) }
 
             it 'should return the pages administrated by the admin' do
+              allow(FbGraph::User).to receive(:fetch)
+                .and_return(Fb_User.new(['CA', 'http://facebook.com/CoursAvenue']))
+
               expect(admin.facebook_pages).not_to be_empty
             end
           end
