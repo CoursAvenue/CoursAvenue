a18548d9b8b7e16011f36e3da5a2dd493790ac8c
Aliou Diallo <code@aliou.me>
2014-12-10 12:06:56 +0100

    Move Mailer spec config in its own file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 
-require 'rails_helper'
+require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
@@ -14,10 +14,8 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
 
 RSpec.configure do |config|
-
   # Immediately run the worker jobs.
   Delayed::Worker.delay_jobs = false
 
--- spec/tasks/scheduler/participation_request_reminder_spec.rb
+++ spec/tasks/scheduler/participation_request_reminder_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'mailer_helper'
 require 'rake'
 
 CoursAvenue::Application.load_tasks
