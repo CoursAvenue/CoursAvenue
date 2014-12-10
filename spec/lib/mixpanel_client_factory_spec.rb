3804e46aa513dc65c88d069820b9034c4f2dd44e
Aliou Diallo <code@aliou.me>
2014-12-10 17:11:00 +0100

    Test whether we're in production instead of test env

--- spec/lib/mixpanel_client_factory_spec.rb
+++ spec/lib/mixpanel_client_factory_spec.rb
@@ -4,22 +4,22 @@ describe MixpanelClientFactory, '' do
 
   it { expect(Rails.env).to be_test }
 
-  it 'returns a Fake client in test mode' do
+  it 'returns a Fake client in non production env' do
     fake_client = FakeMixpanel::Tracker
 
     expect(MixpanelClientFactory.client).to be_a(fake_client)
   end
 
-  it 'returns a real client when in non-test mode' do
+  it 'returns a real client when in production env' do
     real_client = Mixpanel::Tracker
 
-    switch_to_non_test_mode do
+    switch_to_prod_env do
       expect(MixpanelClientFactory.client).to be_a(real_client)
     end
   end
 
-  def switch_to_non_test_mode
-    Rails.env = 'non-test'
+  def switch_to_prod_env
+    Rails.env = 'production'
     yield
   ensure
     Rails.env = 'test'
