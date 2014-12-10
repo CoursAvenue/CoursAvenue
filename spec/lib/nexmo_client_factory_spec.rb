4aa4026f62bd27e1eb8abfb804c77bbfcea7f1c0
Aliou Diallo <code@aliou.me>
2014-12-10 17:30:57 +0100

    Create FakeNexmo module

---
+++ spec/lib/nexmo_client_factory_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe NexmoClientFactory, '' do
+  it { expect(Rails.env).to be_test }
+
+  it 'returns a fake client in non production env' do
+    fake_client = FakeNexmo::Client
+
+    expect(NexmoClientFactory.client).to be_a(fake_client)
+  end
+
+  it 'returns a real client in production env' do
+    real_client = Nexmo::Client
+
+    expect(NexmoClientFactory.client).to be_a(real_client)
+  end
+
+  def switch_to_prod_env
+    Rails.env = 'production'
+    yield
+  ensure
+    Rails.env = 'test'
+  end
+end
