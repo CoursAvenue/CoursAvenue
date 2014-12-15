25fc609cf5422469d0ccf391f2e8b5400ecac3f3
Aliou Diallo <code@aliou.me>
2014-12-15 18:59:55 +0100

    Make sure the reply token is unique

--- spec/models/reply_token_spec.rb
+++ spec/models/reply_token_spec.rb
@@ -1,5 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe ReplyToken, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe ReplyToken, :type => :model do
+  context 'unique token' do
+    let(:reply_token_1) { ReplyToken.create(reply_type: 'conversation') }
+
+    it 'is not valid if the token already exists' do
+      reply_token_2 = ReplyToken.create(token: reply_token_1.token, reply_type: 'conversation')
+
+      expect(reply_token_2).to be_invalid
+    end
+  end
+end
