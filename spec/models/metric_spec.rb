207116ade7b55049cb81baa277faf12852cd3bf9
Aliou Diallo <code@aliou.me>
2014-12-15 15:49:42 +0100

    Speed up test

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -2,10 +2,8 @@ require 'rails_helper'
 
 describe Metric do
   context '#create_action' do
-    let (:structure) { FactoryGirl.create(:structure) }
-
     def create_metric
-      metric = Metric.create_action('impression', nil, structure.id, 'abcdef', '127.0.0.1')
+      metric = Metric.create_action('impression', nil, 1234, 'abcdef', '127.0.0.1')
     end
 
     it 'only creates one metric per day per user' do
