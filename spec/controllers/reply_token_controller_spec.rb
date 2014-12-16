c176a555d540b1d1529238af9cb27be19dded58e
Aliou Diallo <code@aliou.me>
2014-12-16 15:33:43 +0100

    Test the user agent authorizations

--- spec/controllers/reply_token_controller_spec.rb
+++ spec/controllers/reply_token_controller_spec.rb
@@ -2,4 +2,13 @@
 require 'rails_helper'
 
 describe ReplyTokenController, type: :controller do
+  describe '#show' do
+    context 'with wrong user agent' do
+      it 'is unauthorized' do
+        get :show, id: Faker::Number.number(2)
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
 end
