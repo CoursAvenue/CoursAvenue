4066da20065ed331d0a55db58f38f5084d1cd6be
Aliou Diallo <code@aliou.me>
2014-12-16 10:16:44 +0100

    Test ReplyToken#still_valid?

--- spec/models/reply_token_spec.rb
+++ spec/models/reply_token_spec.rb
@@ -10,4 +10,57 @@ RSpec.describe ReplyToken, :type => :model do
       expect(reply_token_2).to be_invalid
     end
   end
+
+  context '#still_valid?' do
+    subject { ReplyToken.create(reply_type: 'participation_request') }
+
+    context 'participation_request' do
+      let(:participation_request) { FactoryGirl.create(:participation_request) }
+
+      before do
+        subject.data = {
+          sender_type:              'user',
+          sender_id:                participation_request.user.id,
+          participation_request_id: participation_request.id
+        }
+
+        subject.save
+      end
+
+      context 'when the participation request is pending' do
+        before do
+          participation_request.state = 'accepted'
+          participation_request.save
+        end
+
+        it 'is not valid' do
+          expect(subject.still_valid?).to be false
+        end
+      end
+
+      context 'when the participation request is in the past' do
+        before do
+          participation_request.start_time = 2.days.ago
+          participation_request.save
+        end
+
+        it 'is not valid' do
+          expect(subject.still_valid?).to be false
+        end
+      end
+
+      context "when it hasn't been used yet" do
+
+        before do
+          participation_request.start_time = 2.days.from_now
+          participation_request.save
+        end
+
+        it "is valid when it hasn't been used yet" do
+          expect(subject.still_valid?).to be true
+        end
+      end
+    end
+
+  end
 end
