3cb26a60c5e3e7b0e24fe6918fa71c28e48e6194
Aliou Diallo <code@aliou.me>
2014-12-17 14:09:39 +0100

    Create TestMailer

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,14 +1,27 @@
 class TestMailer < ActionMailer::Base
-  include ::ActionMailerWithTextPart
-  include Roadie::Rails::Automatic
-
-  helper :mailer
-  layout 'email'
-
-  default from: "\"L'équipe CoursAvenue\" <contact@coursavenue.com>"
-  default to:   "\"L'équipe CoursAvenue\" <contact@coursavenue.com>"
+  default from: "aliou.diallo2@gmail.com"
+  default to:   "aliou.diallo2@gmail.com"
 
   def mail_action
+    token       = ReplyToken.create(reply_type: 'conversation',
+                                   data: { gmail_action_name: 'Testing' })
+    @data = confirm_action(token)
+
     mail subject: "Mail action"
+  end
+
+  def confirm_action(token)
+    helper_data = {
+      "@context" => "http://schema.org",
+      "@type" => "EmailMessage",
+      "action" => {
+        "@type" => "ConfirmAction",
+        "name" => "Confirm registration",
+        "handler" => {
+          "@type" => "HttpActionHandler",
+          "url" => "http://google.fr",
+        },
+      },
+    }
   end
 end
