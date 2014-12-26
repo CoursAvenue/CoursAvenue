c7fc9e93ff1dd891b113822a373a5fb05aca1464
Aliou Diallo <code@aliou.me>
2014-12-26 10:11:22 +0100

    Also spec correct video validation behavior

--- spec/models/media/video_spec.rb
+++ spec/models/media/video_spec.rb
@@ -11,5 +11,24 @@ describe Media::Video do
       end
     end
 
+    context 'the url is a video' do
+
+      let(:provider_name) { 'youtube' }
+      subject { Media::Video.create(url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ') }
+
+      it 'saves the video' do
+        expect(subject).to be_valid
+      end
+
+      it 'updates the provider' do
+        expect(subject.provider_id).to_not be_nil
+        expect(subject.provider_name).to eq(provider_name)
+      end
+
+      it 'updates the thumbnail' do
+        expect(subject.thumbnail_url).to_not be_nil
+      end
+    end
+
   end
 end
