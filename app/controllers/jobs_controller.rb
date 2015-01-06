caf0960984dfb5ac37a1abc55dce86bad5cf0a8e
Nima Izadi <nim.izadi@gmail.com>
2015-01-06 19:14:31 +0100

    Improved jobs pages [ci skip]

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,19 @@
 # encoding: utf-8
 class JobsController < ApplicationController
 
-  layout 'empty_body'
+  layout :get_layout
 
   # GET jobs/frontend-developpeur
   def frontend_developper
+  end
+
+  private
+
+  def get_layout
+    if action_name == 'index'
+      'pages'
+    else
+      'empty_body'
+    end
   end
 end
