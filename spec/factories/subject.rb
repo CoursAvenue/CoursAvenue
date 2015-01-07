a575b5fc5ad6ba2482dbfaca36182dd6155c795b
Aliou Diallo <code@aliou.me>
2015-01-07 13:18:13 +0100

    Update Subject factory

--- spec/factories/subject.rb
+++ spec/factories/subject.rb
@@ -16,5 +16,21 @@ FactoryGirl.define do
         subject.ancestry_depth = 2
       end
     end
+
+    factory :subject_with_parent do
+      association :parent, factory: :subject
+    end
+
+    factory :subject_with_grand_parent do
+      association :parent, factory: :subject_with_parent
+    end
+
+    trait :with_needed_material do
+      needed_meterial { Faker::Lorem.paragraph }
+    end
+
+    trait :with_tips do
+      tips { Faker::Lorem.paragraph }
+    end
   end
 end
