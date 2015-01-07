36260434a89352db13a5fb9e3b198d383c3db566
Aliou Diallo <code@aliou.me>
2015-01-07 12:32:15 +0100

    Improve Place coverage

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -2,18 +2,33 @@
 require 'rails_helper'
 
 describe Place do
-  let(:place)     { FactoryGirl.create(:place) }
-  let(:structure) { place.structure }
-  subject {place}
+  subject { FactoryGirl.create(:place) }
 
   it { should be_valid }
 
-  it { expect(place.to_gmap_json[:lat]).to eq(place.latitude) }
-  it { expect(place.to_gmap_json[:lng]).to eq(place.longitude) }
-
-  context 'destroy' do
-    it 'destroys associate plannings' do
+  describe '#main_contact' do
+    it 'returns the first contact' do
+      expect(subject.main_contact).to eq(subject.contacts.first)
     end
   end
 
+  describe '#parisian' do
+    context 'not parisian' do
+      subject { FactoryGirl.create(:place, :not_parisian) }
+      it { expect(subject.parisian?).to be_falsy }
+    end
+
+    context 'parisian' do
+      it { expect(subject.parisian?).to be_truthy }
+    end
+  end
+
+  describe '#to_gmap_json' do
+    it { expect(subject.to_gmap_json[:lat]).to eq(subject.latitude) }
+    it { expect(subject.to_gmap_json[:lng]).to eq(subject.longitude) }
+  end
+
+  describe '#is_home?' do
+    it { expect(subject.is_home?).to be_falsy }
+  end
 end
