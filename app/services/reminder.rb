edeb7ccd9a8679a2eef4de3cde06485b004d11ad
Aliou Diallo <code@aliou.me>
2015-01-07 18:14:17 +0100

    Create reminder service

---
+++ app/services/reminder.rb
@@ -0,0 +1,15 @@
+class ReminderService
+  # Send a reminder depending on the email status of the structure
+  # @param structure The Structure to send the reminder to
+  #
+  # @return nil
+  def self.send_status_reminder(structure)
+    return if structure.main_contact.nil? or structure.is_sleeping?
+
+    if structure.main_contact.monday_email_opt_in? and structure.update_email_status.present?
+      status = structure.email_status
+      structure.update_last_email_sent_status!(status)
+      AdminMailer.delay.send(status.to_sym, structure)
+    end
+  end
+end
---
+++ spec/services/reminder_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe ReminderService do
+  describe '.send_status_reminder' do
+    let(:structure) { FactoryGirl.create(:structure) }
+
+    context 'structure has no main contact' do
+      before do
+        structure.admins = []
+        structure.save
+      end
+
+      it "doesn't send the reminder" do
+        expect { ReminderService.send_status_reminder(structure) }.
+          to_not change { ActionMailer::Base.deliveries.count }
+      end
+    end
+  end
+end
