69987a31bdf950b0c8ed8a0b644c21d981e657da
Aliou Diallo <code@aliou.me>
2015-01-07 12:01:00 +0100

    Keep spec DRY

--- spec/support/concerns/has_audiences_and_levels_spec.rb
+++ spec/support/concerns/has_audiences_and_levels_spec.rb
@@ -1,23 +1,31 @@
 require 'rails_helper'
 
 shared_examples_for 'HasAudiencesAndLevels' do
-  let (:model) { described_class }
+  let (:model)    { described_class }
+  let (:instance) { FactoryGirl.create(model.to_s.underscore.to_sym) }
 
   it 'is for kids' do
-    model.audiences << Audience::KID
+    set_audience(instance, Audience::KID)
 
-    expect(model.for_kid?).to be_truthy
+    expect(instance.for_kid?).to be_truthy
   end
 
   it 'is for adults' do
-    model.audiences << Audience::ADULT
+    set_audience(instance, Audience::ADULT)
 
-    expect(model.for_adult?).to be_truthy
+    expect(instance.for_adult?).to be_truthy
   end
 
   it 'is for seniors' do
-    model.audiences << Audience::SENIOR
+    set_audience(instance, Audience::SENIOR)
 
-    expect(model.for_senior?).to be_truthy
+    expect(instance.for_senior?).to be_truthy
+  end
+
+  def set_audience(instance, audience)
+    instance.audiences += [audience]
+
+    instance.save
+    instance.reload
   end
 end
