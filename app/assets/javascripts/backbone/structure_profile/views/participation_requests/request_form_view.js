c8e18a94e914f24166a167bdd383c3887761bf0a
Nima Izadi <nim.izadi@gmail.com>
2015-01-29 18:07:14 +0100

    Show phone number if user is connected

--- app/assets/javascripts/backbone/structure_profile/views/participation_requests/request_form_view.js
+++ app/assets/javascripts/backbone/structure_profile/views/participation_requests/request_form_view.js
@@ -165,7 +165,10 @@ StructureProfile.module('Views.ParticipationRequests', function(Module, App, Bac
             if (CoursAvenue.currentUser().get('last_messages_sent')) {
                 _.extend(data, {
                     last_message_sent_at: CoursAvenue.currentUser().get('last_messages_sent')[structure_json.id],
-                    user_participation_requests_path: Routes.user_participation_requests_path({ id: CoursAvenue.currentUser().get('slug') })
+                    user_participation_requests_path: Routes.user_participation_requests_path({ id: CoursAvenue.currentUser().get('slug') }),
+                    user: {
+                        phone_number: CoursAvenue.currentUser().get('phone_number')
+                    }
                 });
             }
             _.extend(data, {
