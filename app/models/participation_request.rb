b0ba3ba9ff20a48a58949bf6f53375705d1dac41
Nima Izadi <nim.izadi@gmail.com>
2015-01-30 09:39:05 +0100

    Fixed bug [ci skip]

--- app/models/participation_request.rb
+++ app/models/participation_request.rb
@@ -43,7 +43,7 @@ class ParticipationRequest < ActiveRecord::Base
   scope :upcoming,                -> { where( arel_table[:date].gteq(Date.today) )
                                       .order("state='pending' DESC, state='canceled' ASC,
                                               updated_at DESC, date ASC") }
-  scope :past,                    -> { where( arel_table[:date].lt(Date.today) ).order("date ASC") } }
+  scope :past,                    -> { where( arel_table[:date].lt(Date.today) ).order("date ASC") }
   scope :canceled,                -> { where( arel_table[:state].eq('canceled') ) }
   scope :tomorrow,                -> { where( state: 'accepted', date: Date.tomorrow ) }
   scope :structure_not_responded, -> { where.not( structure_responded: true ) }
