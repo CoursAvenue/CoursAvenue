7202d915a287ded812e6af6c147902f51ffe03e6
Aliou Diallo <code@aliou.me>
2015-03-03 19:12:08 +0100

    Set default newsletter values.

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -17,4 +17,24 @@ class Newsletter < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
   validates :state, presence: true
+
+  after_create :set_defaults
+
+  ######################################################################
+  # Methods                                                            #
+  ######################################################################
+
+  private
+
+  # Sets the default values for the sender name, the reply_to address and the
+  # object.
+  #
+  # @return self.
+  def set_defaults
+    name     = structure.name if name.nil?
+    reply_to = structure.contact_email if reply_to.nil? and structure.contact_email.present?
+    object   = title if object.nil?
+
+    save
+  end
 end
--- spec/models/newsletter_spec.rb
+++ spec/models/newsletter_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Newsletter, type: :model do
 
   subject         { FactoryGirl.create(:newsletter) }
   let(:structure) { subject.structure }
+
+  describe 'defaults' do
+    it 'is a draft by default' do
+      expect(subject.state).to eq('draft')
+    end
+
+    it 'sets the default sender name' do
+      expect(subject.sender_name).to eq(structure.name)
+    end
+
+    it 'sets the default reply to address' do
+      expect(subject.reply_to).to eq(structure.contact_email)
+    end
+
+    it 'sets the default object' do
+      expect(subject.object).to eq(subject.title)
+    end
+  end
 end
