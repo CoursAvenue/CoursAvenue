bd52501a59f6620c6342a461b85007fa9445cf68
Nima Izadi <nim.izadi@gmail.com>
2015-03-04 20:58:31 +0100

    Fixed spacing and indentation

--- app/assets/javascripts/backbone/pro/application/app_pro.js
+++ app/assets/javascripts/backbone/pro/application/app_pro.js
@@ -1,21 +1,20 @@
 AppPro = new Backbone.Marionette.Application({ slug: 'pro' });
 
 AppPro.addRegions({
-  mainRegion      : '#main-content',
-  topBarRegion    : '#top-bar',
-  sideMenuRegion  : '#side-menu'
+    main_region     : '#main-content',
+    top_bar_region  : '#top-bar',
+    side_menu_region: '#side-menu'
 });
 
 AppPro.addInitializer(function(options) {
-  var topBar          = new AppPro.Views.TopBar();
-  var sideMenu        = new AppPro.Views.SideMenu();
+    var topBar   = new AppPro.Views.TopBar();
+    var sideMenu = new AppPro.Views.SideMenu();
 
-  this.topBarRegion.show(topBar);
-  this.sideMenuRegion.show(sideMenu);
+    this.topBarRegion.show(topBar);
+    this.sideMenuRegion.show(sideMenu);
 
-  new AppProRouter();
-  Backbone.history.start({ pushState: true, root: window.coursavenue.bootstrap.root_url });
-
+    new AppProRouter();
+    Backbone.history.start({ pushState: true, root: window.coursavenue.bootstrap.root_url });
 });
 
 
