1c29a9b8d2faef697d8986df7c9af475992b66fa
Aliou Diallo <code@aliou.me>
2015-03-05 10:22:31 +0100

    Set the layout ids as zero based.

--- app/models/newsletter/layout.rb
+++ app/models/newsletter/layout.rb
@@ -1,27 +1,27 @@
 class Newsletter::Layout < ActiveHash::Base
   self.data = [
     {
-      id: 1,
+      id: 0,
       blocs: ['image', 'text'],
       name: 'Layout #01'
     },
     {
-      id: 2,
+      id: 1,
       blocs: ['image', 'text'],
       name: 'Layout #02'
     },
     {
-      id: 3,
+      id: 2,
       blocs: ['image', 'text'],
       name: 'Layout #03'
     },
     {
-      id: 4,
+      id: 3,
       blocs: ['image', 'text'],
       name: 'Layout #04'
     },
     {
-      id: 5,
+      id: 4,
       blocs: ['image', 'text'],
       name: 'Layout #05'
     },
