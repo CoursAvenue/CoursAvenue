bfc02f23b5e721f41f53f5238560534f77a5c4ea
Aliou Diallo <code@aliou.me>
2015-03-05 11:51:39 +0100

    Update Hound js configuraton.

--- app/assets/javascripts/backbone/newsletters/models/layouts_collection.js
+++ app/assets/javascripts/backbone/newsletters/models/layouts_collection.js
@@ -2,4 +2,4 @@ Newsletter.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.LayoutsCollection = Backbone.Collection.extend({
         model: Module.Layout,
     });
-})
+});
