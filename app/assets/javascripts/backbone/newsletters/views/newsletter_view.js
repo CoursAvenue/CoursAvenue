d41d3a1094d90acb89634b30af092b8270e21ab4
Aliou Diallo <code@aliou.me>
2015-03-05 14:50:42 +0100

    Setup newsletter view rendering.

--- app/assets/javascripts/backbone/newsletters/views/newsletter_view.js
+++ app/assets/javascripts/backbone/newsletters/views/newsletter_view.js
@@ -2,6 +2,29 @@ Newsletter.module('Views', function(Module, App, Backbone, Marionette, $, _) {
     Module.NewsletterView = Backbone.Marionette.ItemView.extend({
         template: Module.templateDirname() + 'newsletter_view',
 
+        events: {
+        },
+
+        bindings: {
+          '#title': 'title',
+        },
+
+        initialize: function initialize () {
+        },
+
+        // We overwrite render so we can call the stickit plugin.
+        render: function render () {
+          // Invoke original render function
+          var args = Array.prototype.slice.apply(arguments);
+          var result = Marionette.ItemView.prototype.render.apply(this, args);
+
+          // Apply stickit
+          this.stickit();
+
+          // Return render result
+          return result;
+        },
+
         updateLayout: function updateLayout (data) {
             var model = data.model;
             this.model.set({ layout_id: model.get('id') });
