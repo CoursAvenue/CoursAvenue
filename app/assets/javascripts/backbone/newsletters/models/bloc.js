b79537af02491c71d35d8d410e8c740c41464941
Aliou Diallo <code@aliou.me>
2015-03-06 17:19:04 +0100

    Cleanup Bloc model.

--- app/assets/javascripts/backbone/newsletters/models/bloc.js
+++ app/assets/javascripts/backbone/newsletters/models/bloc.js
@@ -1,15 +1,4 @@
 Newsletter.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.Bloc = Backbone.Model.extend({
-
-      // TODO: Set the url using:
-      // - The structure
-      // - The newsletter
-      // - The bloc itslef.
-      url: function url () {
-        // var structure = window.coursavenue.bootstrap.structure;
-        // return Routes.pro_structure_newsletter_path(structure, this.get('id'));
-        return '';
-      },
-
     });
 });
