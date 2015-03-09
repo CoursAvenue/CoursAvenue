0464812b85f7d6f1dd168c891404d2fe2cddcec8
Aliou Diallo <code@aliou.me>
2015-03-09 18:40:23 +0100

    Create the subscriptions on mailing list creation.

--- app/models/newsletter/mailing_list.rb
+++ app/models/newsletter/mailing_list.rb
@@ -4,4 +4,19 @@ class Newsletter::MailingList < ActiveRecord::Base
 
   validates :name, presence: true
   validates :tag, presence: true
+
+  after_create :create_subscriptions
+
+  private
+
+  # Create the subscriptions depending on the Mailing List tag.
+  def create_subscriptions
+    profiles = UserProfile.where(structure: newsletter.structure).tagged_with(tag)
+
+    profiles.each do |profile|
+      subscriptions.create(user_profile: profile, subscribed: true)
+    end
+  end
+  handle_asynchronously :create_subscriptions
+
 end
