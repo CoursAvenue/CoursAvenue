6a75fc39a684908bca55e5d432bd1dd665f240a6
Aliou Diallo <code@aliou.me>
2015-03-10 11:04:23 +0100

    Add mailing list unsubscription.

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -13,4 +13,11 @@ class NewsletterMailer < ActionMailer::Base
       reply_to: @newsletter.reply_to,
       from: "#{@newsletter.sender_name} <noreply@coursavenue.com>"
   end
+
+  # Sends the unsubscription confirmation.
+  def confirm_unsubscribtion(newsletter_subscription)
+    mail to: newsletter_subscription.email,
+      subject: '',
+      from:  "#{@newsletter.sender_name} <noreply@coursavenue.com>"
+  end
 end
--- app/models/newsletter/subscription.rb
+++ app/models/newsletter/subscription.rb
@@ -1,4 +1,21 @@
 class Newsletter::Subscription < ActiveRecord::Base
-  belongs_to :newsletter_mailing_list
+  belongs_to :mailing_list, class: 'Newsletter::MailingList'
   belongs_to :user_profile
+
+  # The email address of the subscription.
+  #
+  # @return a String.
+  def email
+    user_profile.email
+  end
+
+  # Unsubscribe from the mailing list and send confirmation.
+  #
+  # @return nothing.
+  def unsubscribe!
+    subscribed = false
+    NewsletterMailer.delay.confirm_unsubscribtion(self)
+
+    save
+  end
 end
