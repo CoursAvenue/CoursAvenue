f6a3a814e9f0dc95647af80ef71b525b415fb797
Aliou Diallo <code@aliou.me>
2015-03-12 18:10:14 +0100

    Update recipient model.

--- app/models/newsletter/recipient.rb
+++ app/models/newsletter/recipient.rb
@@ -1,4 +1,11 @@
 class Newsletter::Recipient < ActiveRecord::Base
   belongs_to :user_profile
   belongs_to :newsletter
+
+  # The email of the recipient.
+  #
+  # @return a String.
+  def email
+    user_profile.email
+  end
 end
--- spec/models/newsletter/recipient_spec.rb
+++ spec/models/newsletter/recipient_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Newsletter::Recipient, type: :model do
       expect(subject.opened?).to be_falsy
     end
   end
+
+  describe '#email' do
+    subject       { FactoryGirl.create(:newsletter_recipient) }
+    let(:profile) { subject.user_profile }
+
+    it "is equal to the profile's email" do
+      expect(subject.email).to eq(profile.email)
+    end
+  end
 end
