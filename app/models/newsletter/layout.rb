f8410e0f729b5f9b43b9c1d0f76ad48294fda750
Aliou Diallo <code@aliou.me>
2015-03-13 11:55:44 +0100

    Add partial path to layouts.

--- app/models/newsletter/layout.rb
+++ app/models/newsletter/layout.rb
@@ -3,27 +3,32 @@ class Newsletter::Layout < ActiveHash::Base
     {
       id: 0,
       blocs: ['image', 'text'],
-      name: 'Layout #01'
+      name: 'Layout #01',
+      partial: 'layout_01'
     },
     {
       id: 1,
       blocs: ['text', 'text'],
-      name: 'Layout #02'
+      name: 'Layout #02',
+      partial: 'layout_02'
     },
     {
       id: 2,
       blocs: ['image', 'image'],
-      name: 'Layout #03'
+      name: 'Layout #03',
+      partial: 'layout_03'
     },
     {
       id: 3,
       blocs: ['image', 'text', 'text'],
-      name: 'Layout #04'
+      name: 'Layout #04',
+      partial: 'layout_04'
     },
     {
       id: 4,
       blocs: ['image', 'text', 'image', 'text'],
-      name: 'Layout #05'
+      name: 'Layout #05',
+      partial: 'layout_05'
     },
   ]
 end
