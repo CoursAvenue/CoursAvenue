3ac34784d1fc8e716548685f3784a0180ba1ed5d
Aliou Diallo <code@aliou.me>
2015-03-16 16:33:23 +0100

    Activate the current tab.

--- app/assets/javascripts/backbone/newsletters/views/newsletter_layout.js
+++ app/assets/javascripts/backbone/newsletters/views/newsletter_layout.js
@@ -9,13 +9,20 @@ Newsletter.module('Views', function(Module, App, Backbone, Marionette, $, _) {
         initialize: function initialize (options) {
             this.router = options.router;
 
-            _.bindAll(this, 'updateNav');
+            _.bindAll(this, 'setCurrentTab', 'updateNav');
 
+        },
+
+        setCurrentTab: function setCurrentTab (tab) {
+            $('[data-newsletter-nav]').parent().removeClass('active');
+            $('[data-newsletter-nav=' + tab + ']').parent().addClass('active');
         },
 
         updateNav: function updateNav (event) {
             var event_sender = $(event.toElement);
             var fragment     = event_sender.data('newsletter-nav');
+
+            this.setCurrentTab(fragment);
 
             this.router.navigate(fragment, { trigger: true });
         },
