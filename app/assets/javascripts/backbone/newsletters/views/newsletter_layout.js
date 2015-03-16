dacba66ba930f4cffce8e1285edacb5943a022f6
Aliou Diallo <code@aliou.me>
2015-03-16 13:54:07 +0100

    Update the application layout to get the nav change events.

--- app/assets/javascripts/backbone/newsletters/views/newsletter_layout.js
+++ app/assets/javascripts/backbone/newsletters/views/newsletter_layout.js
@@ -1,5 +1,23 @@
 Newsletter.module('Views', function(Module, App, Backbone, Marionette, $, _) {
     Module.NewsletterLayout = CoursAvenue.Views.EventLayout.extend({
         template: Module.templateDirname() + 'newsletter_layout',
+
+        events: {
+            'click [data-newsletter-nav]': 'updateNav'
+        },
+
+        initialize: function initialize (options) {
+            this.router = options.router;
+            this.model  = options.model;
+
+            _.bindAll(this, 'updateNav');
+
+        },
+
+        updateNav: function updateNav (event) {
+            var event_sender = $(event.toElement);
+            console.log(event_sender.data('newsletter-nav'));
+        },
+
     });
 });
