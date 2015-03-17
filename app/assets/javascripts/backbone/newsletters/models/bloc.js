39a92ea3fba16ad64a3eff200cfc883c95d38f36
Aliou Diallo <code@aliou.me>
2015-03-17 17:55:07 +0100

    Set the bloc model url.

--- app/assets/javascripts/backbone/newsletters/models/bloc.js
+++ app/assets/javascripts/backbone/newsletters/models/bloc.js
@@ -1,6 +1,16 @@
 Newsletter.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.Bloc = Backbone.Model.extend({
 
+        url: function url () {
+            var newsletter = this.get('newsletter');
+
+            if (newsletter.isNew()) {
+                newsletter.save();
+            }
+
+            return Routes.pro_structure_newsletter_blocs_path(window.coursavenue.bootstrap.structure, newsletter.get('id'));
+        },
+
         initialize: function initialize (model, options) {
             var backend_types = {
                 image: 'Newsletter::Bloc::Image',
@@ -9,6 +19,8 @@ Newsletter.module('Models', function(Module, App, Backbone, Marionette, $, _) {
 
             this.set('view_type', model.type);
             this.set('type', backend_types[model.type]);
+
+            _.bindAll(this, 'url');
         },
 
     });
