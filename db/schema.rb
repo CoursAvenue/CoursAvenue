f9cd7dda83ab6d894cf01652eeb0b1da8da2e19c
Nima Izadi <nim.izadi@gmail.com>
2015-03-18 13:43:47 +0100

    Changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317152159) do
+ActiveRecord::Schema.define(version: 20150318095719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20150317152159) do
     t.integer  "category_id"
     t.string   "page_title"
     t.text     "page_description"
+    t.string   "type"
   end
 
   create_table "blog_articles_subjects", force: true do |t|
@@ -97,6 +98,7 @@ ActiveRecord::Schema.define(version: 20150317152159) do
     t.string   "page_title"
     t.text     "description"
     t.text     "page_description"
+    t.string   "type"
   end
 
   create_table "call_reminders", force: true do |t|
