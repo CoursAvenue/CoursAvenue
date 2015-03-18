4fa77dfc6ad9c25ca42bc2f9f52217b721d1e51d
Aliou Diallo <code@aliou.me>
2015-03-18 19:09:55 +0100

    Rename MailingList* to MailingListsCollection*

--- app/assets/javascripts/backbone/newsletters/views/mailing_lists_collection_view.js
+++ app/assets/javascripts/backbone/newsletters/views/mailing_lists_collection_view.js
@@ -1,26 +1,27 @@
 Newsletter.module('Views', function(Module, App, Backbone, Marionette, $, _) {
-    Module.MailingListView = Backbone.Marionette.ItemView.extend({
+    Module.MailingListsCollectionView = Backbone.Marionette.CompositeView.extend({
         template: Module.templateDirname() + 'mailing_list_view',
         tagName: 'div',
+        childViewContainer: '[data-type=mailing-list]',
+        childView: Module.MailingListView,
+        childViewEventPrefix: 'mailing_list',
 
         events: {
             'click [data-import]': 'importList',
         },
 
         initialize: function initialize () {
-            _.bindAll(this, 'saveModel', 'importList');
-        },
-
-        saveModel: function saveModel () {
+            _.bindAll(this, 'importList');
         },
 
         // When the user wants to import its mailing list.
         importList: function importList () {
             this.model.save();
-            var path = Routes.new_pro_structure_user_profile_import_path(window.coursavenue.bootstrap.structure);
+            var path = Routes.new_pro_structure_user_profile_import_url(window.coursavenue.bootstrap.structure, {
+                return_to: this.model.get('id')
+            });
 
             window.location = path;
         },
-
     });
 });
