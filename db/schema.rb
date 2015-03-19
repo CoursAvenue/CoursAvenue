32bafbd488b2a98bfbc4561a618cf3ffb031112a
Nima Izadi <nim.izadi@gmail.com>
2015-03-19 11:45:16 +0100

    Added avatar to authors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319102813) do
+ActiveRecord::Schema.define(version: 20150319104024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20150319102813) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   create_table "blog_categories", force: true do |t|
