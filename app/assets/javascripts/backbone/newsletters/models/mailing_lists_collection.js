3cf8b739d039afbf37656f15cacf5591c582ee36
Aliou Diallo <code@aliou.me>
2015-03-19 16:04:52 +0100

    Make the 'all profiles' mailing lists show on top.

--- app/assets/javascripts/backbone/newsletters/models/mailing_lists_collection.js
+++ app/assets/javascripts/backbone/newsletters/models/mailing_lists_collection.js
@@ -1,5 +1,15 @@
 Newsletter.module('Models', function(Module, App, Backbone, Marionette, $, _) {
     Module.MailingListsCollection = Backbone.Collection.extend({
         model: Module.MailingList,
+
+        // We use a comparator to show the 'all_profiles' mailing lists first.
+        // Otherwiser, we just return their index in their collection.
+        comparator: function comparator (model) {
+            if (model.has('all_profiles') && model.get('all_profiles') == true) {
+                return 0;
+            } else {
+                return model.collection.indexOf(model);
+            }
+        },
     });
 });
