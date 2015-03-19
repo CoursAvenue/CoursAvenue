ce852759cf94795554426fd811afd000b7c2d1b2
Aliou Diallo <code@aliou.me>
2015-03-19 15:24:02 +0100

    Include the mailing list in the newsletter serializer.

--- app/serializers/newsletter_serializer.rb
+++ app/serializers/newsletter_serializer.rb
@@ -1,4 +1,5 @@
 class NewsletterSerializer < ActiveModel::Serializer
-  attributes :id, :title, :state, :object, :sender_name, :reply_to, :layout_id, :blocs
+  attributes :id, :title, :state, :object, :sender_name, :reply_to, :layout_id, :blocs,
+    :newsletter_mailing_list_id
   has_many :blocs, serializer: NewsletterBlocSerializer
 end
