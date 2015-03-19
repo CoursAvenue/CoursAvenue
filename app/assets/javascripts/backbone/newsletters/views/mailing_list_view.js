66962d14ae9782de4e3fd133b4dcca0f6c868445
Aliou Diallo <code@aliou.me>
2015-03-19 18:04:25 +0100

    Show the filters when the mailing list is selected.

--- app/assets/javascripts/backbone/newsletters/views/mailing_list_view.js
+++ app/assets/javascripts/backbone/newsletters/views/mailing_list_view.js
@@ -5,7 +5,8 @@ Newsletter.module('Views', function(Module, App, Backbone, Marionette, $, _) {
         className: '',
 
         events: {
-            'click [data-mailing-list]': 'selectMailingList'
+            // 'click [data-mailing-list]': 'selectMailingList'
+            'change [type=radio]': 'showFilters'
         },
 
         initialize: function initialize () {
@@ -15,6 +16,11 @@ Newsletter.module('Views', function(Module, App, Backbone, Marionette, $, _) {
         selectMailingList: function selectMailingList () {
             this.trigger('selected', { model: this.model });
         },
+
+        showFilters: function showFilters () {
+            this.$el.find('[data-filters-list]').slideDown();
+        },
+
     });
 });
 
