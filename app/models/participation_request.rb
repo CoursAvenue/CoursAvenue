ab2d88ec765929125ad7f15d2908ddcfff0dc71b
Nima Izadi <nim.izadi@gmail.com>
2015-03-23 18:30:04 +0100

    Fixed bug

--- app/models/participation_request.rb
+++ app/models/participation_request.rb
@@ -176,7 +176,7 @@ class ParticipationRequest < ActiveRecord::Base
       planning.place
     elsif course and course.place
       course.place
-    else
+    elsif structure
       structure.places.first
     end
   end
