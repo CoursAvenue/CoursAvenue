46c3bb7ffb6a4b486b66ee6d5205f687fba472d8
Aliou Diallo <code@aliou.me>
2015-03-23 17:30:08 +0100

    Convert a newsletter recipient to a mandrill recipient.

--- spec/models/newsletter/recipient_spec.rb
+++ spec/models/newsletter/recipient_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe Newsletter::Recipient, type: :model do
       expect(subject.email).to eq(profile.email)
     end
   end
+
+  describe '#to_mandrill_recipient' do
+    subject       { FactoryGirl.create(:newsletter_recipient) }
+    let(:profile) { subject.user_profile }
+
+    it 'create a new object with the right fields' do
+      mandrill_recipient = subject.to_mandrill_recipient
+
+      expect(mandrill_recipient[:email]).to eq(subject.email)
+      expect(mandrill_recipient[:name]).to  eq(profile.name)
+      expect(mandrill_recipient[:type]).to  eq('bcc')
+    end
+
+  end
 end
