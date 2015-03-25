ef72d6095bbb97de2e73a5e38a39a51eaeff56d8
Aliou Diallo <code@aliou.me>
2015-03-25 18:51:16 +0100

    Unscope blog subscribers.

--- app/controllers/pro/blog/subscribers_controller.rb
+++ app/controllers/pro/blog/subscribers_controller.rb
@@ -3,7 +3,7 @@ class Pro::Blog::SubscribersController < Pro::ProController
 
   def create
     respond_to do |format|
-      if Blog::Subscriber::ProSubscriber.where(email: params[:blog_subscriber][:email]).first_or_create.persisted?
+      if ::Blog::Subscriber::ProSubscriber.where(email: params[:blog_subscriber][:email]).first_or_create.persisted?
         format.html { redirect_to((request.referer || pro_blog_articles_path), notice: 'Vous êtes inscrit à notre newsletter !') }
       else
         format.html { redirect_to((request.referer || pro_blog_articles_path), alert: 'Votre e-mail est incorrect.') }
