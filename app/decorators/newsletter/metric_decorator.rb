e04e5558c66ef3e6e272257be24f0c281288d245
Aliou Diallo <code@aliou.me>
2015-03-26 15:00:52 +0100

    Don't divide by 0.

--- app/decorators/newsletter/metric_decorator.rb
+++ app/decorators/newsletter/metric_decorator.rb
@@ -2,10 +2,12 @@ class Newsletter::MetricDecorator < Draper::Decorator
   delegate_all
 
   def opened_percentage
+    return 0 if object.nb_email_sent.zero?
     ((object.nb_opening.to_f / object.nb_email_sent.to_f) * 100.0).round(1)
   end
 
   def click_percentage
+    return 0 if object.nb_email_sent.zero?
     ((object.nb_click.to_f / object.nb_email_sent.to_f) * 100.0).round(1)
   end
 end
