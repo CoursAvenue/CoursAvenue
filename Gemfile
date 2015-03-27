f4b6aa0b396dc3b8fb28af9b1a3067a3e4e3a6a3
Aliou Diallo <code@aliou.me>
2015-03-27 13:48:01 +0100

    Add shoulda matchers

--- Gemfile
+++ Gemfile
@@ -219,6 +219,7 @@ group :test do
   gem 'capybara',             '~> 2.2.1'
   gem 'rspec-instafail',      '~> 0.2.5'
   gem 'mongoid-rspec',        '~> 2.0.0.rc1'
+  gem 'shoulda-matchers',     '~> 2.8.0', require: false
 
   gem 'spring'
   gem 'spring-commands-rspec'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'shoulda/matchers'
 
 # The following line is provided for convenience purposes. It has the downside
 # of increasing the boot-up time by auto-requiring all files in the support
