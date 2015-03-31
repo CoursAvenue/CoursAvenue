958b98fb8980c7c5a3e45c05f383057b85df019c
Aliou Diallo <code@aliou.me>
2015-03-31 15:41:29 +0200

    Update comments.

--- app/models/analytic/client.rb
+++ app/models/analytic/client.rb
@@ -47,7 +47,7 @@ class Analytic::Client
   # @param start_date   The start date, by default 15 days ago.
   # @param end_date     The end date, by defautl yesterday.
   #
-  # @return
+  # @return an OpenStruct with the data.
   def hits(structure_id, start_date = 15.days.ago, end_date = 1.day.ago)
     Analytic::Hit.results(profile, start_date: start_date, end_date: end_date).
       for_structure(structure_id).first
@@ -59,7 +59,7 @@ class Analytic::Client
   # @param start_date   The start date, by default 15 days ago.
   # @param end_date     The end date, by defautl yesterday.
   #
-  # @return The metrics.
+  # @return an OpenStruct with the data.
   def actions(structure_id, start_date = 15.days.ago, end_date = 1.day.ago)
     Analytic::Action.results(profile, start_date: start_date, end_date: end_date).
       for_structure(structure_id).first
@@ -71,7 +71,7 @@ class Analytic::Client
   # @param start_date   The start date, by default 15 days ago.
   # @param end_date     The end date, by defautl yesterday.
   #
-  # @return The metrics.
+  # @return an OpenStruct with the data.
   def views(structure_id, start_date = 15.days.ago, end_date = 1.day.ago)
     Analytic::View.results(profile, start_date: start_date, end_date: end_date).
       for_structure(structure_id).first
@@ -83,7 +83,7 @@ class Analytic::Client
   # @param start_date   The start date, by default 15 days ago.
   # @param end_date     The end date, by defautl yesterday.
   #
-  # @return The metrics.
+  # @return an OpenStruct with the data.
   def impressions(structure_id, start_date = 15.days.ago, end_date = 1.day.ago)
     Analytic::Impression.results(profile, start_date: start_date, end_date: end_date).
       for_structure(structure_id).first
