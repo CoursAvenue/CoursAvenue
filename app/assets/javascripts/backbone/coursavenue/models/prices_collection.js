b2f381aa46379f60291270012849e2864ef48c85
Nima Izadi <nim.izadi@gmail.com>
2015-04-01 18:58:40 +0200

    Fixed stuff

--- app/assets/javascripts/backbone/coursavenue/models/prices_collection.js
+++ app/assets/javascripts/backbone/coursavenue/models/prices_collection.js
@@ -4,7 +4,7 @@ CoursAvenue.module('Models', function(Models, App, Backbone, Marionette, $, _) {
     Models.PricesCollection = Backbone.Collection.extend({
         model: Models.Price,
         comparator: function comparator (a, b) {
-            return (a.get('amount') < b.get('amount'));
+            return (parseFloat(a.get('amount'), 10) > parseFloat(b.get('amount'), 10));
         }
     });
 });
