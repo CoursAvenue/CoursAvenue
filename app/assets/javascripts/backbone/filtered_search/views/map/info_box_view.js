49f2838a8d10e19150929148245e4e5da0d4fa4d
Nima Izadi <nim.izadi@gmail.com>
2015-04-02 13:14:13 +0200

    Fixed infoboxview

--- app/assets/javascripts/backbone/filtered_search/views/map/info_box_view.js
+++ app/assets/javascripts/backbone/filtered_search/views/map/info_box_view.js
@@ -6,22 +6,10 @@ FilteredSearch.module('Views.Map', function(Module, App, Backbone, Marionette, $
         onRender: function onRender (){
             this.$('a').removeClass('bordered--bottom soft-half--ends');
             this.$('[data-content]').addClass('soft-half--left');
-            var self = this;
-            setTimeout(function(){
-                // Start slideshow
-                // Removing images and adding the image url to background image in order to have the image being covered
-                self.$('.rslides img').each(function(){
-                    var $this = $(this);
-                    $this.closest('.media__item').hide();
-                    $this.closest('li').css('background-image', 'url("' + $this.attr('src') + '")');
-                });
-                self.$(".rslides").responsiveSlides({
-                    auto: false,
-                    nav: true,
-                    prevText: '<i class="alpha fa fa-chevron-left"></i>',
-                    nextText: '<i class="alpha fa fa-chevron-right"></i>'
-                });
-                COURSAVENUE.initialize_fancy(self.$('.rslides-wrapper [data-behavior="fancy"]'));
+            this.$('.rslides img').each(function(){
+                var $this = $(this);
+                $this.closest('.media__item').hide();
+                $this.closest('li').css('background-image', 'url("' + $this.attr('src') + '")');
             });
 
         }
