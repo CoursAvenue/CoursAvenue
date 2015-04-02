bc3f3c0f5d92d7a39dc1d6cf165fd03c87d7d5ec
Aliou Diallo <code@aliou.me>
2015-04-02 18:54:55 +0200

    Set default values in multi bloc view.

--- app/assets/javascripts/backbone/newsletters/views/blocs/multi.js
+++ app/assets/javascripts/backbone/newsletters/views/blocs/multi.js
@@ -2,4 +2,12 @@ Newsletter.module('Views.Blocs', function(Module, App, Backbone, Marionette, $, 
     Module.Multi = Backbone.Marionette.CompositeView.extend({
         template: Module.templateDirname() + 'multi',
         tagName: 'div',
+        childViewContainer: '[data-type=sub-blocs]',
+
+        initialize: function initialize () {
+        },
+
+        getChildView: function getChildView (item) {
+        },
+    });
 });
