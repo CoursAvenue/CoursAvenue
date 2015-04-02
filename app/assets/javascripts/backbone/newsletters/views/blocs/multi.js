8e63624d6024c9790d2047568c7de038feed3674
Aliou Diallo <code@aliou.me>
2015-04-02 18:51:18 +0200

    Fix multi bloc type.

--- app/assets/javascripts/backbone/newsletters/views/blocs/multi.js
+++ app/assets/javascripts/backbone/newsletters/views/blocs/multi.js
@@ -1,5 +1,5 @@
 Newsletter.module('Views.Blocs', function(Module, App, Backbone, Marionette, $, _) {
-    Module.Multi = Backbone.Marionette.ItemView.extend({
+    Module.Multi = Backbone.Marionette.CompositeView.extend({
         template: Module.templateDirname() + 'multi',
         tagName: 'div',
 });
