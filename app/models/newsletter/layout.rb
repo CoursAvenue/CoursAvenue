5a845776780f43ba15cb9fb5de22a9e6eb478677
Aliou Diallo <code@aliou.me>
2015-04-03 10:15:08 +0200

    Add subblocs and disposition to layout model.

--- app/models/newsletter/layout.rb
+++ app/models/newsletter/layout.rb
@@ -5,28 +5,32 @@ class Newsletter::Layout < ActiveHash::Base
       blocs: ['image', 'text'],
       name: 'Modèle 1',
       partial: 'layout_01',
-      disposition: :horizontal
+      disposition: :horizontal,
+      sub_blocs: []
     },
     {
       id: 2,
       blocs: ['image', 'text', 'image', 'text'],
       name: 'Modèle 2',
       partial: 'layout_02',
-      disposition: :horizontal
+      disposition: :horizontal,
+      sub_blocs: []
     },
     {
       id: 3,
       blocs: ['image', 'image'],
       name: 'Modèle 3',
       partial: 'layout_03',
-      disposition: :horizontal
+      disposition: :horizontal,
+      sub_blocs: []
     },
     {
       id: 4,
       blocs: ['multi', 'multi'],
       name: 'Modèle 4',
       partial: 'layout_04',
-      disposition: :vertical
+      disposition: :vertical,
+      sub_blocs: [['image', 'text'], ['image', 'text']]
     }
   ]
 end
