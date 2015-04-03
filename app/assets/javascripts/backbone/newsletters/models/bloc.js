04e4d0885ccdf3d6af1fedb92afd5347abf7fac3
Aliou Diallo <code@aliou.me>
2015-04-03 15:12:30 +0200

    Use a different route when saving a sub bloc.

--- app/assets/javascripts/backbone/newsletters/models/bloc.js
+++ app/assets/javascripts/backbone/newsletters/models/bloc.js
@@ -3,12 +3,18 @@ Newsletter.module('Models', function(Module, App, Backbone, Marionette, $, _) {
 
         urlRoot: function urlRoot () {
             var newsletter = this.get('newsletter');
+            var structure  = window.coursavenue.bootstrap.structure;
+            var multiBloc  = this.collection.multiBloc;
 
             if (newsletter.isNew()) {
                 newsletter.save();
             }
 
-            return Routes.pro_structure_newsletter_blocs_path(window.coursavenue.bootstrap.structure, newsletter.get('id'));
+            if (multiBloc) {
+                return Routes.sub_bloc_create_pro_structure_newsletter_bloc_path(structure, newsletter.get('id'), multiBloc.get('id'));
+            } else {
+                return Routes.pro_structure_newsletter_blocs_path(structure, newsletter.get('id'));
+            }
         },
 
         initialize: function initialize (model, options) {
