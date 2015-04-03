d1c99a9e07d7527c10161189b25f17ac225c7b77
Aliou Diallo <code@aliou.me>
2015-04-03 10:16:15 +0200

    Create the sub bloc collection on multi bloc initialization

--- app/assets/javascripts/backbone/newsletters/views/blocs/multi.js
+++ app/assets/javascripts/backbone/newsletters/views/blocs/multi.js
@@ -5,6 +5,18 @@ Newsletter.module('Views.Blocs', function(Module, App, Backbone, Marionette, $, 
         childViewContainer: '[data-type=sub-blocs]',
 
         initialize: function initialize () {
+            var layout        = this.model.collection.newsletter.get('layout')
+            var subBlocsTypes = layout.get('sub_blocs')[this.model.get('index')];
+            var subBlocs      = []
+
+            subBlocs = subBlocsTypes.map(function(blocType, index) {
+                return { type: blocType, index: index, position: index + 1, isSubBloc: true };
+            });
+
+            this.collection = new Newsletter.Models.BlocsCollection(subBlocs, {
+                multiBloc:  this.model,
+                newsletter: this.model.collection.newsletter,
+            });
         },
 
         getChildView: function getChildView (item) {
