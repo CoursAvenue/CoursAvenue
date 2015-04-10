183e91c611ab74e3ccd24f649ae1cb5a226d8ec5
Nima Izadi <nim.izadi@gmail.com>
2015-04-10 11:36:19 +0200

    Fixed bugs

--- app/models/blog/category/pro_category.rb
+++ app/models/blog/category/pro_category.rb
@@ -2,7 +2,11 @@ class Blog::Category::ProCategory < Blog::Category
   friendly_id :name, use: [:slugged, :finders]
 
   def articles
-    Blog::Article::ProArticle.where(:category_id => self.path_ids)
+    if self.root?
+      Blog::Article::ProArticle.where(:category_id => [self.id] + self.children.map(&:id))
+    else
+      Blog::Article::ProArticle.where(:category_id => self.path_ids)
+    end
   end
 
   def pro_category?
