c646740a13008c3cf919e198c826f73b0df570da
Aliou Diallo <code@aliou.me>
2015-04-13 12:24:05 +0200

    Remove fake client since it doesn't make sense

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -4,10 +4,6 @@ class Analytic
   METRICS    = %w(impression view action) # Index base should be 1
 
   def self.client
-    if Rails.env.test?
-      Analytic::FakeClient.new
-    else
-      Analytic::Client.new
-    end
+    Analytic::Client.new
   end
 end
