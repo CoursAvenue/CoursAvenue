24a214c07b6e5162b5aaa3f4315618bef5a95ad9
Aliou Diallo <code@aliou.me>
2015-04-13 18:33:52 +0200

    Add empty_subscription factory

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :subscription do
+    structure { FactoryGirl.create(:structure, :with_stripe_customer) }
     stripe_subscription_id ''
+
+    factory :empty_subscription do
+      stripe_subscription_id nil
+    end
 
     trait :canceled do
       canceled_at 10.days.ago
