80333ba18a0fbfff295813743b1722069e5b5e5b
Aliou Diallo <code@aliou.me>
2015-04-13 17:25:43 +0200

    Fix typo.

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -5,19 +5,5 @@ FactoryGirl.define do
     trait :canceled do
       canceled_at 10.days.ago
     end
-
-    # factory :gold_plan do
-    #   name 'Gold'
-    #   price 3000
-    #   interval :month
-    #   stripe_subscription_id 'gold_test_rspec'
-    # end
-    #
-    # factory :silver_plan do
-    #   name 'Silver'
-    #   price 1500
-    #   interval :month
-    #   stripe_subscription_id 'silver_test_rspec'
-    # end
   end
 end
