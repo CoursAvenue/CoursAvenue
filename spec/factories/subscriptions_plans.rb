bdf0a8827936a0c3095739123d398c40992d271e
Aliou Diallo <code@aliou.me>
2015-04-14 17:41:43 +0200

    Update factories.

--- spec/factories/subscriptions_plans.rb
+++ spec/factories/subscriptions_plans.rb
@@ -1,22 +1,26 @@
 FactoryGirl.define do
   factory :subscriptions_plan, class: 'Subscriptions::Plan' do
-    name 'bronze'
-    amount 0
-    interval :month
-    stripe_plan_id nil
+    name           { Faker::Name.name }
+    amount         { (15..30).to_a.sample * 100 }
+    interval       :month
+    stripe_plan_id { "#{name.parameterize}" }
 
     trait :gold_plan do
-      name 'Gold'
-      amount 3000
-      interval :month
+      name           'Gold'
+      amount         3000
+      interval       :month
       stripe_plan_id 'gold_test_rspec'
     end
 
     trait :silver_plan do
-      name 'Silver'
-      amount 1500
-      interval :month
+      name           'Silver'
+      amount         1500
+      interval       :month
       stripe_plan_id 'silver_test_rspec'
+    end
+
+    trait :empty do
+      stripe_plan_id nil
     end
   end
 end
