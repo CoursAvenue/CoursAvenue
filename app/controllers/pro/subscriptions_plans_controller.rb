050dc9ef2e933c09ef87a7ff29641052e1b3f459
Aliou Diallo <code@aliou.me>
2015-04-15 17:07:03 +0200

    Update subscription plans list view

--- app/controllers/pro/subscriptions_plans_controller.rb
+++ app/controllers/pro/subscriptions_plans_controller.rb
@@ -2,10 +2,12 @@ class Pro::SubscriptionsPlansController < Pro::ProController
   before_action :authenticate_pro_super_admin!
 
   def index
-    @monthly_plans = Subscriptions::Plan.monthly
-    @yearly_plans  = Subscriptions::Plan.yearly
+    # @monthly_plans = Subscriptions::Plan.monthly
+    # @yearly_plans  = Subscriptions::Plan.yearly
+    @plans = Subscriptions::Plan.all
   end
 
+  # TODO: Find an elegant way to have the amount in cents.
   def new
     @plan = Subscriptions::Plan.new
 
@@ -14,10 +16,20 @@ class Pro::SubscriptionsPlansController < Pro::ProController
 
   def create
     @plan = Subscriptions::Plan.new(permitted_params)
+
+    respond_to do |format|
+      if @plan.save
+        # format.html { redirect_to pro_subscriptions_plans_path, notice: 'Offre bien créé', status: 201 }
+      else
+        # format.html { render action: :new }
+      end
+    end
+
   end
 
   private
 
   def permitted_params
+    params.require(:subscriptions_plan).permit(:name, :amount, :interval)
   end
 end
