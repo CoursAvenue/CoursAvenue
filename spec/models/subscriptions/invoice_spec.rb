6701266979162c884e4d4bd988f917543cb42152
Aliou Diallo <code@aliou.me>
2015-04-16 17:35:26 +0200

    Create Stripe invoice before running tests.

--- spec/models/subscriptions/invoice_spec.rb
+++ spec/models/subscriptions/invoice_spec.rb
@@ -8,7 +8,13 @@ RSpec.describe Subscriptions::Invoice, type: :model do
     # Stripe::Invoice.all(limit: 100).each(&:delete)
   end
 
-  subject { FactoryGirl.create(:subscriptions_invoice) }
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  let(:plan)          { FactoryGirl.create(:subscriptions_plan) }
+  let(:structure)     { FactoryGirl.create(:structure, :with_contact_email) }
+  let(:token)         { stripe_helper.generate_card_token }
+  let(:subscription)  { plan.create_subscription!(structure, token) }
+
+  subject { FactoryGirl.create(:subscriptions_invoice, structure: structure, subscription: subscription) }
 
   it { should belong_to(:structure) }
   it { should belong_to(:subscription) }
@@ -21,7 +27,11 @@ RSpec.describe Subscriptions::Invoice, type: :model do
     end
 
     context "when there's a stripe_invoice_id" do
-      subject { create_invoice(subscription) }
+
+      before do
+        subject.stripe_invoice_id = structure.stripe_customer.invoices.first.id
+        subject.save
+      end
 
       it 'returs a Stripe::Invoice' do
         stripe_invoice = Stripe::Invoice
