39650a0750ed8d40dd3a31a471daf09906fbf7b4
Aliou Diallo <code@aliou.me>
2015-04-17 14:39:30 +0200

    Add trial period days to plans.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417112611) do
+ActiveRecord::Schema.define(version: 20150417121758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1175,6 +1175,7 @@ ActiveRecord::Schema.define(version: 20150417112611) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
+    t.integer  "trial_period_days"
   end
 
   add_index "subscriptions_plans", ["stripe_plan_id"], name: "index_subscriptions_plans_on_stripe_plan_id", unique: true, using: :btree
