1a661a03b44683a2649b3fd0546d48c6dc0adbd7
Aliou Diallo <code@aliou.me>
2015-04-17 11:46:15 +0200

    Delegate PDF generation to the PDFGenerator service.

--- app/models/subscriptions/invoice.rb
+++ app/models/subscriptions/invoice.rb
@@ -22,4 +22,30 @@ class Subscriptions::Invoice < ActiveRecord::Base
     Stripe::Invoice.retrieve(stripe_invoice_id)
   end
 
+  # The URL of the invoice as a PDF. (Hosted on S3)
+  #
+  # @return nil or a string.
+  def pdf_url
+    return nil if stripe_invoice_id.nil?
+    generate_pdf! if pdf_file_path.nil?
+
+    pdf_file_path
+  end
+
+  private
+
+  # Generate the PDF for the invoice.
+  #
+  # @return nil or a String
+  def generate_pdf!
+    self.pdf_file_path = PDFGenerator.generate_invoice(self, pdf_template)
+    save
+  end
+
+  # The template to use of the PDF generation.
+  #
+  # @return a String.
+  def pdf_template
+    'pro/subscriptions/invoices.pdf.haml'
+  end
 end
