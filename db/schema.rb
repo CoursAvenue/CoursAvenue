e0553a5ee1c47d1b8901dd052828e05533c92e90
Aliou Diallo <code@aliou.me>
2015-04-20 14:00:45 +0200

    Add an expiration date to subscriptions.

--- db/schema.rb
+++ db/schema.rb
@@ -1130,6 +1130,7 @@ ActiveRecord::Schema.define(version: 20150421115523) do
     t.datetime "deleted_at"
     t.integer  "structure_id"
     t.integer  "subscriptions_plan_id"
+    t.datetime "expires_at"
   end
 
   add_index "subscriptions", ["stripe_subscription_id"], name: "index_subscriptions_on_stripe_subscription_id", unique: true, using: :btree
