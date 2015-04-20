7158174ad04b03979d708f546c38ff583462704b
Aliou Diallo <code@aliou.me>
2015-04-20 15:45:28 +0200

    Create controller for invoices.

---
+++ spec/controllers/pro/structures/invoices_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Pro::Structures::InvoicesController, type: :controller do
+  include Devise::TestHelpers
+
+  before(:all) { StripeMock.start }
+  after(:all)  { StripeMock.stop }
+
+  it { should use_before_action(:authenticate_pro_admin!) }
+  it { should use_before_action(:set_structure) }
+
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  let!(:structure)    { FactoryGirl.create(:structure_with_admin, :with_contact_email) }
+  let!(:admin)        { structure.main_contact }
+  let(:plan)          { FactoryGirl.create(:subscriptions_plan) }
+  let(:token)         { stripe_helper.generate_card_token }
+
+  before do
+    sign_in admin
+  end
+
+  describe '#index' do
+    render_views
+
+    it 'renders the index template' do
+      get :index, structure_id: structure.id
+
+      expect(response).to render_template('index')
+    end
+  end
+end
