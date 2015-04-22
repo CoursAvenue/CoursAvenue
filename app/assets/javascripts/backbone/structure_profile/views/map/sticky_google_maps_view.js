3f7380673bc649bd8b126cbf206ce7e19ee1c688
Nima Izadi <nim.izadi@gmail.com>
2015-04-22 13:40:37 +0200

    Fixed map stickiness

--- app/assets/javascripts/backbone/structure_profile/views/map/sticky_google_maps_view.js
+++ app/assets/javascripts/backbone/structure_profile/views/map/sticky_google_maps_view.js
@@ -7,28 +7,26 @@ StructureProfile.module('Views.Map', function(Module, App, Backbone, Marionette,
             // If the current instance is the stycky map that appears on the right of the StructureProfile
             this.$('.google-map').addClass('google-map--medium-small');
             // Stick if it has to
-            if ($('[data-type=sticky-map]').data('stick') == true || $('[data-type=sticky-map]').data('stick') == 'true') {
-                var setStickyStyle,
-                    $view             = this.$el.closest('[data-type=sticky-map-container]'),
-                    $grid_item        = $view.closest('.grid__item'),
-                    initial_map_width = $view.width();
-                $view.sticky({
-                    updateOnScroll: true,
-                    z             : 10,
-                    oldWidth      : false,
-                    offsetTop     : 50,
-                    stopAtEl      : '#coursavenue-footer',
-                    onStick: function onStick () {
-                        $view.css({
-                            left : $grid_item.offset().left + parseInt($view.closest('.grid__item').css('padding-left'), 10) + 'px',
-                            width: initial_map_width
-                        });
-                    },
-                    onUnStick: function onUnStick () {
-                        $view.removeAttr('style');
-                    }
-                });
-            }
+            var setStickyStyle,
+                $view             = this.$el.closest('[data-type=sticky-map-container]'),
+                $grid_item        = $view.closest('.grid__item'),
+                initial_map_width = $view.width();
+            $view.sticky({
+                updateOnScroll: true,
+                z             : 10,
+                oldWidth      : false,
+                offsetTop     : 50,
+                stopAtEl      : '#coursavenue-footer',
+                onStick: function onStick () {
+                    $view.css({
+                        left : $grid_item.offset().left + parseInt($view.closest('.grid__item').css('padding-left'), 10) + 'px',
+                        width: initial_map_width
+                    });
+                },
+                onUnStick: function onUnStick () {
+                    $view.removeAttr('style');
+                }
+            });
             this.recenterMap();
         },
 
