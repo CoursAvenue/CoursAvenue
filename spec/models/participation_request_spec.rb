d6672cee08834ce32c0e4714e9335974bcfc038c
Aliou Diallo <code@aliou.me>
2015-04-27 11:37:27 +0200

    Wrap stripe tests in stripe context.
    This way we only need to define the stripe helper and the before/after
    blocks once.

--- spec/models/participation_request_spec.rb
+++ spec/models/participation_request_spec.rb
@@ -19,34 +19,43 @@ describe ParticipationRequest do
     end
   end
 
-  describe '#stripe_charge' do
+  context 'stripe' do
+
     before(:all) { StripeMock.start }
     after(:all)  { StripeMock.stop }
 
     let(:stripe_helper) { StripeMock.create_test_helper }
 
-    context "when there isn't a stripe_charge_id" do
-      it 'return nil' do
-        expect(subject.stripe_charge).to be_nil
+    describe '#stripe_charge' do
+
+      context "when there isn't a stripe_charge_id" do
+        it 'return nil' do
+          expect(subject.stripe_charge).to be_nil
+        end
+      end
+
+      context "when there's a stripe_charge_id" do
+        before do
+          source        = stripe_helper.generate_card_token
+          stripe_charge = Stripe::Charge.create({
+            amount:   (5..30).to_a.sample * 100,
+            currency: Subscription::CURRENCY,
+            source:   source
+          })
+          subject.stripe_charge_id = stripe_charge.id
+
+          subject.save
+        end
+
+        it 'returns the Stripe::Charge' do
+          expect(subject.stripe_charge).to_not be_nil
+          expect(subject.stripe_charge).to be_a(Stripe::Charge)
+        end
       end
     end
 
-    context "when there's a stripe_charge_id" do
-      before do
-        source        = stripe_helper.generate_card_token
-        stripe_charge = Stripe::Charge.create({
-          amount:   (5..30).to_a.sample * 100,
-          currency: Subscription::CURRENCY,
-          source:   source
-        })
-        subject.stripe_charge_id = stripe_charge.id
-
-        subject.save
       end
 
-      it 'returns the Stripe::Charge' do
-        expect(subject.stripe_charge).to_not be_nil
-        expect(subject.stripe_charge).to be_a(Stripe::Charge)
       end
     end
   end
