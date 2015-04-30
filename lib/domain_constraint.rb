45496ebd70dbdbeff33072392caee3cc668805e0
Nima Izadi <nim.izadi@gmail.com>
2015-04-30 12:05:13 +0200

    Remove canonical from config.ru

--- lib/domain_constraint.rb
+++ lib/domain_constraint.rb
@@ -1,8 +1,6 @@
 class DomainConstraint
 
   def matches?(request)
-    Bugsnag.notify(RuntimeError.new("DomainConstraint"),
-                                    { request: request, subdomain: request.subdomain })
     Structure.where(slug: request.subdomain).first.present?
   end
 end
