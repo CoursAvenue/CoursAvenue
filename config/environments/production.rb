bbc7e8bbadfb694f835ece9d21f04c5d154e6fb3
Nima Izadi <nim.izadi@gmail.com>
2015-04-30 11:34:18 +0200

    Put log level to info on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,8 @@ CoursAvenue::Application.configure do
   # config.force_ssl = false
 
   # See everything in the log (default is :info)
-  config.log_level = :warn
+  config.log_level = :info
+  # config.log_level = :warn
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
