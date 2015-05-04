af9194537cde05799117fe3bc9a73549fb8f79a6
Nima Izadi <nim.izadi@gmail.com>
2015-05-04 16:08:11 +0200

    Don't use analytics action stat but actual PR

--- app/controllers/pro/structures/statistics_controller.rb
+++ app/controllers/pro/structures/statistics_controller.rb
@@ -21,7 +21,9 @@ class Pro::Structures::StatisticsController < Pro::ProController
 
       @impressions[date] = entry.metric1.to_i
       @views[date]       = entry.metric2.to_i
-      @actions[date]     = entry.metric3.to_i
+      @actions[date] = ParticipationRequest.where(created_at: (date.beginning_of_day..date.end_of_day)).count
+      @actions[date] += Mailboxer::Conversation.where(created_at: (date.beginning_of_day..date.end_of_day),
+                                                      mailboxer_label_id: Mailboxer::Label::INFORMATION.id).count
     end
 
     @impressions_total_count = @impressions.values.reduce(&:+)
