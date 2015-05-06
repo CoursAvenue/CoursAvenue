24cf377b3d84119f2f1b1bdfa9097e15523f31e2
Nima Izadi <nim.izadi@gmail.com>
2015-05-06 15:38:01 +0200

    Fixed bug

--- app/models/website_parameter.rb
+++ app/models/website_parameter.rb
@@ -13,6 +13,6 @@ class WebsiteParameter < ActiveRecord::Base
   # @return WebsiteParameter
   def self.create_for_structure(structure)
     return structure.website_parameter if structure.website_parameter.present?
-    structure.create_website_parameter slug: @structure.slug, title: @structure.name
+    structure.create_website_parameter slug: structure.slug, title: structure.name
   end
 end
