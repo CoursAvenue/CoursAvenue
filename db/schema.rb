fa5720da9d38e1c5564f6ea487a3ba92eaffea80
Aliou Diallo <code@aliou.me>
2015-05-07 18:49:56 +0200

    Change type of amount column to have accurate values.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507112603) do
+ActiveRecord::Schema.define(version: 20150507162244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1181,7 +1181,7 @@ ActiveRecord::Schema.define(version: 20150507112603) do
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "amount"
+    t.float    "amount"
   end
 
   create_table "subscriptions_invoices", force: true do |t|
