3fa4fd5d7475a41b7b8219c80a6f72e7ccb9e41d
Nima Izadi <nim.izadi@gmail.com>
2015-05-11 15:31:35 +0200

    Removed trial will end event

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -8,18 +8,13 @@ class SubscriptionMailer < ActionMailer::Base
   default from: 'CoursAvenue <noreply@coursavenue.com>'
 
   def invoice_creation_notification(invoice)
-    decorated_invoice = invoice.decorate
-    mail to: "#{decorated_invoice.structure_name} noreply@coursavenue.com",
-      subject: 'coucou'
-  end
-
-  def trial_will_end(subscription, structure)
-    mail to:   structure.contact_email,
-      subject: "La période d'essai de votre abonnement arrive à sa fin"
+    # decorated_invoice = invoice.decorate
+    # mail to: "#{decorated_invoice.structure_name} noreply@coursavenue.com",
+    #   subject: 'coucou'
   end
 
   def subscription_canceled(structure, subscription)
-    mail to: structure.contact_email,
-      subject: 'Votre abonnement a été annulé'
+    # mail to: structure.contact_email,
+    #   subject: 'Votre abonnement a été annulé'
   end
 end
