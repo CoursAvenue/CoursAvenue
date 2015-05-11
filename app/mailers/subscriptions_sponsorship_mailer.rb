506a2838f3eb55ab3141dd4cf749e3eb2e041865
Aliou Diallo <code@aliou.me>
2015-05-11 11:22:37 +0200

    Update email sent to the user when sponsored.

--- app/mailers/subscriptions_sponsorship_mailer.rb
+++ app/mailers/subscriptions_sponsorship_mailer.rb
@@ -7,12 +7,13 @@ class SubscriptionsSponsorshipMailer < ActionMailer::Base
 
   default from: 'CoursAvenue <noreply@coursavenue.com>'
 
-  def sponsor_user(sponsorship, custom_message = nil)
+  def sponsor_user(sponsorship, message = nil)
+    @message     = message
     @sponsorship = sponsorship
     @structure   = sponsorship.subscription.structure
 
     mail to: sponsorship.sponsored_email,
-      subject: "#{sponsorship.subscription.structure} veut vous parrainer !"
+      subject: "#{ sponsorship.subscription.structure.name } veut vous parrainer !"
   end
 
   def subscription_reedeemed(sponsorship)
