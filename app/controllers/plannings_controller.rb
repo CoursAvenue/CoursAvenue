0761b20409c3dfe1bf18bfcf0ee1d3b1af40ea23
Nima Izadi <nim.izadi@gmail.com>
2015-05-13 10:46:21 +0200

    Fixed page on plannings controller

--- app/controllers/plannings_controller.rb
+++ app/controllers/plannings_controller.rb
@@ -5,7 +5,7 @@ class PlanningsController < ApplicationController
 
   def index
     search_params = { lat:48.8592, lng: 2.3417, order_by: :start_date,
-                      start_date: Date.today, per_page: 20 }
+                      start_date: Date.today, per_page: 20, page: (params[:page] || 1) }
     if params[:sort] == 'week-end'
       plannings = PlanningSearch.search(search_params.merge(week_days: [0, 6],
                                                              start_date: Date.today.beginning_of_week(:sunday) + 1.week ))
