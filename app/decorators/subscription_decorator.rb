3c643af41e6d3e7c1bd852c932a35aec5b542ecf
Nima Izadi <nim.izadi@gmail.com>
2015-05-13 18:37:31 +0200

    Added next_frequency to subscription decorator

--- app/decorators/subscription_decorator.rb
+++ app/decorators/subscription_decorator.rb
@@ -5,6 +5,14 @@ class SubscriptionDecorator < Draper::Decorator
     "tous les #{object.plan.interval == 'month' ? 'mois' : 'ans'}"
   end
 
+  def next_frequency
+    if object.plan.interval == 'month'
+      'Le mois suivant'
+    else
+      "L'année suivante"
+    end
+  end
+
   def remaining_trial_days
     (object.trial_end.to_date - Date.today).to_i
   end
