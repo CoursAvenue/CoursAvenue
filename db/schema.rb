be2ff1717f441a8f2adc705eb7d808036f3303b3
Aliou Diallo <code@aliou.me>
2015-05-13 11:21:12 +0200

    Add the number of maximum redemptions to coupons.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512143527) do
+ActiveRecord::Schema.define(version: 20150513090522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1179,6 +1179,7 @@ ActiveRecord::Schema.define(version: 20150512143527) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "amount"
+    t.integer  "max_redemptions"
   end
 
   create_table "subscriptions_invoices", force: true do |t|
