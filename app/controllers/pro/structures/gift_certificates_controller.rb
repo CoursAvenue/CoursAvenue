5dbb8239917d3f79c896947e1dc05f893454ca29
Aliou Diallo <code@aliou.me>
2015-05-22 15:32:20 +0200

    Add CRUD methods for gift certificates.

--- app/controllers/pro/structures/gift_certificates_controller.rb
+++ app/controllers/pro/structures/gift_certificates_controller.rb
@@ -8,6 +8,66 @@ class Pro::Structures::GiftCertificatesController < ApplicationController
     @gift_certificates = @structure.gift_certificates.decorate
   end
 
+  def new
+    @gift_certificate = @structure.gift_certificates.new
+
+    if request.xhr?
+      render layout: false
+    end
+  end
+
+  def edit
+    @gift_certificate = @structure.gift_certificates.find(params[:id])
+
+    if request.xhr?
+      render layout: false
+    end
+  end
+
+  def create
+    @gift_certificate = @structure.gift_certificates.new(gift_certificate_params)
+
+    respond_to do |format|
+      if @gift_certificate.save
+        format.html { redirect_to pro_structure_gift_certificates_path(@structure),
+                      notice: 'Votre bon cadeau a été créé avec succés.' }
+        format.js
+      else
+        format.html { render action: :new }
+        format.js
+      end
+    end
+  end
+
+  def update
+    @gift_certificate = @structure.gift_certificates.find(params[:id])
+
+    respond_to do |format|
+      if @gift_certificate.update_attributes(gift_certificate_params)
+        format.html { redirect_to pro_structure_gift_certificates_path(@structure),
+                      notice: 'Votre bon cadeau a été mis a jour avec succés.' }
+        format.js
+      else
+        format.html { render action: :new }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @gift_certificate = @structure.gift_certificates.find(params[:id])
+
+    respond_to do |format|
+      if @gift_certificate.destroy
+        format.html { redirect_to pro_structure_gift_certificates_path(@structure),
+                      notice: 'Le bon cadeau a été supprimé avec succés' }
+      else
+        format.html { redirect_to pro_structure_gift_certificates_path(@structure),
+                      error: 'Erreur lors de la suppression du bon cadeau, veuillez rééssayer.' }
+      end
+    end
+  end
+
   private
 
   def set_structure
--- spec/controllers/pro/structures/gift_certificates_controller_spec.rb
+++ spec/controllers/pro/structures/gift_certificates_controller_spec.rb
@@ -65,4 +65,129 @@ describe Pro::Structures::GiftCertificatesController, type: :controller do
       end
     end
   end
+
+  describe '#new' do
+    render_views
+
+    it 'renders the new template' do
+      get :new, structure_id: structure.slug
+
+      expect(response).to render_template('new')
+    end
+
+    it 'renders the edition form partial' do
+      get :new, structure_id: structure.slug
+
+      expect(response).to render_template(partial: '_form')
+    end
+
+    it 'assigns a new gift certificate' do
+      get :new, structure_id: structure.slug
+
+      expect(assigns(:gift_certificate)).to be_a_new(GiftCertificate)
+    end
+
+    context 'when xhr request' do
+      it "doens't render the layout" do
+        xhr :get, :new, structure_id: structure.id
+
+        expect(response).to_not render_with_layout('admin')
+      end
+    end
+  end
+
+  describe '#create' do
+    context 'with valid attributes' do
+      let(:gift_certificate_attributes) {
+        { gift_certificate: FactoryGirl.build(:gift_certificate).attributes }
+      }
+
+      it 'creates a new Gift Certificate' do
+        expect {
+          post :create, { structure_id: structure.id }.merge(gift_certificate_attributes)
+        }.to change { GiftCertificate.count }.by(1)
+      end
+
+      it 'redirect to the index page' do
+        post :create, { structure_id: structure.id }.merge(gift_certificate_attributes)
+
+        expect(response).to redirect_to(action: :index, structure_id: structure.slug)
+      end
+    end
+
+    context 'with missing attributes' do
+      it "doesn't create a new Gift Certificate" do
+        expect {
+          post :create, { structure_id: structure.id, gift_certificate: { name: Faker::Name.name } }
+        }.to_not change { GiftCertificate.count }
+      end
+
+      it 're-renders the :new action' do
+        post :create, { structure_id: structure.id, gift_certificate: { name: Faker::Name.name } }
+
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:gift_certificate) { FactoryGirl.create(:gift_certificate, structure: structure) }
+    context 'with valid attributes' do
+      let(:updated_name)     { gift_certificate.name + ' updated'}
+      let(:gift_certificate_attributes) {
+        { gift_certificate: { name: updated_name } }
+      }
+
+      it 'updates the gift certificate' do
+        patch :update, { structure_id: structure.id, id: gift_certificate.id }.
+          merge(gift_certificate_attributes)
+
+        gift_certificate.reload
+        expect(gift_certificate.name).to eq(updated_name)
+      end
+
+      it 'redirect to the index page' do
+        patch :update, { structure_id: structure.id, id: gift_certificate.id }.
+          merge(gift_certificate_attributes)
+
+        expect(response).to redirect_to(action: :index, structure_id: structure.slug)
+      end
+    end
+
+    context 'with missing attributes' do
+      let(:updated_name) { '' }
+      let(:gift_certificate_attributes) {
+        { gift_certificate: { name: updated_name } }
+      }
+
+      it "doesn't create a new Gift Certificate" do
+        current_name = gift_certificate.name
+        patch :update, { structure_id: structure.id, id: gift_certificate.id }.
+          merge(gift_certificate_attributes)
+
+        gift_certificate.reload
+        expect(gift_certificate.name).to eq(current_name)
+        expect(gift_certificate.name).to_not eq(updated_name)
+      end
+
+      it 're-renders the :new action' do
+        patch :update, { structure_id: structure.id, id: gift_certificate.id }.
+          merge(gift_certificate_attributes)
+
+        expect(response).to render_template(:new)
+      end
+    end
+
+  end
+
+  describe '#destroy' do
+    let(:gift_certificate) { FactoryGirl.create(:gift_certificate, structure: structure) }
+
+    it 'deletes the gift certificate' do
+      delete :destroy, id: gift_certificate.id, structure_id: structure.slug
+
+      gift_certificate.reload
+      expect(gift_certificate.deleted_at).to_not be_nil
+    end
+  end
 end
