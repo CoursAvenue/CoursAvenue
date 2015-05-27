02a3fb885e1ce53c74f61f8de535a1650965ccad
Nima Izadi <nim.izadi@gmail.com>
2015-05-27 13:45:36 +0200

    Removed validation on duplicate request

--- app/models/participation_request.rb
+++ app/models/participation_request.rb
@@ -57,7 +57,6 @@ class ParticipationRequest < ActiveRecord::Base
   # Validation                                                         #
   ######################################################################
   validates :date, presence: true
-  validate :request_is_not_duplicate, on: [:create]
 
   ######################################################################
   # Scopes                                                             #
@@ -371,19 +370,6 @@ class ParticipationRequest < ActiveRecord::Base
       self.end_time   ||= self.start_time + 1.hour if self.start_time
     end
     nil
-  end
-
-  # Check if the request is duplicate or not
-  #
-  # @return Boolean
-  def request_is_not_duplicate
-    if self.user.participation_requests.where(ParticipationRequest.arel_table[:created_at].gt(Date.today - 1.week)
-      .and(ParticipationRequest.arel_table[:planning_id].eq(self.planning_id))
-      .and(ParticipationRequest.arel_table[:date].eq(self.date))).any?
-    self.errors[:base] << "duplicate"
-    return false
-    end
-    true
   end
 
   # When a request is created (always by user), we alert the teacher
