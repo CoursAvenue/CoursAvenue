1823d6b73b134eb645d649e1e6f90f955b97fbf4
Nima Izadi <nim.izadi@gmail.com>
2015-05-27 14:51:16 +0200

    Formatted phone numbers

---
+++ app/decorators/phone_number_decorator.rb
@@ -0,0 +1,16 @@
+class PhoneNumberDecorator < Draper::Decorator
+
+  # Return string as: 0X XX XX XX XX
+  def formatted_number
+    number = object.number
+    number = number.gsub(' ', '').gsub('.', '').gsub('-', '')
+
+    PhoneNumber::FRANCE_PREFIXES.each do |prefix|
+      if number.starts_with? prefix
+        number = number.gsub prefix, "0#{prefix.last}" #.last because last is 6 or 7
+        break
+      end
+    end
+    "#{number[0..1]} #{number[2..3]} #{number[4..5]} #{number[6..7]} #{number[8..9]}"
+  end
+end
