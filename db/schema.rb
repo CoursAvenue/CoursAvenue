4baf43d672dcf0da8bb91beda893b20a7d68d503
Aliou Diallo <code@aliou.me>
2015-06-01 14:28:31 +0200

    Add card lock attribute.
    When the card are being generated or updated, we will lock their access
    by anything else.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527152827) do
+ActiveRecord::Schema.define(version: 20150601122412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1072,6 +1072,7 @@ ActiveRecord::Schema.define(version: 20150527152827) do
     t.string   "stripe_managed_account_id"
     t.string   "stripe_managed_account_secret_key"
     t.string   "stripe_managed_account_publishable_key"
+    t.boolean  "card_locked",                            default: false
   end
 
   add_index "structures", ["principal_mobile_id"], name: "index_structures_on_principal_mobile_id", using: :btree
