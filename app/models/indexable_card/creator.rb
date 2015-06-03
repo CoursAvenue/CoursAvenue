3745bf1bbda84916b76ca5de12e8b8238294561f
Aliou Diallo <code@aliou.me>
2015-06-03 11:18:32 +0200

    Create new cards.

--- app/models/indexable_card/creator.rb
+++ app/models/indexable_card/creator.rb
@@ -13,10 +13,12 @@ class IndexableCard::Creator
     subjects  = (@structure.subjects - plannings.flat_map(&:subjects)).compact
     places    = (@structure.places - plannings.flat_map(&:course).flat_map(&:place)).compact
 
+    # We loop on each plannings and create a card from there.
     plannings.each do |planning|
       @structure.indexable_cards.create_from_planning(planning)
     end
 
+    # We loop on each subjects and places not in the plannings above and create a card from there.
     subjects.each do |subject|
       places.each do |place|
         @structure.indexable_cards.create_from_subject_and_place(subject, place)
@@ -29,6 +31,26 @@ class IndexableCard::Creator
   # @return the card.
   def update_cards
     return create_cards if @structure.indexable_cards.empty?
+    new_cards = []
+
+    # We start by creating the cards from plannings since the associated subjects and places will
+    # also be in the `new_subjects` and `new_places`.
+    new_cards += new_plannings.map do |planning|
+      @structure.indexable_cards.create_from_planning(planning)
+    end
+
+    # We then get the new subjects and places but remove any of them if it is already in one of the
+    # newly created card.
+    _new_subjects = new_subjects - new_cards.map(&:planning).uniq
+    _new_places   = new_places - new_cards.map(&:place).uniq
+
+    new_cards += _new_subjects.flat_map do |subject|
+      _new_places.map do |place|
+        @structure.indexable_cards.create_from_subject_and_place(subject, place)
+      end
+    end
+
+    new_cards
   end
 
   private
@@ -40,17 +62,17 @@ class IndexableCard::Creator
     (@structure.plannings - @structure.indexable_cards.flat_map(&:planning)).compact
   end
 
-  # The courses not represented in the cards.
-  #
-  # @return nil or Array of courses
-  def new_courses
-    (@structure.courses - @structure.indexable_cards.flat_map(&:course)).compact
-  end
-
   # The subjects not represented in the cards.
   #
   # @return nil or Array of subjects
   def new_subjects
     (@structure.subjects - @structure.indexable_cards.flat_map(&:subjects)).compact
+  end
+
+  # The places not represented in the cards.
+  #
+  # @return nil or Array of places
+  def new_places
+    (@structure.places - @structure.indexable_cards.flat_map(&:place)).compact
   end
 end
--- spec/models/indexable_card/creator_spec.rb
+++ spec/models/indexable_card/creator_spec.rb
@@ -28,5 +28,67 @@ describe IndexableCard::Creator do
           to change { structure.indexable_cards.count }
       end
     end
+
+    context 'when there are cards' do
+      let(:new_subject)  { FactoryGirl.create(:subject) }
+      let(:new_course)   { FactoryGirl.create(:course, structure: structure) }
+      let(:new_planning) { FactoryGirl.create(:planning, course: new_course) }
+      let(:new_place)    { FactoryGirl.create(:place, structure: structure) }
+
+      before do
+        subject.create_cards
+      end
+
+      context "when there's a new planning" do
+        before do
+          new_planning
+          structure.reload
+        end
+
+        it 'creates a new card' do
+          expect { subject.update_cards }.
+            to change { structure.indexable_cards.count }.by(1)
+        end
+
+        it 'associates the new card with the planning' do
+          cards = subject.update_cards
+          expect(cards.first.planning).to eq(new_planning)
+        end
+      end
+
+      context "when there's a new subject" do
+        before do
+          structure.subjects << new_subject
+          structure.reload
+        end
+
+        it 'creates a new card' do
+          expect { subject.update_cards; structure.reload }.
+            to change { structure.indexable_cards.count }.by(1)
+        end
+
+        it 'associates the new card with the subject' do
+          cards = subject.update_cards
+          expect(cards.first.subjects).to include(new_subject)
+        end
+      end
+
+      context "when there's a new place" do
+        before do
+          structure.places << new_place
+          structure.reload
+        end
+
+        it 'creates a new card' do
+          expect { subject.update_cards }.
+            to change { structure.indexable_cards.count }.by(1)
+        end
+
+        it 'associates the new card with the place' do
+          cards = subject.update_cards
+          expect(cards.first.place).to eq(new_place)
+        end
+      end
+    end
   end
 end
