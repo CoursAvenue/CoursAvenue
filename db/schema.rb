1903d2720208de8679da2b9d0c9d3a7f4bc3073c
Aliou Diallo <code@aliou.me>
2015-06-05 16:31:28 +0200

    Empty migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528122619) do
+ActiveRecord::Schema.define(version: 20150605142845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
