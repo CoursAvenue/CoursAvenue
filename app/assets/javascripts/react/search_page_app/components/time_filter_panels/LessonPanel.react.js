bf40778ebde641ce66bdb88643253c37bb245c72
Aliou Diallo <code@aliou.me>
2015-06-15 17:16:05 +0200

    Extract time table into component.

--- app/assets/javascripts/react/search_page_app/components/time_filter_panels/LessonPanel.react.js
+++ app/assets/javascripts/react/search_page_app/components/time_filter_panels/LessonPanel.react.js
@@ -1,7 +1,20 @@
+var TimeTable = require('./time_table/TimeTable'),
+    FilterStore = require('../../stores/FilterStore');
+
 var LessonPanel = React.createClass({
+
+    getInitialState: function getInitialState () {
+        return { timeTable: FilterStore.timeFilters() };
+    },
+
     render: function render () {
         return (
-            <div>LessonPanel</div>
+          <div>
+              <h2>Quand &gt; Cours Réguliers</h2>
+              <h3>Sélectionnez vos disponibilités</h3>
+
+              <TimeTable timeTable={ this.props.timeTable } />
+          </div>
         );
     },
 });
---
+++ app/assets/javascripts/react/search_page_app/components/time_filter_panels/time_table/TimeTable.react.js
@@ -0,0 +1,7 @@
+var TimeTable = React.createClass({
+    render: function render () {
+        return false;
+    }
+});
+
+module.exports = TimeTable;
