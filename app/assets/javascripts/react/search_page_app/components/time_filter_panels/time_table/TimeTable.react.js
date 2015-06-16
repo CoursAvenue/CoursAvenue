23d66aa9da887908f2a2bf2335c1d5b81962a43b
Aliou Diallo <code@aliou.me>
2015-06-16 15:29:35 +0200

    Add period titles.

--- app/assets/javascripts/react/search_page_app/components/time_filter_panels/time_table/TimeTable.react.js
+++ app/assets/javascripts/react/search_page_app/components/time_filter_panels/time_table/TimeTable.react.js
@@ -21,6 +21,12 @@ var TimeTable = React.createClass({
             <div className='main-container'>
                 <div className='grid'>
                     <div className='grid__item one-third'>
+                        <div className=''>Empty</div>
+                        <div className=''>Matin (avant 12h)</div>
+                        <div className=''>Midi (12h-14h)</div>
+                        <div className=''>Après Midi (14h-18h)</div>
+                        <div className=''>Soirée (après 18h)</div>
+                        <div className=''>Toute la journée</div>
                     </div>
                     <div className='grid__item two-thirds'>
                         <div className='grid'>
