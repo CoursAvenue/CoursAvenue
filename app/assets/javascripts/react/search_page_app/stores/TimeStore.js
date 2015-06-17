8d6530311003e9e4a7aa3367e5a46c3334921296
Aliou Diallo <code@aliou.me>
2015-06-17 11:08:42 +0200

    Allow models to generate their algolia filters.

--- app/assets/javascripts/react/search_page_app/stores/TimeStore.js
+++ app/assets/javascripts/react/search_page_app/stores/TimeStore.js
@@ -4,12 +4,35 @@ var _                    = require('underscore'),
     SearchPageConstants  = require('../constants/SearchPageConstants');
 
 var ActionTypes = SearchPageConstants.ActionTypes;
+var PERIODS = ['morning', 'noon', 'afternoon', 'evening'];
 
 // A day in the time table.
 var DayColumn = Backbone.Model.extend({
     defaults: function defaults() {
         return { periods: [false, false, false, false] }
-    }
+    },
+
+    initialize: function initialize () {
+        _.bindAll(this, 'toAlgolia', 'setPeriod');
+    },
+
+    // Returns the current day periods as algolia filters, e.g. `monday-afternoon`.
+    toAlgolia: function toAlgolia () {
+        var filters = this.get('periods').map(function(selected, index) {
+            if (selected) {
+                return this.get('name') + "-" + PERIODS[index];
+            }
+        }.bind(this));
+
+        return filters;
+    },
+
+    // Select the period whose index is passed.
+    setPeriod: function setPeriod (periodIndex) {
+        periods = this.get('periods');
+        periods[periodIndex] = true;
+        this.set('periods', periods);
+    },
 });
 
 var TimeStore = Backbone.Collection.extend({
