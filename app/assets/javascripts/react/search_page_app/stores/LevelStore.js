21690059c8b66aeb45b894af80ef32b46435393e
Aliou Diallo <code@aliou.me>
2015-06-19 11:06:35 +0200

    Create level store.

---
+++ app/assets/javascripts/react/search_page_app/stores/LevelStore.js
@@ -0,0 +1,66 @@
+var _                    = require('underscore'),
+    Backbone             = require('backbone'),
+    SearchPageDispatcher = require('../dispatcher/SearchPageDispatcher'),
+    SearchPageConstants  = require('../constants/SearchPageConstants');
+
+var ActionTypes = SearchPageConstants.ActionTypes;
+
+var Level = Backbone.Model.extend({
+    defaults: function defaults () {
+        return { selected: false };
+    },
+
+    initialize: function initialize () {
+        _.bindAll(this, 'toggleSelection');
+    },
+
+    toggleSelection: function toggleSelection () {
+        this.set('selected', !this.get('selected'));
+    },
+});
+
+var LevelStore = Backbone.Collection.extend({
+    model: Level,
+
+    initialize: function initialize () {
+        _.bindAll(this, 'dispatchCallback');
+        this.dispatchToken = SearchPageDispatcher.register(this.dispatchCallback);
+    },
+
+    dispatchCallback: function dispatchCallback (payload) {
+        switch(payload.actionType) {
+            case ActionTypes.TOGGLE_LEVEL:
+                this.toggleLevelSelection(payload.data);
+                break;
+        }
+    },
+
+    toggleLevelSelection: function toggleLevelSelection (level) {
+        level.toggleSelection();
+        this.trigger('change');
+    },
+
+    algoliaFilters: function algoliaFilters () {
+        var filters = this.models.map(function(model) {
+            if (model.get('selected')) {
+                return (model.get('id'));
+            }
+        });
+        filters = _.compact(filters);
+
+        if (_.isEmpty(filters)) {
+            return false;
+        }
+
+        return filters;
+    },
+
+});
+
+module.exports = new LevelStore([
+    { id: 'level.all',          name: 'All' },
+    { id: 'level.beginner',     name: 'Beginner' },
+    { id: 'level.intermediate', name: 'Intermediate' },
+    { id: 'level.confirmed',    name: 'Confirmed' },
+    { id: 'level.professional', name: 'Professional' }
+]);
