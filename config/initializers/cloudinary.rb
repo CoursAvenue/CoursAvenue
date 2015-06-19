658576e77bb58fe6bac3d069b5a1fed2b07d0c65
Nima Izadi <nim.izadi@gmail.com>
2015-06-19 13:51:18 +0200

    Fixed cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,8 +1,8 @@
 Cloudinary.config do |config|
   config.cdn_subdomain = true
-  config.cloud_name    = ENV['CLOUDINARY_PRODUCTION_CLOUD_NAME']
-  config.api_key       = ENV['CLOUDINARY_PRODUCTION_API_KEY']
-  config.api_secret    = ENV['CLOUDINARY_PRODUCTION_SECRET']
+  config.cloud_name    = ENV['CLOUDINARY_CLOUD_NAME']
+  config.api_key       = ENV['CLOUDINARY_API_KEY']
+  config.api_secret    = ENV['CLOUDINARY_SECRET']
   config.secure        = true
   config.sign_url      = true
 end
