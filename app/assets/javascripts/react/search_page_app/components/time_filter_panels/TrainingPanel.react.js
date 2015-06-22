2cf5e771247f8d5842db932e3617f08a65389a9d
Aliou Diallo <code@aliou.me>
2015-06-22 16:01:40 +0200

    Make sure the TrainingPanel updates on store change.

--- app/assets/javascripts/react/search_page_app/components/time_filter_panels/TrainingPanel.react.js
+++ app/assets/javascripts/react/search_page_app/components/time_filter_panels/TrainingPanel.react.js
@@ -1,22 +1,30 @@
 var FilterActionCreators = require('../../actions/FilterActionCreators'),
+    TimeStore            = require('../../stores/TimeStore'),
     TimePicker           = require('./training/TimePicker');
+    FluxBoneMixin        = require('../../../mixins/FluxBoneMixin');
 
 var TrainingPanel = React.createClass({
+
+    mixins: [
+        FluxBoneMixin('time_store')
+    ],
+
+    getInitialState: function getInitialState () {
+        return { time_store: TimeStore };
+    },
 
     closePanel: function closePanel () {
         FilterActionCreators.toggleTimeFilter();
     },
 
-    setDate: function setDate () {
-        debugger
-    },
-
     render: function render () {
+        var start_date = this.state.time_store.get('training_start_date');
+        var end_date = this.state.time_store.get('training_end_date');
         return (
             <div>
                 <h2>Indiquez vos disponibilités</h2>
-                <TimePicker label="Du" />
-                <TimePicker label="Au" />
+                <TimePicker label="Du" attribute="start_date" initialValue={ start_date } />
+                <TimePicker label="Au" attribute="end_date" initialValue={ end_date } />
                 <a onClick={ this.closePanel } className='btn'>Valider</a>
             </div>
         );
