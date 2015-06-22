69abc4d092133d7b973c943146813dcf25a20714
Aliou Diallo <code@aliou.me>
2015-06-22 15:18:01 +0200

    Create TimePicker on element.

--- app/assets/javascripts/react/search_page_app/components/time_filter_panels/training/TimePicker.react.js
+++ app/assets/javascripts/react/search_page_app/components/time_filter_panels/training/TimePicker.react.js
@@ -1,19 +1,40 @@
+var TimeActionCreators = require('../../../actions/TimeActionCreators');
+
 var TimePicker = React.createClass({
     propTypes: {
-        label: React.PropTypes.string.isRequired
+        label:        React.PropTypes.string.isRequired,
+        attribute:    React.PropTypes.string.isRequired,
+        initialValue: React.PropTypes.string
     },
 
-    setDate: function setDate(event) {
-        console.log('changing');
+    setDate: function setDate (event) {
+        TimeActionCreators.setTrainingDate({
+            value:     $(event.currentTarget).val(),
+            attribute: this.props.attribute,
+        });
+    },
+
+    componentDidMount: function componentDidMount () {
+        var $picker = $(this.getDOMNode()).children('input');
+        var options = {
+            format: COURSAVENUE.constants.DATE_FORMAT,
+            weekStart: 1,
+            language: 'fr',
+            autoclose: true,
+            todayHighlight: true,
+            startDate: new Date(),
+        };
+        $picker.datepicker(options);
+        $picker.on('changeDate', this.setDate);
     },
 
     render: function render () {
         return (
-            <div className='soft-half'>
+            <div className='soft-half' data-behavior='datepicker'>
                 <label className='inline-block soft-half--right'>
                     { this.props.label }
                 </label>
-                <input onChange={ this.setDate } />
+                <input className='datepicker-input' onChange={ this.setDate } value={ this.props.initialValue } />
             </div>
         );
     },
