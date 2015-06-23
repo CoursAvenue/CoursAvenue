74ee75f0a132dfae5a29583dfaa95257dfe8ae5a
Aliou Diallo <code@aliou.me>
2015-06-23 18:30:05 +0200

    Add indexing of metro stops.

--- app/models/metro/stop.rb
+++ app/models/metro/stop.rb
@@ -1,4 +1,8 @@
+# TODO: Import metro stops.
+# http://data.ratp.fr/fr/les-donnees.html
+# https://raw.githubusercontent.com/vied12/timereader/master/data/stations.json
 class Metro::Stop < ActiveRecord::Base
+  include AlgoliaSearch
   extend FriendlyId
   friendly_id :name, use: [:slugged, :finders]
 
@@ -7,4 +11,20 @@ class Metro::Stop < ActiveRecord::Base
   validates :name,      presence: true
   validates :latitude,  presence: true
   validates :longitude, presence: true
+
+  # TODO: Enable indexing when it will be needed.
+  # :nocov:
+  algoliasearch per_environment: true, disable_indexing: true do
+    attribute :id
+    attribute :name
+
+    add_attribute :_geoloc do
+      { lat: self.latitude, lng: self.longitude }
+    end
+
+    add_attribute :metro_lines do
+      self.lines.map(&:slug)
+    end
+  end
+  # :nocov:
 end
