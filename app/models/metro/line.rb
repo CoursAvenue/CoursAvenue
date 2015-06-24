43ed044aa5b05c76c84ef0278070010991c76784
Aliou Diallo <code@aliou.me>
2015-06-24 10:37:07 +0200

    Index Metro::Line

--- app/models/metro/line.rb
+++ app/models/metro/line.rb
@@ -1,8 +1,21 @@
 class Metro::Line < ActiveRecord::Base
+  include AlgoliaSearch
   extend FriendlyId
   friendly_id :name, use: [:slugged, :finders]
 
   has_and_belongs_to_many :stops, class_name: 'Metro::Stop'
 
   validates :name, presence: true
+
+  # :nocov:
+  algoliasearch per_environment: true, disable_indexing: Rails.env.test? do
+    attribute :id
+    attribute :slug
+    attribute :name
+
+    add_attribute :metro_stops do
+      self.lines.map(&:slug)
+    end
+  end
+  # :nocov:
 end
