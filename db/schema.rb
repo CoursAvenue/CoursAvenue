a23d112e83ccd4f35c45d95c0af3d3f3914c5edd
Aliou Diallo <code@aliou.me>
2015-06-24 10:52:24 +0200

    Store the line number as a string.
    This allows us to have '3 bis' and stuff.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623090505) do
+ActiveRecord::Schema.define(version: 20150624084840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -629,7 +629,7 @@ ActiveRecord::Schema.define(version: 20150623090505) do
   create_table "metro_lines", force: true do |t|
     t.string   "name"
     t.string   "slug"
-    t.integer  "line_number"
+    t.string   "line_number"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/factories/metro_lines.rb
+++ spec/factories/metro_lines.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :metro_line, :class => 'Metro::Line' do
     name 'Ligne 8'
-    line_number 8
+    line_number '8'
   end
 
 end
