fd6f8efff50949f1b7dd1bbeb262086c20b40155
Aliou Diallo <code@aliou.me>
2015-06-25 15:54:38 +0200

    Create Metro Line Store.

---
+++ app/assets/javascripts/react/search_page_app/stores/MetroLineStore.js
@@ -0,0 +1,68 @@
+var _                    = require('underscore'),
+    Backbone             = require('backbone'),
+    SearchPageDispatcher = require('../dispatcher/SearchPageDispatcher'),
+    SearchPageConstants  = require('../constants/SearchPageConstants'),
+    FluxBoneMixin        = require('../../mixins/FluxBoneMixin');
+
+var MetroLine = Backbone.Model.extend({
+    defaults: function defaults () {
+        return { selected: false };
+    },
+
+    initialize: function initialize () {
+        _.bindAll(this, 'toggleSelection');
+    },
+
+    toggleSelection: function toggleSelection () {
+        this.set('selected', !this.get('selected'));
+    },
+});
+
+var MetroLineStore = Backbone.Collection.extend({
+    model: MetroLine,
+
+    initialize: function initialize () {
+        _.bindAll(this, 'dispatchCallback', 'selectLine');
+        this.dispatchToken = SearchPageDispatcher.register(this.dispatchCallback);
+    },
+
+    dispatchCallback: function dispatchCallback (payload) {
+        switch(payload.actionType) {
+            case ActionTypes.SELECT_METRO_LINE:
+                this.selectLine(payload.data);
+                break;
+        }
+    },
+
+    selectLine: function selectLine (metro_line_slug) {
+        var current_line = this.findWhere({ selected: true });
+        var metro_line   = this.findWhere({ slug: metro_line_slug });
+
+        if (current_line) { current_line.toggleSelection(); }
+        if (metro_line)   { metro_line.toggleSelection(); }
+        this.trigger('change');
+    },
+
+    getSelectedLine: function getSelectedLine () {
+        return (this.findWhere({ selected: true }) || this.models[0]);
+    },
+});
+
+module.exports = new MetroLineStore([
+    { name: 'Ligne 1',     slug: 'ligne-1' },
+    { name: 'Ligne 2',     slug: 'ligne-2' },
+    { name: 'Ligne 3',     slug: 'ligne-3' },
+    { name: 'Ligne 3 Bis', slug: 'ligne-3-bis' },
+    { name: 'Ligne 4',     slug: 'ligne-4' },
+    { name: 'Ligne 5',     slug: 'ligne-5' },
+    { name: 'Ligne 6',     slug: 'ligne-6' },
+    { name: 'Ligne 7',     slug: 'ligne-7' },
+    { name: 'Ligne 7 Bis', slug: 'ligne-7-bis' },
+    { name: 'Ligne 8',     slug: 'ligne-8' },
+    { name: 'Ligne 9',     slug: 'ligne-9' },
+    { name: 'Ligne 10',    slug: 'ligne-10' },
+    { name: 'Ligne 11',    slug: 'ligne-11' },
+    { name: 'Ligne 12',    slug: 'ligne-12' },
+    { name: 'Ligne 13',    slug: 'ligne-13' },
+    { name: 'Ligne 14',    slug: 'ligne-14' },
+]);
