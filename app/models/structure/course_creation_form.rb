06998d766dfdd63e0c9966aac609b7007778c623
Aliou Diallo <code@aliou.me>
2015-07-03 16:56:07 +0200

    Create Form object for the course creation.

---
+++ app/models/structure/course_creation_form.rb
@@ -0,0 +1,74 @@
+class Structure::CourseCreationForm
+  include ActiveModel::Model
+  include Virtus.model
+
+  attr_reader :structure
+  attr_reader :course
+  attr_reader :place
+
+  attribute :structure_slug, String
+  validates :structure_slug, presence: true
+
+  # Course attributes
+  attribute :course_name, String
+  validates :course_name, presence: true
+
+  attribute :course_type, String
+  validates :course_type, presence: true
+
+  attribute :course_subject_ids, Array[Integer]
+  validates :course_subject_ids, presence: true
+
+  # attribute :course_prices, Array
+  # validates :course_prices, presence: true
+
+  attribute :course_frequency, String
+
+  # Place attributes
+  attribute :place_name, String
+  validates :place_name, presence: true
+
+  attribute :place_street, String
+  validates :place_street, presence: true
+
+  attribute :place_zip_code, String
+  validates :place_zip_code, presence: true
+
+  attribute :place_city_id, Integer
+  validates :place_city_id, presence: true
+
+  # "Save" the Course Creation form.
+  # We don't really save the object, but persist the attributes of the object in the related Models,
+  # here the Course and Price models.
+  #
+  # @return Boolean, whether the object has been "saved".
+  def save
+    @structure = Structure.find(@structure_slug)
+    valid? ? persist! : false
+  end
+
+  private
+
+  def persist!
+    @course_subject_ids = @course_subject_ids.map(&:to_i)
+
+    @place = @structure.places.create(
+      name: @place_name,
+      street: @place_street,
+      zip_code: @place_zip_code,
+      city_id: @place_city_id
+    )
+    return false if @place.persisted?
+
+    @course = @structure.courses.build(
+      type: @course_type,
+      name: @course_name,
+      subjects_ids: @course_subject_ids,
+      prices_attributes: @course_prices,
+      place_id: @place.id
+    )
+    return false if @course.persisted?
+
+    true
+  end
+end
---
+++ spec/models/structure/course_creation_form_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe Structure::CourseCreationForm do
+  context 'validations' do
+    it { should validate_presence_of(:structure_slug) }
+
+    it { should validate_presence_of(:course_name) }
+    it { should validate_presence_of(:course_type) }
+    it { should validate_presence_of(:course_subject_ids) }
+    it { should validate_presence_of(:course_prices) }
+
+    it { should validate_presence_of(:place_name) }
+    it { should validate_presence_of(:place_street) }
+    it { should validate_presence_of(:place_zip_code) }
+    it { should validate_presence_of(:place_city_id) }
+  end
+
+  let!(:structure) { FactoryGirl.create(:structure) }
+
+  describe '#save' do
+    context 'when not valid' do
+      subject { Structure::CourseCreationForm.new(structure_slug: structure.slug) }
+
+      it { expect(subject.save).to be_falsy }
+
+      it "doesn't create the place" do
+        expect { subject.save }.to_not change { Place.count }
+      end
+
+      it "doesn't create the course" do
+        expect { subject.save }.to_not change { Course.count }
+      end
+    end
+
+    context 'when valid' do
+      subject { Structure::CourseCreationForm.new(valid_params(structure)) }
+
+      it 'creates the place' do
+        expect { subject.save }.to change { Place.count }.by(1)
+      end
+
+      it 'creates the course' do
+        expect { subject.save }.to change { Course.count }.by(1)
+      end
+    end
+  end
+
+  def valid_params(structure)
+    subject_ = FactoryGirl.create(:subject_children)
+    price = FactoryGirl.build(:registration)
+    city = FactoryGirl.create(:city)
+
+    {
+      structure_slug: structure.slug,
+
+      course_name: Faker::Name.name + ' course',
+      course_type: ["Course::Lesson", "Course::Training", "Course::Private"].sample,
+      course_subject_ids: [subject_.id],
+      # course_prices: [price.attributes.compact],
+      course_frequency: Course::COURSE_FREQUENCIES.sample,
+
+      place_name: Faker::Name.name + ' place',
+      place_street: Faker::Address.street_address,
+      place_zip_code: Faker::Address.zip_code,
+      place_city_id: city.id
+    }
+  end
+end
