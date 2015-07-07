27e3a121b95769225bc69c0d72c62b8f264390a4
Aliou Diallo <code@aliou.me>
2015-07-07 16:08:07 +0200

    Create Guide model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630135638) do
+ActiveRecord::Schema.define(version: 20150707135146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -458,6 +458,13 @@ ActiveRecord::Schema.define(version: 20150630135638) do
   end
 
   add_index "gift_certificates", ["structure_id"], name: "index_gift_certificates_on_structure_id", using: :btree
+
+  create_table "guides", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "indexable_cards", force: true do |t|
     t.integer  "structure_id"
