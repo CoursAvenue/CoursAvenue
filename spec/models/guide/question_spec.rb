3048dbcd34f6ce55c5b65794472f501888099bd1
Aliou Diallo <code@aliou.me>
2015-07-08 16:51:58 +0200

    Fix typo.

--- spec/models/guide/question_spec.rb
+++ spec/models/guide/question_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Guide::Question, type: :model, user_guide: true do
   context 'validations' do
     it { should validate_presence_of(:content) }
     it { should validate_presence_of(:ponderation) }
-    it { should accept_nested_attributes_for(:questions) }
+    it { should accept_nested_attributes_for(:answers) }
   end
 
   context 'assocations' do
