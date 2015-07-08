f2f1c23de5205170a59c102d7db2ae22934c08ff
Aliou Diallo <code@aliou.me>
2015-07-08 13:53:51 +0200

    Allow edition of questions through the related guide.

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,7 +1,29 @@
 class Guide < ActiveRecord::Base
-  has_many :questions, class_name: 'Guide::Question'
-  has_many :answers,   class_name: 'Guide::Answer'
+  attr_accessible :title, :description, :questions_attributes
+
+  has_many :questions, class_name: 'Guide::Question', dependent: :destroy
+  has_many :answers,   class_name: 'Guide::Answer', dependent: :destroy
 
   validates :title,       presence: true
   validates :description, presence: true
+
+  accepts_nested_attributes_for :questions,
+                                reject_if: :reject_question,
+                                allow_destroy: true
+  private
+
+  # Method to check if we reject the question.
+  #
+  # We reject the question if the content is empty and the questions doesn't already exists.
+  # If the question exists and the content is empty, we add the attribute to delete the question.
+  #
+  # @return Boolean
+  def reject_question(attributes)
+    exists = attributes[:id].present?
+    if exists and attributes[:content].blank?
+      attributes.merge!({ :_destroy => 1 })
+    end
+
+    return (!exists and attributes[:content].blank?)
+  end
 end
--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Guide, type: :model, user_guide: true do
   context 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
+    it { should accept_nested_attributes_for(:questions) }
   end
 
   context 'associations' do
