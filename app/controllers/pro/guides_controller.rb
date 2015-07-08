c782585131f1ca8532a6ac478535fc2470e4dd36
Aliou Diallo <code@aliou.me>
2015-07-08 16:48:57 +0200

    Add other CRUD actions.

--- app/controllers/pro/guides_controller.rb
+++ app/controllers/pro/guides_controller.rb
@@ -16,4 +16,52 @@ class Pro::GuidesController < Pro::ProController
       render layout: false
     end
   end
+
+  def create
+    @guide = Guide.new(guide_params)
+
+    if @guide.save
+      redirect_to pro_guides_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @guide = Guide.find(params[:id])
+    3.times { q = @guide.questions.build }
+    @guide.questions.each do |q|
+      (4 - q.answers.count).times { q.answers.build }
+    end
+  end
+
+  def update
+    @guide = Guide.find(params[:id])
+
+    if @guide.update_attributes(guide_params)
+      redirect_to pro_guides_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @guide = Guide.find(params[:id])
+
+    if @guide.destroy
+      redirect_to pro_guides_path, notice: 'Guide supprimé'
+    else
+      redirect_to pro_guides_path, error: 'Une erreur est survenue, veuillez rééssayer'
+    end
+  end
+
+  private
+
+  def guide_params
+    params.require(:guide).permit(
+      :title,
+      :description,
+      :questions_attributes => [:content, :ponderation, :answers_attributes => [:content] ]
+    )
+  end
 end
--- spec/controllers/pro/guides_controller_spec.rb
+++ spec/controllers/pro/guides_controller_spec.rb
@@ -54,4 +54,122 @@ describe Pro::GuidesController, user_guide: true do
       expect(:response).to render_template(partial:'_form')
     end
   end
+
+  describe 'POST #create' do
+    context 'with invalid params' do
+      render_views
+      let(:params) { { guide: { foo: 'bar' } } }
+      it "doens't create a guide" do
+        expect { post :create, params }.to_not change { Guide.count }
+      end
+
+      it 'renders the `new` template' do
+        post :create, params
+        expect(response).to render_template('new')
+      end
+    end
+
+    context 'with valid params' do
+      let(:params) { { guide: valid_guide_params } }
+
+      it 'creates a new guide' do
+        expect { post :create, params }.to change { Guide.count }.by(1)
+      end
+
+      it 'creates new questions' do
+        expect { post :create, params }.to change { Guide::Question.count }.by(2)
+      end
+
+      it 'creates new answers' do
+        expect { post :create, params }.to change { Guide::Answer.count }.by(2)
+      end
+
+      it 'redirect to the list page' do
+        post :create, params
+        expect(response).to redirect_to(pro_guides_path)
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    render_views
+
+    let(:guide) { FactoryGirl.create(:guide) }
+
+    it 'assigns an existing guide' do
+      get :edit, id: guide.id
+      expect(assigns(:guide)).to be_a(Guide)
+    end
+
+    it 'renders the form' do
+      get :edit, id: guide.id
+      expect(:response).to render_template(partial:'_form')
+    end
+  end
+
+  describe 'PATCH #update' do
+    let(:guide) { FactoryGirl.create(:guide) }
+
+    context 'with invalid params' do
+      render_views
+
+      let(:invalid_params) { { title: nil, foo: 'bar' } }
+
+      it "doesn't update the guide" do
+        expect { patch :update, { id: guide.id, guide: invalid_params } }.to_not change { guide }
+      end
+
+      it 'renders the `edit` template' do
+        patch :update, { id: guide.id, guide: invalid_params }
+        expect(response).to render_template('edit')
+      end
+    end
+
+    context 'with valid params' do
+      it 'updates the guide' do
+        current_title = guide.title
+        updated_title = guide.title + " updated"
+        expect { patch :update, { id: guide.id, guide: valid_updated_guide_params(guide) }; guide.reload }.
+          to change { guide.title }.from(current_title).to(updated_title)
+      end
+
+      it 'redirects to the list page' do
+        patch :update, { id: guide.id, guide: valid_updated_guide_params(guide) }
+        expect(response).to redirect_to(pro_guides_path)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:guide) { FactoryGirl.create(:guide) }
+
+    it 'destroys the guide' do
+      delete :destroy, id: guide.id
+      expect(Guide.where(id: guide.id)).to be_empty
+    end
+
+    it 'redirects to the guide list' do
+      delete :destroy, id: guide.id
+      expect(response).to redirect_to(pro_guides_path)
+    end
+  end
+
+  def valid_guide_params
+    q1 = { content: Faker::Lorem.sentence, ponderation: 0,
+           answers_attributes: [ { content: Faker::Lorem.sentence } ] }
+    q2 = { content: Faker::Lorem.sentence, ponderation: 0,
+           answers_attributes: [ { content: Faker::Lorem.sentence } ] }
+
+    {
+      title: Faker::Lorem.sentence,
+      description: Faker::Lorem.paragraph(3),
+      questions_attributes: [q1, q2]
+    }
+  end
+
+  def valid_updated_guide_params(guide)
+    {
+      title: guide.title + " updated"
+    }
+  end
 end
