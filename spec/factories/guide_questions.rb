68f4ccd2cf0a0f6727eca958c2e3e59f6a567c6d
Aliou Diallo <code@aliou.me>
2015-07-09 14:05:39 +0200

    Improve guide/question fixture.

--- spec/factories/guide_questions.rb
+++ spec/factories/guide_questions.rb
@@ -2,6 +2,13 @@ FactoryGirl.define do
   factory :guide_question, :class => 'Guide::Question' do
     # guide nil
     ponderation 1
-    content Faker::Lorem.sentence
+    content { Faker::Lorem.sentence }
+
+    trait :with_answers do
+      after(:create) do |question|
+        3.times { question.answers << FactoryGirl.create(:guide_answer, question: question) }
+      end
+    end
+
   end
 end
