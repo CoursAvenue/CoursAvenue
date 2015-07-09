12f7868409a249496927349a2a9a89d17428a630
Aliou Diallo <code@aliou.me>
2015-07-09 14:06:58 +0200

    Add Guide#subject.

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -13,6 +13,14 @@ class Guide < ActiveRecord::Base
   accepts_nested_attributes_for :questions,
                                 reject_if: :reject_question,
                                 allow_destroy: true
+
+  # The subjects associated with different answers of this guide.
+  #
+  # @return An array of subjects.
+  def subjects
+    answers.flat_map(&:subjects).uniq
+  end
+
   private
 
   # Method to check if we reject the question.
--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe Guide, type: :model, user_guide: true do
     it { should have_many(:questions).class_name('Guide::Question') }
     it { should have_many(:answers).class_name('Guide::Answer').through(:questions) }
   end
+
+  describe 'GET #subjects' do
+    subject { FactoryGirl.create(:guide) }
+    let!(:questions) {
+      3.times.map { FactoryGirl.create(:guide_question, :with_answers, guide: subject) }
+    }
+
+    it 'returns the subjects associated with the answers' do
+      subjects = subject.answers.flat_map(&:subjects).uniq
+      expect(subject.subjects).to match_array(subjects)
+    end
+
+  end
 end
