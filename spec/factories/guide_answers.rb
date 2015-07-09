aac4bcc19dbecc70a48353980f3bf6b4edd7a7b4
Aliou Diallo <code@aliou.me>
2015-07-09 13:49:57 +0200

    Improve Guide::Answer factory.

--- spec/factories/guide_answers.rb
+++ spec/factories/guide_answers.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :guide_answer, :class => 'Guide::Answer' do
-    guide nil
-guide_question nil
-content "MyString"
-  end
+    # guide nil
+    # guide_question nil
+    content { Faker::Lorem.sentence }
 
+    trait :with_subjects do
+      subjects { [ FactoryGirl.create(:subject), FactoryGirl.create(:subject) ] }
+    end
+  end
 end
