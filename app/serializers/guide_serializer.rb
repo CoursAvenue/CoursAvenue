87afbf1caae7983b6dfa8bdb4d3ca727e8ddd768
Aliou Diallo <code@aliou.me>
2015-07-10 14:04:27 +0200

    Initialize questions and answers in app component.

--- app/serializers/guide_serializer.rb
+++ app/serializers/guide_serializer.rb
@@ -2,4 +2,5 @@ class GuideSerializer < ActiveModel::Serializer
   delegate :attributes, to: :object
 
   has_many :questions, serializer: Guide::QuestionSerializer
+  has_many :answers, serializer: Guide::AnswerSerializer
 end
