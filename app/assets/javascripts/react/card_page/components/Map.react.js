ba1fb158acd29f74c2444d45b01fd62f9f9cd431
Nima Izadi <nim.izadi@gmail.com>
2015-07-10 15:41:48 +0200

    Remove metros from card page map

--- app/assets/javascripts/react/card_page/components/Map.react.js
+++ app/assets/javascripts/react/card_page/components/Map.react.js
@@ -5,7 +5,6 @@ var MapComponent = React.createClass({
         // Provide your access token
         L.mapbox.accessToken = ENV.MAPBOX_ACCESS_TOKEN;
         this.createMap();
-        this.addMetros();
         this.addMarker()
     },
 
@@ -19,17 +18,6 @@ var MapComponent = React.createClass({
             icon: L.divIcon({ className: 'map-box-marker on-top' })
         });
         this.map.addLayer(marker);
-    },
-
-    addMetros: function addMetros () {
-        _.each(this.props.metro_stops, function(metro_stop) {
-            var marker = L.marker([metro_stop.latitude, metro_stop.longitude], {
-                icon: L.divIcon({ className: 'nowrap', html: '<div class="metro-line metro-line-m-for-map v-middle">M</div><span class="v-middle metro-line-station-name">' + metro_stop.name + '</span>' })
-            });
-            var string_popup = React.renderToString(<MetroMarkerPopup metro_stop={metro_stop} />);
-            marker.bindPopup(string_popup, { className: 'ca-leaflet-popup' });
-            this.map.addLayer(marker);
-        }.bind(this));
     },
 
     render: function render () {
