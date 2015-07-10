0615dad01dd74f779199080557f3d5b21f88db57
Aliou Diallo <code@aliou.me>
2015-07-10 17:32:04 +0200

    Update ResultComponent to use the SubjectStore.

--- app/assets/javascripts/react/user_guide/components/Results.react.js
+++ app/assets/javascripts/react/user_guide/components/Results.react.js
@@ -1,26 +1,31 @@
-var AnswerStore   = require('../stores/AnswerStore'),
-    MainSubject   = require('./results/MainSubject'),
-    SubjectList   = require('./results/SubjectList'),
-    CourseSearch  = require('./results/CourseSearch'),
-    FluxBoneMixin = require('../../mixins/FluxBoneMixin');
+var _           = require('underscore'),
+  SubjectStore  = require('../stores/AnswerStore'),
+  MainSubject   = require('./results/MainSubject'),
+  SubjectList   = require('./results/SubjectList'),
+  CourseSearch  = require('./results/CourseSearch'),
+  FluxBoneMixin = require('../../mixins/FluxBoneMixin');
 
 var Results = React.createClass({
     mixins: [
-        FluxBoneMixin(['answer_store'])
+        FluxBoneMixin(['subject_store'])
     ],
 
     getInitialState: function getInitialState () {
-        return { answer_store: AnswerStore };
+        return { subject_store: SubjectStore };
     },
 
     render: function render () {
-        return (
-            <div className='section relative one-whole relative white full-screen-item bg-cover'>
-                <MainSubject />
-                <SubjectList />
-                <CourseSearch />
-            </div>
-        );
+      // The store is sorted in ASC order on the score.
+      var main_subject   = SubjectStore.last(); // The last.
+      var other_subjects = SubjectStore.initial(); // Everything but the last.
+
+      return (
+          <div className='section relative one-whole relative white full-screen-item bg-cover'>
+              <MainSubject subject={ main_subject } />
+              <CourseSearch subject={ main_subject } />
+              <SubjectList subjects={ other_subjects } />
+          </div>
+      );
     },
 });
 
