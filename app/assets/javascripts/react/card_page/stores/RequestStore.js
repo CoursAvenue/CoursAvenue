fa2679625cf7ba03bbc003489254f85c30f3146a
Nima Izadi <nim.izadi@gmail.com>
2015-07-12 12:56:29 +0200

    Changed underscore by lodash

--- app/assets/javascripts/react/card_page/stores/RequestStore.js
+++ app/assets/javascripts/react/card_page/stores/RequestStore.js
@@ -1,4 +1,4 @@
-var _                  = require('underscore'),
+var _                  = require('lodash'),
     Backbone           = require('backbone'),
     PlanningStore      = require('../stores/PlanningStore'),
     CourseStore        = require('../stores/CourseStore'),
