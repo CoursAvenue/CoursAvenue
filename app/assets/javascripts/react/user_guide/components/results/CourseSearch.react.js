f45bd8853ddfed7208f410fc4c13a38c671b6902
Aliou Diallo <code@aliou.me>
2015-07-15 16:38:01 +0200

    Update course search component.

--- app/assets/javascripts/react/user_guide/components/results/CourseSearch.react.js
+++ app/assets/javascripts/react/user_guide/components/results/CourseSearch.react.js
@@ -1,7 +1,33 @@
-// TODO: Use the city completion from zip code.
 var CourseSearch = React.createClass({
+    propTypes: {
+        subject: React.PropTypes.object,
+    },
+
+    getInitialState: function getInitialState () {
+        return { searchURL: null };
+    },
+
+    componentDidUpdate: function componentDidUpdate () {
+        $(this.getDOMNode()).find('[data-behavior=city-autocomplete]').cityAutocomplete();
+    },
+
+    // TODO: This doesn't fucking work.
+    updateUrl: function updateUrl (event) {
+        event.preventDefaul();
+        var city = event.trigger;
+        debugger
+        this.setState({ searchURL: Routes.city_subject_path(city, this.props.subject.slug) });
+    },
+
     render: function render () {
-        return false;
+        if (!this.props.subject) { return false; }
+        return (
+            <div className='text--center'>
+                <input autoComplete='off' placeholder='Code Postal' data-behavior='city-autocomplete' data-el='#subject_search_city'></input>
+                <select id='subject_search_city' placeholder='Ville' onChange={ this.updateUrl }></select>
+                <a className='disabled btn' href={ this.state.searchURL }>Trouver un cours de { this.props.subject.get('name') }</a>
+            </div>
+        );
     },
 });
 
