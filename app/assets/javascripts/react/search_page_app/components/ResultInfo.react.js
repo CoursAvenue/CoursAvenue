9cc265f6de4891b277a162f502da13be1e0d414a
Nima Izadi <nim.izadi@gmail.com>
2015-07-16 15:20:46 +0200

    Fixed bug

--- app/assets/javascripts/react/search_page_app/components/ResultInfo.react.js
+++ app/assets/javascripts/react/search_page_app/components/ResultInfo.react.js
@@ -39,7 +39,7 @@ var ResultInfo = React.createClass({
             });
             if (_.size(this.state.card_store.facets[0].data) > 3) {
                 var popover_facet_content = _.map(this.state.card_store.facets[0].data, function(value, key) {
-                    return key.split(':')[0] + ' (' + value + ')';
+                    return key.split(':')[1] + ' (' + value + ')';
                 });
                 dot_dot_dot = (<span className="cursor-pointer search-page__result-info search-page__result-info--dot-dot-dot"
                                       onClick={this.showSubjectFilterPanel}
