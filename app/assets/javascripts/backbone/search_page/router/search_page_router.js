7be468c238c2cdd416c9eba1459a0ccfeaa1b28b
Nima Izadi <nim.izadi@gmail.com>
2015-07-19 10:26:30 +0200

    Removed filtered search app

--- app/assets/javascripts/backbone/search_page/router/search_page_router.js
+++
@@ -1,9 +0,0 @@
-SearchPage.module('Router', function(Module, App, Backbone, Marionette, $, _) {
-    Module.SearchPageRouter = Backbone.Router.extend({
-        routes: {
-        },
-
-        // initialize: function initialize (options) {
-        // }
-    });
-});
--- app/assets/javascripts/backbone/search_page/views/planning_view.js
+++
@@ -1,7 +0,0 @@
-SearchPage.module('Views', function(Module, App, Backbone, Marionette, $, _, undefined) {
-
-    Module.PlanningView = Marionette.ItemView.extend({
-        template: Module.templateDirname() + 'planning_view',
-        // childViewContainer: '[data-type=container]'
-    });
-});
