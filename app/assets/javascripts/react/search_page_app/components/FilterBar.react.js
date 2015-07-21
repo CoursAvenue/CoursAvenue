20c1a9e92466bc7829eb9cf05477b6cb192a7b87
Nima Izadi <nim.izadi@gmail.com>
2015-07-21 13:14:53 +0200

    Improved design

--- app/assets/javascripts/react/search_page_app/components/FilterBar.react.js
+++ app/assets/javascripts/react/search_page_app/components/FilterBar.react.js
@@ -38,37 +38,51 @@ var FilterBar = React.createClass({
 
     render: function render () {
         return (
-          <div className="search-page-filter-bar-wrapper grid--full visuallyhidden--palm">
+          <div className="search-page-filter-bar-wrapper main-container--1000 main-container grid--full visuallyhidden--palm">
               <div className={ cx("grid__item search-page-filter search-page-filter--subject three-tenths",
                                 { 'search-page-filter--active': this.state.filter_store.get('current_panel') == FilterPanelConstants.FILTER_PANELS.SUBJECTS,
                                   'search-page-filter--has-filters': this.state.card_store.hasActiveFilters('subject') }) }
                    onClick={this.toggleSubjectFilter}>
-                <i className="v-middle fa fa-chevron-up"></i>
-                <i className="v-middle fa fa-question-circle"></i>
-                Quoi
+                <div className="search-page-filter__icon relative inline-block v-middle">
+                    <i className="v-middle fa fa-chevron-up"></i>
+                    <i className="v-middle fa fa-question-circle"></i>
+                </div>
+                <div className="inline-block v-middle">
+                    Quoi
+                </div>
               </div>
               <div className={cx("grid__item search-page-filter search-page-filter--location three-tenths",
                                 { 'search-page-filter--active': this.state.filter_store.get('current_panel') == FilterPanelConstants.FILTER_PANELS.LOCATION,
                                   'search-page-filter--has-filters': this.state.card_store.hasActiveFilters('location') }) }
                    onClick={this.toggleLocationFilter}>
-                <i className="v-middle fa fa-chevron-up"></i>
-                <i className="v-middle fa fa-map-marker"></i>
-                Où
+                  <div className="search-page-filter__icon relative inline-block v-middle">
+                      <i className="v-middle fa fa-chevron-up"></i>
+                      <i className="v-middle fa fa-map-marker"></i>
+                  </div>
+                  <div className="inline-block v-middle">
+                      Où
+                  </div>
               </div>
               <div className={cx("grid__item search-page-filter search-page-filter--time three-tenths",
                                 { 'search-page-filter--active': this.state.filter_store.get('current_panel') == FilterPanelConstants.FILTER_PANELS.TIME,
                                   'search-page-filter--has-filters': this.state.card_store.hasActiveFilters('time') }) }
                    onClick={this.toggleTimeFilter}>
-                <i className="v-middle fa fa-chevron-up"></i>
-                <i className="v-middle fa fa-clock"></i>
-                Quand
+                  <div className="search-page-filter__icon relative inline-block v-middle">
+                      <i className="v-middle fa fa-chevron-up"></i>
+                      <i className="v-middle fa fa-clock"></i>
+                  </div>
+                  <div className="inline-block v-middle">
+                      Quand
+                  </div>
               </div>
               <div className={cx("grid__item one-tenth search-page-filter search-page-filter--more",
                                { 'search-page-filter--active': this.state.filter_store.get('current_panel') == FilterPanelConstants.FILTER_PANELS.MORE,
                                  'search-page-filter--has-filters': this.state.card_store.hasActiveFilters('more') }) }
                    onClick={this.toggleMoreFilter}>
-                <i className="v-middle fa fa-chevron-up"></i>
-                <i className="v-middle fa fa-more-filters"></i>
+                  <div className="search-page-filter__icon relative inline-block v-middle">
+                      <i className="v-middle fa fa-chevron-up"></i>
+                      <i className="v-middle fa fa-more-filters"></i>
+                  </div>
               </div>
           </div>
         );
