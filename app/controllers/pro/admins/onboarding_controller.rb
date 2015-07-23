14549a779050576827a486b270245d6fdc773615
Aliou Diallo <code@aliou.me>
2015-07-23 11:11:34 +0200

    Render onboarading on every pages.

--- app/controllers/pro/admins/onboarding_controller.rb
+++ app/controllers/pro/admins/onboarding_controller.rb
@@ -10,40 +10,4 @@ class Pro::Admins::OnboardingController < ApplicationController
       raise 'step_one'
     end
   end
-
-  #
-  # def step_zero
-  # end
-  #
-  # def step_one
-  #   if request.xhr?
-  #     render layout: false
-  #   else
-  #     raise 'step_one'
-  #   end
-  # end
-  #
-  # def step_two
-  #   if request.xhr?
-  #     render layout: false
-  #   else
-  #     raise 'step_two'
-  #   end
-  # end
-  #
-  # def step_three
-  #   if request.xhr?
-  #     render layout: false
-  #   else
-  #     raise 'step_three'
-  #   end
-  # end
-  #
-  # def step_four
-  #   if request.xhr?
-  #     render layout: false
-  #   else
-  #     raise 'step_four'
-  #   end
-  # end
 end
