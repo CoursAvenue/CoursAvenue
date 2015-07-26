da890c755603b29c8f38e9a9c1647b46e884fc2e
Aliou Diallo <code@aliou.me>
2015-07-26 22:42:34 +0200

    Center answer.

--- app/assets/javascripts/react/user_guide/components/Answer.react.js
+++ app/assets/javascripts/react/user_guide/components/Answer.react.js
@@ -14,7 +14,8 @@ var Answer = React.createClass({
             <div className='cursor-pointer black-curtain__fading-on-hover bg-position-top bg-cover relative text--center white grid__item one-half palm-one-whole height-35vh'
                 onClick={ this.props.select } style={ style }>
               <div className='black-curtain north west one-whole absolute'></div>
-              <div className='f-weight-600 soft-half--sides absolute west one-whole quizz-question-text palm-text--milli'>
+              <div className='f-weight-600 soft-half--sides absolute west one-whole quizz-question-text palm-text--milli'
+                       style={ { marginTop: 'auto' } } >
                 <div className='text--center alpha palm-delta'>{ this.props.answer.content }</div>
               </div>
             </div>
