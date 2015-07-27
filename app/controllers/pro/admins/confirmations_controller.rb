db9bbc830c1ffc991a4d39c651fa7e2a343e7bae
Aliou Diallo <code@aliou.me>
2015-07-27 18:09:03 +0200

    Show onboarding after registration.

--- app/controllers/pro/admins/confirmations_controller.rb
+++ app/controllers/pro/admins/confirmations_controller.rb
@@ -1,7 +1,7 @@
 class Pro::Admins::ConfirmationsController < Devise::ConfirmationsController
   def after_confirmation_path_for(resource_name, admin)
     if admin.structure
-      edit_pro_structure_path(admin.structure)
+      edit_pro_structure_path(admin.structure, { show_onboarding: true })
     else
       root_path
     end
