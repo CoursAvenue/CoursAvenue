43715adcbfae0b5aebddb76e716ce4490216fccd
Aliou Diallo <code@aliou.me>
2015-07-27 16:14:59 +0200

    Simply close the onboarding guide when finished.

--- app/controllers/pro/admins/onboarding_controller.rb
+++ app/controllers/pro/admins/onboarding_controller.rb
@@ -1,14 +1,6 @@
 class Pro::Admins::OnboardingController < ApplicationController
   before_action :render_step, except: [:update]
 
-  def update
-    @structure = Structure.find(params[:id])
-    @admin = @structure.main_contact
-
-    @admin.onboard! if @admin and !current_pro_admin.super_admin?
-    redirect_to edit_pro_structure_path(@structure)
-  end
-
   private
 
   def render_step
