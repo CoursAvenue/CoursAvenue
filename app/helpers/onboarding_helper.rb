c16050e1614df551581c2e47a67f4bc4763a5b26
Aliou Diallo <code@aliou.me>
2015-07-27 15:33:57 +0200

    Extract onboarding step and percentage to helper.

---
+++ app/helpers/onboarding_helper.rb
@@ -0,0 +1,33 @@
+module OnboardingHelper
+  def admin_current_onboarding_step(structure)
+    if !structure.profile_completed?
+      link_to edit_pro_structure_path(structure) do
+        "<i class='fa fa-square-o'></i>".html_safe +
+          ' Compléter mes informations générales et ajouter un logo'
+      end
+    elsif structure.comments.empty? and structure.comment_notifications.empty?
+      link_to recommendations_pro_structure_path(structure) do
+        "<i class='fa fa-square-o'></i>".html_safe +
+          ' Demander des recommandations à mes élèves'
+      end
+    elsif structure.medias.empty?
+      link_to pro_structure_medias_path(structure) do
+        "<i class='fa fa-square-o'></i>".html_safe +
+          ' Mettre en ligne les photos et vidéos de mes cours'
+      end
+    elsif structure.plannings.future.any?
+      link_to pro_structure_courses_path(structure) do
+        "<i class='fa fa-square-o'></i>".html_safe +
+          ' Renseigner mes cours et mon planning'
+      end
+    end
+  end
+
+  def admin_current_onboarding_percentage(structure)
+    status = 100
+    status -= 25 if !structure.profile_completed?
+    status -= 25 if structure.medias.empty?
+    status -= 25 if structure.comments.empty?
+    status -= 25 if structure.plannings.future.empty?
+  end
+end
