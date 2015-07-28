1697f0b4cf5593d44528db61095c43b27f108fe8
Aliou Diallo <code@aliou.me>
2015-07-28 14:04:50 +0200

    Create community thread.

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,9 @@
 class Community < ActiveRecord::Base
   belongs_to :structure
 
+  has_many :threads, -> { order(created_at: :desc) },
+    class_name: 'Community::Thread', dependent: :destroy
+
   has_many :memberships, class_name: 'Community::Membership', dependent: :destroy
   has_many :users, through: :memberships
 end
--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Community, type: :model, community: true do
   context 'association' do
     it { should belong_to(:structure) }
+    it { should have_many(:threads).class_name('Community::Thread') }
     it { should have_many(:memberships).class_name('Community::Membership') }
     it { should have_many(:users).through(:memberships) }
   end
