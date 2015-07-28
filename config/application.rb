c125b73a27dd1ac934dad24b87281436b465deb9
Aliou Diallo <code@aliou.me>
2015-07-28 16:57:25 +0200

    Update the mandrill reply domain

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 module CoursAvenue
   class Application < Rails::Application
 
-    MANDRILL_REPLY_TO_DOMAIN = Rails.env.staging? ? 'reply-staging.coursaven.eu' : 'reply.coursavenue.com'
+    MANDRILL_REPLY_TO_DOMAIN = Rails.env.staging? ? 'reply.coursaven.eu' : 'reply.coursavenue.com'
     AMAZON_S3                = AWS::S3.new(access_key_id: ENV['AWS_ACCESS_KEY_ID'], secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
     S3_BUCKET                = AMAZON_S3.buckets[ENV['AWS_BUCKET']]
     FACEBOOK_APP_ID          = 589759807705512
