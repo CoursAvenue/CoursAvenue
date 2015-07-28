91991cf1dbee7d51f11f6b7669fcf4aa07f62b7c
Aliou Diallo <code@aliou.me>
2015-07-28 16:00:08 +0200

    Add community membership to message thread.

--- app/models/community/message_thread.rb
+++ app/models/community/message_thread.rb
@@ -1,4 +1,6 @@
 class Community::MessageThread < ActiveRecord::Base
   belongs_to :community
-  belongs_to :conversation, class_name: 'Mailboxer::Conversation', foreign_key: 'mailboxer_conversation_id'
+  belongs_to :membership, class_name: 'Community::Membership', foreign_key: 'community_membership_id'
+  belongs_to :conversation, class_name: 'Mailboxer::Conversation',
+    foreign_key: 'mailboxer_conversation_id'
 end
--- spec/models/community/message_thread_spec.rb
+++ spec/models/community/message_thread_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Community::MessageThread, type: :model, community: true do
   context 'associations' do
     it { should belong_to(:community) }
+    it { should belong_to(:membership).class_name('Community::Membership').
+         with_foreign_key('community_membership_id') }
     it { should belong_to(:conversation).class_name('Mailboxer::Conversation').
          with_foreign_key('mailboxer_conversation_id') }
   end
