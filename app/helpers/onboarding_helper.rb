31e60348cb9528247ff1df615673b273688f4e1b
Aliou Diallo <code@aliou.me>
2015-07-30 09:44:45 +0200

    Show tick on hover of whole onboarding step.

--- app/helpers/onboarding_helper.rb
+++ app/helpers/onboarding_helper.rb
@@ -1,23 +1,23 @@
 module OnboardingHelper
   def admin_current_onboarding_step(structure)
     if !structure.profile_completed?
-      link_to edit_pro_structure_path(structure) do
-        "<i class='fa fa-square-o'></i>".html_safe +
+      link_to edit_pro_structure_path(structure), data: { onboarding_step: true } do
+        "<i class='fa fa-square-o' data-square='true'></i>".html_safe +
           ' Ajoutez votre description et votre logo'
       end
     elsif structure.comments.empty? and structure.comment_notifications.empty?
-      link_to recommendations_pro_structure_path(structure) do
-        "<i class='fa fa-square-o'></i>".html_safe +
+      link_to recommendations_pro_structure_path(structure), data: { onboarding_step: true } do
+        "<i class='fa fa-square-o' data-square='true'></i>".html_safe +
           ' Boostez votre bouche-à-oreille'
       end
     elsif structure.medias.empty?
-      link_to pro_structure_medias_path(structure) do
-        "<i class='fa fa-square-o'></i>".html_safe +
+      link_to pro_structure_medias_path(structure), data: { onboarding_step: true } do
+        "<i class='fa fa-square-o' data-square='true'></i>".html_safe +
           ' Ajoutez vos photos et vidéos'
       end
     elsif structure.plannings.future.any?
-      link_to pro_structure_courses_path(structure) do
-        "<i class='fa fa-square-o'></i>".html_safe +
+      link_to pro_structure_courses_path(structure), data: { onboarding_step: true } do
+        "<i class='fa fa-square-o' data-square='true'></i>".html_safe +
           ' Mettez à jour votre planning'
       end
     else
