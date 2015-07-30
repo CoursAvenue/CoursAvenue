a3d11da28d442dbfe3e6cd68ea3d2ad90c4fc26a
Aliou Diallo <code@aliou.me>
2015-07-30 16:16:25 +0200

    Show message threads.

--- app/serializers/community/message_threads_serializer.rb
+++ app/serializers/community/message_threads_serializer.rb
@@ -1,19 +1,29 @@
 class Community::MessageThreadsSerializer < ActiveModel::Serializer
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::DateHelper
+
   attributes :id, :community_id, :messages
 
   def messages
     return [] if object.conversation.nil?
 
     object.conversation.messages.map do |message|
-      sender = message.sender_type == 'User' ? User.find(message.sender_id) : Admin.find(message.sender_id)
+      if message.sender_type == 'User'
+        sender = User.find(message.sender_id)
+        avatar_url = sender.avatar.url
+      else
+        sender = Admin.find(message.sender_id)
+        avatar_url = sender.avatar
+      end
+
       {
         id: message.id,
-        body: message.body,
-        sender: {
-          id: message.sender_id,
-          name: sender.name,
-        },
-        date: I18n.l(message.created_at, format: :long_human)
+        content: message.body,
+        avatar_url: avatar_url,
+        created_at: message.created_at,
+        author_name: sender.name,
+        distance_of_time: distance_of_time_in_words_to_now(message.created_at),
+        created_at_iso: I18n.l(message.created_at, format: :iso_date_8601)
       }
     end
   end
