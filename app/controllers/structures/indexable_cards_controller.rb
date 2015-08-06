6f6e3b81feb042a36aa68aa37ac55cb654b989d8
Nima Izadi <nim.izadi@gmail.com>
2015-08-06 18:04:34 +0200

    Added 301 for google

--- app/controllers/structures/indexable_cards_controller.rb
+++ app/controllers/structures/indexable_cards_controller.rb
@@ -4,7 +4,12 @@ class Structures::IndexableCardsController < ApplicationController
   def show
     @structure           = Structure.friendly.find params[:structure_id]
     @structure_decorator = @structure.decorate
-    @indexable_card      = @structure.indexable_cards.find params[:id]
+    @indexable_card      = @structure.indexable_cards.where(id: params[:id]).first
+    # 301 for Google
+    if @indexable_card.nil?
+      redirect_to structure_path(@structure), status: 301
+      return
+    end
     @course              = @indexable_card.course
     @place               = @indexable_card.place.decorate
 
