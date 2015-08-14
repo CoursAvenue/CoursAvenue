0ec0e01c1b1fac50ede9e12bd67857e7edede0f4
Nima Izadi <nim.izadi@gmail.com>
2015-08-14 19:05:25 +0200

    Stop using jpegmin

--- app/uploaders/vertical_page_image_uploader.rb
+++ app/uploaders/vertical_page_image_uploader.rb
@@ -9,37 +9,37 @@ class VerticalPageImageUploader < CarrierWave::Uploader::Base
 
   version :thumb do
     cloudinary_transformation transformation: [{ width: 250, height: 200, crop: :fill }],
-                              flags: :progressive, quality: 'jpegmini:0'
+                              flags: :progressive, quality: 70
   end
 
   version :small do
     cloudinary_transformation transformation: [{ width: 250, height: 200, crop: :fill }],
-                              flags: :progressive, quality: 'jpegmini:0'
+                              flags: :progressive, quality: 70
   end
 
   version :autocomplete do
     cloudinary_transformation transformation: [{ width: 80, height: 45, crop: :fill }],
-                              flags: :progressive, quality: 'jpegmini:0'
+                              flags: :progressive, quality: 70
   end
 
   version :homepage do
     cloudinary_transformation transformation: [{ width: 1000, height: 500, crop: :fill }],
-                              flags: :progressive, quality: 'jpegmini:0'
+                              flags: :progressive, quality: 70
   end
 
   version :homepage_small do
     cloudinary_transformation transformation: [{ width: 500, height: 220, crop: :fill }],
-                              flags: :progressive, quality: 'jpegmini:0'
+                              flags: :progressive, quality: 70
   end
 
   version :search_page do
     cloudinary_transformation transformation: [{ width: 600, height: 500, crop: :fill }],
-                              flags: :progressive, quality: 'jpegmini:0'
+                              flags: :progressive, quality: 70
   end
 
   version :large do
     cloudinary_transformation transformation: [{ width: 1600, height: 500, crop: :fill }],
-                              flags: :progressive, quality: 'jpegmini:0'
+                              flags: :progressive, quality: 70
   end
 
 end
