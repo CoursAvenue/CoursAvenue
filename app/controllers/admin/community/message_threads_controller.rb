e65eb3a059a17162aa0da99c2465e78601e370b2
Aliou Diallo <code@aliou.me>
2015-08-18 09:49:41 +0200

    Unscope Community to prevent module mismatch.

--- app/controllers/admin/community/message_threads_controller.rb
+++ app/controllers/admin/community/message_threads_controller.rb
@@ -4,6 +4,6 @@ class Admin::Community::MessageThreadsController < ApplicationController
   layout 'admin'
 
   def index
-    @message_threads = Community::MessageThread.order('created_at DESC').page(params[:page] || 1)
+    @message_threads = ::Community::MessageThread.order('created_at DESC').page(params[:page] || 1)
   end
 end
