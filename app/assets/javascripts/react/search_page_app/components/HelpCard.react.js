a126665d9ec21e22acd642d17f144df3b6ce1955
Aliou Diallo <code@aliou.me>
2015-08-19 11:57:17 +0200

    Add help card component.

---
+++ app/assets/javascripts/react/search_page_app/components/HelpCard.react.js
@@ -0,0 +1,32 @@
+var Card               = require('./Card'),
+    CardActionCreators = require("../actions/CardActionCreators");
+
+var HelpCard = React.createClass({
+    goToPage: function goToPage () {
+        window.location = this.props.helper.get('url');
+    },
+
+    dismiss: function dismiss () {
+        CardActionCreators.dismissHelp(this.props.helper);
+    },
+
+    render: function render () {
+        var options = {
+            index: this.props.index,
+            onClick: this.goToPage,
+            classes: this.props.width_class
+        };
+
+        return (
+            <Card {...options}>
+                <div className='bg-white search-page-card__content'>
+                    <h4 className="flush text--center soft-half--sides">
+                        { this.props.helper.get('name') }
+                    </h4>
+                </div>
+            </Card>
+        );
+    },
+});
+
+module.exports = HelpCard;
