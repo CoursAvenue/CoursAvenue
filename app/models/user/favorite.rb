639566c3e7319866616af55d436c55f950839141
Aliou Diallo <code@aliou.me>
2015-08-19 17:25:55 +0200

    Update validations to match model changes.

--- app/models/user/favorite.rb
+++ app/models/user/favorite.rb
@@ -3,16 +3,24 @@ class User::Favorite < ActiveRecord::Base
   belongs_to :structure
   belongs_to :indexable_card
 
-  attr_accessible :structure, :user
+  attr_accessible :structure, :user, :indexable_card
 
-  validates :structure, :user, presence: true
-  validates :user_id, uniqueness: { scope: :structure_id }
+  validates :user,           presence: true
+  validates :structure,      presence: true, unless: -> (favorite) { favorite.indexable_card_id.present? }
+  validates :indexable_card, presence: true, unless: -> (favorite) { favorite.structure_id.present? }
+
+  validates :user_id, uniqueness: { scope: :structure_id },      allow_nil: true
+  validates :user_id, uniqueness: { scope: :indexable_card_id }, allow_nil: true
 
   after_create :affect_tag_to_user_profile
+
+  scope :structure,      -> { where.not(structure_id: nil) }
+  scope :indexable_card, -> { where.not(indexable_card_id: nil) }
 
   private
 
   def affect_tag_to_user_profile
+    return if structure.nil?
     structure.create_or_update_user_profile_for_user(user, UserProfile::DEFAULT_TAGS[:following])
   end
 end
