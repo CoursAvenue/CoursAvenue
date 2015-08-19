bade567bd289776bc686007508ba14d7fe354738
Aliou Diallo <code@aliou.me>
2015-08-19 14:46:51 +0200

    Improve helper card design.

--- app/assets/javascripts/react/search_page_app/components/HelpCard.react.js
+++ app/assets/javascripts/react/search_page_app/components/HelpCard.react.js
@@ -6,18 +6,44 @@ var HelpCard = React.createClass({
         CardActionCreators.dismissHelp(this.props.helper);
     },
 
+    toggleDismiss: function toggleDismiss () {
+        // TODO:
+    },
+
     render: function render () {
+        var helper = this.props.helper;
         var options = {
-            index: this.props.index,
             classes: this.props.width_class
         };
 
+        var content_class = 'bg-white search-page-card__content ' + 'search-page-card__content--' +
+            helper.get('type');
+
         return (
             <Card {...options}>
-                <div className='bg-white search-page-card__content'>
-                    <h4 className="flush text--center soft-half--sides">
-                        { this.props.helper.get('name') }
-                    </h4>
+                <div className={ content_class }>
+                    <div className='search-page-card__content-top'>
+                        <div className='relative'>
+                            <div className='search-page-card__dismiss'>
+                                <a href='javascript:void(0)' onClick={ this.dismiss }>&times;</a>
+                            </div>
+                        </div>
+                        <h4 className="flush text--center soft-half caps f-weight-bold">
+                            { helper.strType() }
+                        </h4>
+                    </div>
+
+                    <div className="soft-half--sides soft-half--bottom text--center">
+                        <h4 className='line-height-normal'>{ helper.get('description') }</h4>
+                        <a href={ helper.get('url') } className='btn btn--blue' target='_blank'>
+                            { helper.get('call_to_action') }
+                        </a>
+                        <div className='text--center soft-half'>
+                            <input type='checkbox' className='input--large' onChange={ this.toggleDismiss } />
+                            <label className='f-weight-bold'>Ne plus afficher</label>
+                        </div>
+                    </div>
+
                 </div>
             </Card>
         );
