f27511f6c8a4eac82080913b4d6b70f145e951bb
Aliou Diallo <code@aliou.me>
2015-08-27 18:20:17 +0200

    Add Similar card to indexable card page.

--- app/assets/javascripts/react/components.js
+++ app/assets/javascripts/react/components.js
@@ -13,6 +13,7 @@ Course                    = require('./card_page/components/Course.react');
 CommentList               = require('./card_page/components/CommentList.react');
 MessageThreadList         = require('./card_page/components/MessageThreadList.react');
 SimilarProfileList        = require('./card_page/components/SimilarProfileList.react');
+SimilarCardList           = require('./card_page/components/SimilarCardList.react');
 Favorite                  = require('./card_page/components/Favorite.react');
 
 UserGuideApp              = require('./user_guide/components/UserGuide.react');
