67b94fb4188eded9049dd1b0687762fab00efd92
Aliou Diallo <code@aliou.me>
2015-08-31 11:51:00 +0200

    Remove unused code.

--- spec/models/indexable_card/creator_spec.rb
+++ spec/models/indexable_card/creator_spec.rb
@@ -49,12 +49,6 @@ describe IndexableCard::Creator do
           expect { subject.update_cards; structure.reload }.
             to change { structure.indexable_cards.count }.by(1)
         end
-
-        # Can't be correctly tested anymore because we no longer return the new cards.
-        # it 'associates the new card with the place' do
-        #   cards = subject.update_cards
-        #   expect(cards.first.place).to eq(new_place)
-        # end
       end
     end
   end
