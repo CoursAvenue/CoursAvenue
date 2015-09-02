ef7dd28d5157ce944a5947c47e7e37f0400ea43d
Nima Izadi <nim.izadi@gmail.com>
2015-09-02 11:37:22 +0200

    Fixed design for cards places

--- app/decorators/place_decorator.rb
+++ app/decorators/place_decorator.rb
@@ -1,12 +1,12 @@
 class PlaceDecorator < Draper::Decorator
   delegate_all
 
-  def popover_place_infos
+  def popover_place_infos(show_private_info=false)
     infos = ""
     if info.present?
       infos << "<div><strong>Infos publiques</strong><br>#{info}</div>"
     end
-    if private_info.present?
+    if show_private_info and private_info.present?
       infos << "<div><strong>Infos privées</strong><br>#{private_info}</div>"
     end
     infos.html_safe
