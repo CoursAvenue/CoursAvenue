dbfc8ea2552c6757ab80f63bbdf0c6c568cbd224
Aliou Diallo <code@aliou.me>
2015-09-02 12:09:00 +0200

    Use the course price when computing the total price.

--- app/models/participation_request/participant.rb
+++ app/models/participation_request/participant.rb
@@ -15,6 +15,18 @@ class ParticipationRequest::Participant < ActiveRecord::Base
   end
 
   def total_price
-    price.present? ? price.final_amount * number : 0
+    individual_price.present? ? individual_price.final_amount * number : 0
+  end
+
+  # TODO (aliou): This is a bad idea.
+  # Get the actual price for the participant. Either the price it has, or the trial price.
+  #
+  # @return a price or nil
+  def individual_price
+    if price.present?
+      price
+    elsif !participation_request.course.no_trial?
+      participation_request.course.prices.where(type: 'Price::Trial').first
+    end
   end
 end
