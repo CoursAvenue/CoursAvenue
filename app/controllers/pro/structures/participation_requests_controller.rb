cbc0c7730b55f44bd0840e7a8de2a62844564a15
Aliou Diallo <code@aliou.me>
2015-09-03 10:34:27 +0200

    Fix error rejecting the prs.

--- app/controllers/pro/structures/participation_requests_controller.rb
+++ app/controllers/pro/structures/participation_requests_controller.rb
@@ -13,11 +13,11 @@ class Pro::Structures::ParticipationRequestsController < ApplicationController
 
   # GET pro/etablissements/:structure_id/pass-decouverte-suivi
   def index
-    @participation_requests = @structure.participation_requests.reject { |pr| pr.user.deleted? }
+    @participation_requests = @structure.participation_requests
     # Select participation request that have the right label id (some could have been flagged as inapropriate
     # and therefore have a different label_id)
-    @upcoming_participation_requests = @participation_requests.upcoming
-    @past_participation_requests     = @participation_requests.order('date DESC').past
+    @upcoming_participation_requests = @participation_requests.upcoming.reject { |pr| pr.user.deleted? }
+    @past_participation_requests     = @participation_requests.order('date DESC').past.reject { |pr| pr.user.deleted? }
   end
 
   # GET pro/etablissements/:structure_id/inscriptions-via-carte-bleu
