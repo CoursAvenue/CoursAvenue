ee5b119c312932dea7fd52215843c75eb8e148dd
Nima Izadi <nim.izadi@gmail.com>
2015-09-07 10:36:40 +0200

    Fixed migrations

--- db/migrate/20150904083823_add_image_to_teachers.rb
+++ db/migrate/20150904083823_add_image_to_teachers.rb
@@ -6,14 +6,14 @@ class AddImageToTeachers < ActiveRecord::Migration
     rename_column :teachers, :image_file_size, :old_image_file_size
     rename_column :teachers, :image_updated_at, :old_image_updated_at
 
-    if Rails.env.production?
-      bar = ProgressBar.new(Teacher.where.not(old_image_file_name: nil).count)
-      Teacher.where.not(old_image_file_name: nil).find_each do |teacher|
-        bar.increment!
-        next if teacher.old_image.blank?
-        teacher.remote_image_url = teacher.old_image.url
-        teacher.save
-      end
-    end
+    # if Rails.env.production?
+    #   bar = ProgressBar.new(Teacher.where.not(old_image_file_name: nil).count)
+    #   Teacher.where.not(old_image_file_name: nil).find_each do |teacher|
+    #     bar.increment!
+    #     next if teacher.old_image.blank?
+    #     teacher.remote_image_url = teacher.old_image.url
+    #     teacher.save
+    #   end
+    # end
   end
 end
--- db/migrate/20150904093814_add_cloudinary_header_image_to_emailings.rb
+++ db/migrate/20150904093814_add_cloudinary_header_image_to_emailings.rb
@@ -6,14 +6,14 @@ class AddCloudinaryHeaderImageToEmailings < ActiveRecord::Migration
     rename_column :emailings, :header_image_file_size, :old_header_image_file_size
     rename_column :emailings, :header_image_updated_at, :old_header_image_updated_at
 
-    if Rails.env.production?
-      bar = ProgressBar.new(Emailing.where.not(old_header_image_file_name: nil).count)
-      Emailing.where.not(old_header_image_file_name: nil).find_each do |emailing|
-        bar.increment!
-        next if emailing.old_header_image.blank?
-        emailing.remote_header_image_url = emailing.old_header_image.url
-        emailing.save
-      end
-    end
+    # if Rails.env.production?
+    #   bar = ProgressBar.new(Emailing.where.not(old_header_image_file_name: nil).count)
+    #   Emailing.where.not(old_header_image_file_name: nil).find_each do |emailing|
+    #     bar.increment!
+    #     next if emailing.old_header_image.blank?
+    #     emailing.remote_header_image_url = emailing.old_header_image.url
+    #     emailing.save
+    #   end
+    # end
   end
 end
