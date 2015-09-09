8d39d06f7ee02ce55b496ac63a8978ed57f378bc
Aliou Diallo <code@aliou.me>
2015-09-09 10:16:19 +0200

    Add workers with different job queues.

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -2,4 +2,12 @@ HireFire::Resource.configure do |config|
   config.dyno(:worker) do
     HireFire::Macro::Delayed::Job.queue(mapper: :active_record)
   end
+
+  config.dyno(:mailers) do
+    HireFire::Macro::Delayed::Job.queue(mapper: :active_record)
+  end
+
+  config.dyno(:cards) do
+    HireFire::Macro::Delayed::Job.queue(mapper: :active_record)
+  end
 end
