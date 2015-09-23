d7886fca6ecb27771983549bf65b5067e9627898
Aliou Diallo <code@aliou.me>
2015-09-23 10:55:15 +0200

    Optimize delayed job queries by indexing the queue.
    http://www.sitepoint.com/delayed-jobs-best-practices/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922151005) do
+ActiveRecord::Schema.define(version: 20150923085208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -388,6 +388,7 @@ ActiveRecord::Schema.define(version: 20150922151005) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  add_index "delayed_jobs", ["queue"], name: "index_delayed_jobs_on_queue", using: :btree
 
   create_table "emailing_section_bridges", force: true do |t|
     t.integer "emailing_section_id"
