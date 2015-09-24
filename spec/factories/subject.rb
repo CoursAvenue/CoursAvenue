6b532d3234c7dd713a8daef37639f0a3d09d4fb1
Aliou Diallo <code@aliou.me>
2015-09-24 10:47:49 +0200

    Fuck off and die in a fire.

--- spec/factories/subject.rb
+++ spec/factories/subject.rb
@@ -2,29 +2,30 @@
 FactoryGirl.define do
 
   factory :subject do
-    name { Faker::Name.name + ' child'}
+    name { Faker::Name.name + rand.to_s + ' child'}
 
     factory :subject_children do
-      name { Faker::Name.name + ' child' }
+      name { Faker::Name.name + rand.to_s + ' child' }
 
       after :build do |subject|
-        subject_grand_parent   = Subject.create(name: Faker::Name.name)
+        subject_grand_parent   = Subject.create(name: Faker::Name.name + rand.to_s)
         # Save record before performing tree operations.
         subject_grand_parent.save
-        subject_parent         = subject_grand_parent.children.create(name: Faker::Name.name)
+        subject_parent         = subject_grand_parent.children.
+          create(name: Faker::Name.name + rand.to_s)
         subject.parent         = subject_parent
         subject.ancestry_depth = 2
       end
     end
 
     factory :subject_with_parent do
-      name { Faker::Name.name + ' parent' }
+      name { Faker::Name.name + rand.to_s + ' parent' }
       association :parent, factory: :subject
       ancestry_depth 1
     end
 
     factory :subject_with_grand_parent do
-      name { Faker::Name.name + ' grand parent' }
+      name { Faker::Name.name + rand.to_s + ' grand parent' }
       association :parent, factory: :subject_with_parent
       ancestry_depth 2
     end
