d70cf9f2d4ee3e0d56a2f5b42d87ddfd5f8894ee
Aliou Diallo <code@aliou.me>
2015-09-25 11:30:19 +0200

    Create base controller for in pro subdomain.
    We authenticate and authorize the admin and its structure in this
    controller, and we will only have to inherit from in any other
    controller that needs those behaviors.

--- app/controllers/pro/pro_controller.rb
+++ app/controllers/pro/pro_controller.rb
@@ -1,4 +1,18 @@
 class Pro::ProController < ApplicationController
   layout 'admin'
   helper OnboardingHelper
+
+  before_action :authenticate_pro_admin!
+  before_action :load_structure
+
+  authorize_resource :structure
+
+  private
+
+  def load_structure
+    if params[:id] == '_REPLACE_THIS_' and current_pro_admin.present?
+      params[:id] = current_pro_admin.structure.slug
+    end
+    @structure = Structure.friendly.find(params[:id]) if params[:id].present?
+  end
 end
--- spec/controllers/pro/pro_controller_spec.rb
+++ spec/controllers/pro/pro_controller_spec.rb
@@ -1,4 +1,21 @@
 require "rails_helper"
 
 describe Pro::ProController do
+  include Devise::TestHelpers
+
+  let(:structure) { FactoryGirl.create(:structure_with_admin) }
+  let(:admin)     { structure.admin }
+
+  before do
+    sign_in admin
+  end
+
+  controller(Pro::ProController) do
+    def index
+      render nothing: true
+    end
+  end
+
+  it { should use_before_action(:authenticate_pro_admin!) }
+  it { should use_before_action(:load_structure) }
 end
