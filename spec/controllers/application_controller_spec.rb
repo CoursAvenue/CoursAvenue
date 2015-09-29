ed6da2b742ab6e884bde7be2461486676a9391d2
Aliou Diallo <code@aliou.me>
2015-09-29 13:57:12 +0200

    Try to test abilities, by nah.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 describe ApplicationController do
+  include Devise::TestHelpers
+
   # Creating dummy routes to make sure errors are properly handled.
   controller(ApplicationController) do
+    before_filter :authenticate_user!, only: [:update]
+
     def index
       raise 'A random error'
     end
 
     def show
       structure = Structure.find(params[:id])
+      render nothing: true
+    end
+
+    def update
+      # Should raise since we are trying to update another user
+      user = User.find(params[:id])
+      user.first_name = Faker::Name.first_name
+      user.save
+
       render nothing: true
     end
   end
@@ -53,6 +66,28 @@ describe ApplicationController do
         get :show, id: Faker::Name.name.parameterize
         expect(response).to have_http_status(:moved_permanently)
       end
+    end
+  end
+
+  describe 'PUT #update -> not_allowed' do
+    xit 'redirects to the root_path' do
+      logged_in_user = FactoryGirl.create(:user)
+      other_user = FactoryGirl.create(:user)
+
+      sign_in logged_in_user
+      put :update, id: other_user.id
+
+      expect(response).to redirect_to(root_url(subdomain: 'www'))
+    end
+
+    xit 'redirects with an alert' do
+      logged_in_user = FactoryGirl.create(:user)
+      other_user = FactoryGirl.create(:user)
+
+      sign_in logged_in_user
+      put :update, id: other_user.id
+
+      expect(flash[:success]).to eq(I18n.t('devise.failure.unauthenticated'))
     end
   end
 end
