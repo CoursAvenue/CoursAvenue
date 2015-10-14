f8bb0b748efe0983bab98d31445d3bf1bb5ec639
Nima Izadi <nim.izadi@gmail.com>
2015-10-14 10:58:35 +0200

    Updated landing page

--- app/controllers/vertical_pages_controller.rb
+++ app/controllers/vertical_pages_controller.rb
@@ -7,6 +7,23 @@ class VerticalPagesController < ApplicationController
   before_action :load_root_vertical_page, only: [:show_root]
   before_action :load_vertical_page, only: [:show, :show_with_neighborhood]
 
+  SUBJECT_FOR_TEST = %w(formation-d-acteurs theatre-amateurs improvisation-theatrale
+                        art-dramatique danse-orientale salsa-bachata-chacha hatha-yoga
+                        mise-en-scene-expression-scenique pratiques-theatrales-mixtes
+                        danse-contemporaine-moderne techniques-vocales danse comedie-humour
+                        dessin jazz-street-jazz-modern-jazz tango-argentin relaxation-detente
+                        decouverte-dessin-peinture hip-hop-break-dance diction-declamation
+                        danse-indienne-bollywood tango-chachacha-samba-forro-mambo
+                        respiration danse-classique aquarelle-lavis danse-africaine-afro-jazz
+                        estime-de-soi-confiance comedies-musicales qi-gong zumba yoga-autres
+                        peinture comedie-musicale-choregraphies ashtanga-vinyasa-yoga kung-fu
+                        coaching-personnel perfectionnement-a-la-photographie croquis
+                        gymnastique-douce-stretching-etirements ateliers-corps-mouvement
+                        modelage chant-choral clown flamenco pilates self-defence peinture-a-l-huile
+                        fusion crayon-mine-fusain tai-chi-chuan-ba0d6088-526b-4b7d-a357-3706eaa2754a
+                        claquettes guitare-seche piano ecriture-theatrale sophrologie
+                        initiation-a-la-photographie yoga-nidra)
+
   def redirect_to_show
     @vertical_page = VerticalPage.friendly.find(params[:id])
     redirect_to vertical_page_path(@vertical_page.subject.root, @vertical_page), status: 301
@@ -26,7 +43,9 @@ class VerticalPagesController < ApplicationController
       redirect_to root_vertical_page_path(@vertical_page), status: 301
     end
     @ancestors               = @subject.ancestors
-    render 'test_landing'
+    if SUBJECT_FOR_TEST.include?(@subject.slug)
+      render 'test_landing'
+    end
   end
 
   def show_with_neighborhood
